83d831a81e66eb70e04e0afc2fb1b136b388b37c
MrTroble <nifroh56@gmail.com>
2017-09-17 06:22:18 +0200

    Added Render (Version independence layer)

---
+++ src/main/java/info/u_team/u_team_core/render/BufferEntry.java
@@ -0,0 +1,84 @@
+package info.u_team.u_team_core.render;
+
+
+import javax.annotation.Nonnull;
+
+import info.u_team.u_team_core.intern.UCoreConstants;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.VertexBuffer;
+import net.minecraft.client.renderer.vertex.*;
+
+public class BufferEntry {
+	
+	private static BufferEntry INSTANCE;
+	
+	public static BufferEntry getBufferEntry() {
+		if(INSTANCE == null) INSTANCE = new BufferEntry();
+		return INSTANCE;
+	}
+	
+	private Tessellator tesllator;
+	private VertexBuffer buffer;
+	
+	public BufferEntry() {
+		this.tesllator = Tessellator.getInstance();
+		this.buffer = this.tesllator.getBuffer();
+	}
+	
+	public BufferEntry vertex(float x, float y, float z) {
+		this.buffer.pos(x, y, z);
+		return this;
+	}
+	
+	public BufferEntry tex(float u, float v) {
+		this.buffer.tex(u, v);
+		return this;
+	}
+	
+	public BufferEntry color(float r, float b, float g, float a) {
+		this.buffer.color(r, g, b, a);
+		return this;
+	}
+	
+	public BufferEntry normal(float x, float y,float z) {
+		this.buffer.normal(x, y, z);
+		return this;
+	}
+	
+	public BufferEntry start(@Nonnull DrawFormat format) {
+		if(format == null) {
+			UCoreConstants.LOGGER.error("DrawFormar is null in BufferEntry.start()");
+		}
+		VertexFormat v = null;
+		switch (format) {
+		case POS_COLOR:
+			v = DefaultVertexFormats.POSITION_COLOR;
+			break;
+		case POS_TEX:
+			v = DefaultVertexFormats.POSITION_TEX;
+			break;
+		case POS_TEX_COLOR:
+			v = DefaultVertexFormats.POSITION_TEX_COLOR;
+			break;
+		case POS_TEX_COLOR_NORMAL:
+			v = DefaultVertexFormats.POSITION_TEX_COLOR_NORMAL;
+			break;
+		case POS_TEX_NORMAL:
+			v = DefaultVertexFormats.POSITION_TEX_NORMAL;
+			break;
+		}
+		if(v == null) {
+			UCoreConstants.LOGGER.error("Somthing went wrong with the draw start (Wrong vertex Format)");
+		}
+		this.buffer.begin(7, v);
+		return this;
+	}
+	
+	public void end() {
+		this.buffer.endVertex();
+	}
+	
+	public void endDraw() {
+		this.tesllator.draw();
+	}
+}
---
+++ src/main/java/info/u_team/u_team_core/render/DrawFormat.java
@@ -0,0 +1,7 @@
+package info.u_team.u_team_core.render;
+
+public enum DrawFormat {
+
+	POS_COLOR, POS_TEX, POS_TEX_COLOR, POS_TEX_NORMAL, POS_TEX_COLOR_NORMAL;
+	
+}
