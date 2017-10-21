46a086cf96bfbbcd5ff9bc033abf26713684c53c
HyCraftHD <hycrafthd@live.de>
2017-10-21 02:35:15 +0200

    Removed unnecessary methods of Rotation

--- src/main/java/info/u_team/u_team_core/schematic/Rotation.java
+++ src/main/java/info/u_team/u_team_core/schematic/Rotation.java
@@ -54,34 +54,4 @@ public enum Rotation {
 			return this;
 		}
 	}
-	
-	public EnumFacing rotate(EnumFacing facing) {
-		if (facing.getAxis() == EnumFacing.Axis.Y) {
-			return facing;
-		} else {
-			switch (this) {
-			case ROTATION_90:
-				return facing.rotateY();
-			case ROTATION_180:
-				return facing.getOpposite();
-			case ROTATION_270:
-				return facing.rotateYCCW();
-			default:
-				return facing;
-			}
-		}
-	}
-	
-	public int rotate(int p_185833_1_, int p_185833_2_) {
-		switch (this) {
-		case ROTATION_90:
-			return (p_185833_1_ + p_185833_2_ / 4) % p_185833_2_;
-		case ROTATION_180:
-			return (p_185833_1_ + p_185833_2_ / 2) % p_185833_2_;
-		case ROTATION_270:
-			return (p_185833_1_ + p_185833_2_ * 3 / 4) % p_185833_2_;
-		default:
-			return p_185833_1_;
-		}
-	}
 }
