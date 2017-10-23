9abdb967cb4258e55a75832c4f2676fdba7b9728
HyCraftHD <hycrafthd@live.de>
2017-10-23 18:15:52 +0200

    Added hooks

---
+++ src/main/java/info/u_team/u_team_core/util/hooks/ClientForgeHooks.java
@@ -0,0 +1,9 @@
+package info.u_team.u_team_core.util.hooks;
+
+import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.fml.relauncher.*;
+
+@SideOnly(Side.CLIENT)
+public class ClientForgeHooks extends ForgeHooksClient {
+	
+}
