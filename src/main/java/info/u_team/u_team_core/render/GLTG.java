e33eb2426f54968458640fada4ab10efa9cb4352
HyCraftHD <hycrafthd@live.de>
2017-11-18 14:04:57 +0100

    Gl Size fix. #2

--- src/main/java/info/u_team/u_team_core/render/GLTG.java
+++ src/main/java/info/u_team/u_team_core/render/GLTG.java
@@ -27,9 +27,9 @@ public class GLTG {
 		this.log = UCoreConstants.LOGGER;
 	}
 	
-	public GLExtent setTexture(GuiRescourceLocation location) {
+	public GLSize setTexture(GuiRescourceLocation location) {
 		Minecraft.getMinecraft().getTextureManager().bindTexture(location);
-		return new GLExtent(GL11.glGetTexLevelParameterf(GL11.GL_TEXTURE_2D, 0, GL11.GL_TEXTURE_WIDTH), GL11.glGetTexLevelParameterf(GL11.GL_TEXTURE_2D, 0, GL11.GL_TEXTURE_HEIGHT));
+		return new GLSize(GL11.glGetTexLevelParameterf(GL11.GL_TEXTURE_2D, 0, GL11.GL_TEXTURE_WIDTH), GL11.glGetTexLevelParameterf(GL11.GL_TEXTURE_2D, 0, GL11.GL_TEXTURE_HEIGHT));
 	}
 	
 	public void drawManual(@Nonnull float[][] vertex, float[][] color, float[][] tex, float[][] normal) {
