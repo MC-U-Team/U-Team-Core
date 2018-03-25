d0d8c27c5cefbea0ac8e33c55ed13e06eff848be
HyCraftHD <hycrafthd@live.de>
2018-03-26 00:08:53 +0200

    Added discord rich presence integration (Version 1.1.1)

---
+++ src/main/java/info/u_team/u_team_core/intern/discord/DiscordRichPresence.java
@@ -0,0 +1,60 @@
+package info.u_team.u_team_core.intern.discord;
+
+import java.time.OffsetDateTime;
+
+import info.u_team.u_team_core.intern.UCoreConstants;
+import info.u_team.u_team_core.repack.com.jagrosh.discordipc.IPCClient;
+import info.u_team.u_team_core.repack.com.jagrosh.discordipc.entities.RichPresence;
+import info.u_team.u_team_core.repack.com.jagrosh.discordipc.exceptions.NoDiscordClientException;
+
+/**
+ * Discord rich presence
+ * 
+ * @author HyCraftHD
+ * @date 24.03.2018
+ *
+ */
+public class DiscordRichPresence {
+	
+	private static IPCClient client = new IPCClient(427196986064764928L);
+	
+	static {
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			
+			@Override
+			public void run() {
+				client.close();
+			}
+		});
+	}
+	
+	public static void start() {
+		try {
+			client.connect();
+		} catch (NoDiscordClientException ex) {
+			UCoreConstants.LOGGER.info("No discord client is present.");
+		}
+		setIdling();
+	}
+	
+	public static void setIdling() {
+		setState("Idling in menue");
+	}
+	
+	public static void setWorld(String world) {
+		setState("World: " + world);
+	}
+	
+	public static void setServer(String server) {
+		setState("Server: " + server);
+	}
+	
+	public static void setState(String state) {
+		try {
+			RichPresence.Builder builder = new RichPresence.Builder();
+			builder.setDetails("Playing Minecraft " + UCoreConstants.MCVERSION).setState(state).setStartTimestamp(OffsetDateTime.now()).setLargeImage("minecraft", "Minecraft");
+			client.sendRichPresence(builder.build());
+		} catch (Exception ex) {
+		}
+	}
+}
