f2a9e4d11a09437480ef02e5d53cc7f476e5a9d8
HyCraftHD <hycrafthd@live.de>
2018-05-25 14:25:04 +0200

    In discord ipc i made some changes (default case for switch)

--- src/main/java/info/u_team/u_team_core/repack/com/jagrosh/discordipc/exceptions/NoDiscordClientException.java
+++ src/main/java/info/u_team/u_team_core/repack/com/jagrosh/discordipc/exceptions/NoDiscordClientException.java
@@ -6,6 +6,8 @@
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *
+ * Modifications Copyright (C) 2018 https://U-Team.info
  */
 package info.u_team.u_team_core.repack.com.jagrosh.discordipc.exceptions;
 
@@ -20,5 +22,7 @@ import info.u_team.u_team_core.repack.com.jagrosh.discordipc.entities.DiscordBui
  * @author John Grosh (john.a.grosh@gmail.com)
  */
 public class NoDiscordClientException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
 	
 }
