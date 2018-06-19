b7292435edff9ad15d7a6024d61c50c5d81dcc03
HyCraftHD <hycrafthd@live.de>
2018-06-19 03:30:41 +0200

    Removed anything that could track data. (like 1.8)

--- src/main/java/info/u_team/u_team_core/sub/USubMod.java
+++ src/main/java/info/u_team/u_team_core/sub/USubMod.java
@@ -1,9 +1,7 @@
 package info.u_team.u_team_core.sub;
 
-import org.apache.logging.log4j.Logger;
-
 import info.u_team.u_team_core.sub.metadata.MetadataFetcher;
-import info.u_team.u_team_core.updatechecker.UpdateCheckerRegistry;
+import net.minecraftforge.fml.common.*;
 import net.minecraftforge.fml.common.event.*;
 
 /**
@@ -17,28 +15,17 @@ public abstract class USubMod {
 	
 	private String modid;
 	private String name, version;
-	private String updateurl;
 	
-	protected Logger logger;
-	
-	public USubMod(String modid, String name, String version) {
-		this(modid, name, version, null);
-	}
-	
-	public USubMod(String modid, String name, String version, String updateurl) {
-		this.modid = modid;
-		this.name = name;
-		this.version = version;
-		this.updateurl = updateurl;
+	public USubMod() {
+		ModContainer container = Loader.instance().activeModContainer();
+		modid = container.getModId();
+		name = container.getName();
+		version = container.getVersion();
 	}
 	
 	public void preinit(FMLPreInitializationEvent event) {
 		USub.setID(modid);
-		logger = event.getModLog();
 		new MetadataFetcher(modid).setName(name).setVersion(version).applyMetadata(event.getModMetadata());
-		if (updateurl != null) {
-			UpdateCheckerRegistry.addMod(modid, updateurl);
-		}
 	}
 	
 	public void init(FMLInitializationEvent event) {
