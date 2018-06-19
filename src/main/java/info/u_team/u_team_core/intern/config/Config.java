58c371c7b41bff710b94ce7c6122101cebbe2da5
HyCraftHD <hycrafthd@live.de>
2018-06-19 03:30:41 +0200

    Removed anything that could track data. (like 1.8)

--- src/main/java/info/u_team/u_team_core/intern/config/Config.java
+++ src/main/java/info/u_team/u_team_core/intern/config/Config.java
@@ -15,36 +15,19 @@ public class Config {
 	
 	private static Configuration configuration;
 	
-	private static Property client_trackclientdata, client_showeula;
 	private static Property discord_enablerichpresence;
 	
 	public static void init(File file) {
 		configuration = new Configuration(file);
 		configuration.load();
 		
-		client_trackclientdata = configuration.get("client", "TrackClientData", true, "Track playtime, pc specifics and software like java, mc and forge version, as well as installed mods.");
-		client_showeula = configuration.get("client", "EulaShow", true, "Show eula on startup.");
-		
 		discord_enablerichpresence = configuration.get("discord", "EnableRichPresence", true, "If you have discord installed, then it's cool to show some details about your game ;)");
 		
 		configuration.save();
 	}
 	
-	public static boolean getTrackClientData() {
-		return client_trackclientdata.getBoolean();
-	}
-	
 	public static boolean getDiscordRichPresenceEnabled() {
 		return discord_enablerichpresence.getBoolean();
-	}
-	
-	public static boolean getEulaShow() {
-		return client_showeula.getBoolean();
-	}
-	
-	public static void setEulaShow(boolean value) {
-		client_showeula.set(value);
-		configuration.save();
 	}
 	
 }
