02e9d7cf5b9621b9f5c5369caaf7d56dcc0af39c
HyCraftHD <hycrafthd@live.de>
2018-06-24 16:03:10 +0200

    Added tileentity methods

--- src/main/java/info/u_team/u_team_core/tileentity/UTileEntity.java
+++ src/main/java/info/u_team/u_team_core/tileentity/UTileEntity.java
@@ -1,5 +1,6 @@
 package info.u_team.u_team_core.tileentity;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 
 /**
@@ -11,8 +12,25 @@ import net.minecraft.tileentity.TileEntity;
  *
  */
 
-public class UTileEntity extends TileEntity {
+public abstract class UTileEntity extends TileEntity {
 	
 	public UTileEntity() {
 	}
+	
+	@Override
+	public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+		super.writeToNBT(compound);
+		writeNBT(compound);
+		return compound;
+	}
+	
+	@Override
+	public void readFromNBT(NBTTagCompound compound) {
+		super.readFromNBT(compound);
+		readNBT(compound);
+	}
+	
+	public abstract void readNBT(NBTTagCompound compound);
+	
+	public abstract void writeNBT(NBTTagCompound compound);
 }
