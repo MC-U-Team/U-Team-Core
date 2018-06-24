85ac8fdaf9178eaf1555d02cd2a1402b3ea8f770
HyCraftHD <hycrafthd@live.de>
2018-06-24 15:08:42 +0200

    Added utility method for player inventory

--- src/main/java/info/u_team/u_team_core/container/UContainer.java
+++ src/main/java/info/u_team/u_team_core/container/UContainer.java
@@ -1,7 +1,7 @@
 package info.u_team.u_team_core.container;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.Container;
+import net.minecraft.entity.player.*;
+import net.minecraft.inventory.*;
 
 /**
  * Container API<br>
@@ -15,6 +15,18 @@ public class UContainer extends Container {
 	@Override
 	public boolean canInteractWith(EntityPlayer player) {
 		return true;
+	}
+	
+	public void appendPlayerInventory(InventoryPlayer inventory, int x, int y) {
+		for (int height = 0; height < 4; height++) {
+			for (int width = 0; width < 9; width++) {
+				if (height == 3) {
+					addSlotToContainer(new Slot(inventory, width, width * 18 + x, height * 18 + 4 + y));
+					continue;
+				}
+				addSlotToContainer(new Slot(inventory, width + height * 9 + 9, width * 18 + x, height * 18 + y));
+			}
+		}
 	}
 	
 }
