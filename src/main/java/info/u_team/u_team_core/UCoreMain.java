f5ea1dac3200b6013c02ccee1e996462b704116a
HyCraftHD <hycrafthd@live.de>
2019-02-16 21:14:48 +0100

    Fixeed mod event calling

--- src/main/java/info/u_team/u_team_core/UCoreMain.java
+++ src/main/java/info/u_team/u_team_core/UCoreMain.java
@@ -3,21 +3,23 @@ package info.u_team.u_team_core;
 import info.u_team.u_team_core.intern.proxy.*;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.*;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("uteamcore")
-@EventBusSubscriber(bus = Bus.MOD)
 public class UCoreMain {
 	
+	public UCoreMain() {
+		FMLJavaModLoadingContext.get().getModEventBus().register(this);
+	}
+	
 	@SubscribeEvent
-	private void common(FMLCommonSetupEvent event) {
+	public void common(FMLCommonSetupEvent event) {
 		CommonProxy.setup();
 	}
 	
 	@SubscribeEvent
-	private void client(final FMLClientSetupEvent event) {
+	public void client(final FMLClientSetupEvent event) {
 		ClientProxy.setup();
 	}
 	
--- src/main/java/info/u_team/u_team_core/intern/proxy/CommonProxy.java
+++ src/main/java/info/u_team/u_team_core/intern/proxy/CommonProxy.java
@@ -3,6 +3,7 @@ package info.u_team.u_team_core.intern.proxy;
 public class CommonProxy {
 	
 	public static void setup() {
+		System.out.println("------------------------------------------------------------");
 	}
 	
 }
