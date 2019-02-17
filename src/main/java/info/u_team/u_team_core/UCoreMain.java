7fc75708553dcc083ca538c63641c7652cd78fd5
HyCraftHD <hycrafthd@live.de>
2019-02-17 01:03:24 +0100

    Added registry util

--- src/main/java/info/u_team/u_team_core/UCoreMain.java
+++ src/main/java/info/u_team/u_team_core/UCoreMain.java
@@ -2,12 +2,9 @@ package info.u_team.u_team_core;
 
 import org.apache.logging.log4j.*;
 
-import info.u_team.u_team_core.intern.config.ClientConfig;
 import info.u_team.u_team_core.intern.proxy.*;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig.Type;
 import net.minecraftforge.fml.event.lifecycle.*;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -18,7 +15,7 @@ public class UCoreMain {
 	
 	public UCoreMain() {
 		FMLJavaModLoadingContext.get().getModEventBus().register(this);
-		ModLoadingContext.get().registerConfig(Type.CLIENT, ClientConfig.config);
+		CommonProxy.construct();
 	}
 	
 	@SubscribeEvent
--- src/main/java/info/u_team/u_team_core/intern/proxy/CommonProxy.java
+++ src/main/java/info/u_team/u_team_core/intern/proxy/CommonProxy.java
@@ -1,6 +1,14 @@
 package info.u_team.u_team_core.intern.proxy;
 
+import info.u_team.u_team_core.intern.config.ClientConfig;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.config.ModConfig.Type;
+
 public class CommonProxy {
+	
+	public static void construct() {
+		ModLoadingContext.get().registerConfig(Type.CLIENT, ClientConfig.config);
+	}
 	
 	public static void setup() {
 	}
