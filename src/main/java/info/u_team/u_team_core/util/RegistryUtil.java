3f3f9d526b71da46947f546f0dfc60f6af82a22f
HyCraftHD <hycrafthd@live.de>
2019-02-17 01:14:58 +0100

    Updated Registryutil to use lambda and streams

--- src/main/java/info/u_team/u_team_core/util/RegistryUtil.java
+++ src/main/java/info/u_team/u_team_core/util/RegistryUtil.java
@@ -1,7 +1,7 @@
 package info.u_team.u_team_core.util;
 
-import java.lang.reflect.Field;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import info.u_team.u_team_core.UCoreMain;
 import net.minecraftforge.registries.IForgeRegistryEntry;
@@ -10,17 +10,14 @@ public class RegistryUtil {
 	
 	@SuppressWarnings("unchecked")
 	public static <T extends IForgeRegistryEntry<T>> List<T> getRegistryEntries(Class<T> clazz, Class<?> init) {
-		List<T> list = new ArrayList<>();
-		try {
-			for (Field field : init.getDeclaredFields()) {
-				if (clazz.isAssignableFrom(field.getType())) {
-					list.add((T) field.get(null));
-				}
+		return Arrays.asList(init.getDeclaredFields()).stream().filter(field -> clazz.isAssignableFrom(field.getType())).map(field -> {
+			try {
+				return (T) field.get(null);
+			} catch (Exception ex) {
+				UCoreMain.logger.error("Failed fetching registry entries for class {}", init, ex);
 			}
-		} catch (Exception ex) {
-			UCoreMain.logger.error("Failed fetching registry entries for class {}", init, ex);
-		}
-		return list;
+			return null;
+		}).collect(Collectors.toList());
 	}
 	
 }
