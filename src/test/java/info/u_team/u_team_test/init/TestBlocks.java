d3fa22e5f8185f383052704710bbb41689513e4f
HyCraftHD <hycrafthd@live.de>
2019-02-22 14:30:22 +0100

    Updated testmod

--- src/test/java/info/u_team/u_team_test/init/TestBlocks.java
+++ src/test/java/info/u_team/u_team_test/init/TestBlocks.java
@@ -1,10 +1,8 @@
 package info.u_team.u_team_test.init;
 
 import info.u_team.u_team_core.registry.BlockRegistry;
-import info.u_team.u_team_core.util.RegistryUtil;
 import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.block.*;
-import net.minecraft.block.Block;
 
 public class TestBlocks {
 	
@@ -13,7 +11,7 @@ public class TestBlocks {
 	public static BlockTileEntity tileentity = new BlockTileEntity("tileentity");
 	
 	public static void construct() {
-		BlockRegistry.register(TestMod.modid, RegistryUtil.getRegistryEntries(Block.class, TestBlocks.class));
+		BlockRegistry.register(TestMod.modid, TestBlocks.class);
 	}
 	
 }
