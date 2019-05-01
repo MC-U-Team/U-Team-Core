8eabad77c996c30ed2ae920ddc65e96aa086467c
HyCraftHD <hycrafthd@live.de>
2019-05-01 17:50:33 +0200

    Added some new gui features (buttons and list)

---
+++ src/main/java/info/u_team/u_team_core/gui/elements/GuiButtonClick.java
@@ -0,0 +1,27 @@
+package info.u_team.u_team_core.gui.elements;
+
+import net.minecraftforge.fml.client.config.GuiButtonExt;
+
+public class GuiButtonClick extends GuiButtonExt {
+	
+	private Runnable runnable;
+	
+	public GuiButtonClick(int xPos, int yPos, String displayString) {
+		super(-1, xPos, yPos, displayString);
+	}
+	
+	public GuiButtonClick(int xPos, int yPos, int width, int height, String displayString) {
+		super(-1, xPos, yPos, width, height, displayString);
+	}
+	
+	public void setClickAction(Runnable runnable) {
+		this.runnable = runnable;
+	}
+	
+	@Override
+	public void onClick(double mouseX, double mouseY) {
+		if (runnable != null) {
+			runnable.run();
+		}
+	}
+}
