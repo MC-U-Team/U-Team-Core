e2d9a48e77fbbc20b132c89e7d96d06e7168774a
HyCraftHD <hycrafthd@live.de>
2019-06-10 15:45:38 +0200

    Fix raytrace method

--- src/main/java/info/u_team/u_team_core/util/world/WorldUtil.java
+++ src/main/java/info/u_team/u_team_core/util/world/WorldUtil.java
@@ -1,33 +1,27 @@
 package info.u_team.u_team_core.util.world;
 
-import java.util.function.Function;
+import java.util.function.Supplier;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.*;
-import net.minecraft.world.World;
-import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.util.math.RayTraceContext.*;
+import net.minecraft.world.ServerWorld;
 import net.minecraft.world.storage.WorldSavedData;
 
 public class WorldUtil {
 	
 	public static RayTraceResult rayTraceServerSide(PlayerEntity player, double range) {
-		return rayTraceServerSide(player, range, RayTraceFluidMode.NEVER, false, true);
+		return rayTraceServerSide(player, range, BlockMode.OUTLINE, FluidMode.NONE);
 	}
 	
-	public static RayTraceResult rayTraceServerSide(PlayerEntity player, double range, RayTraceFluidMode liquidMode, boolean ignoreBlockWithoutBoundingBox, boolean returnLastUncollidableBlock) {
-		Vec3d playerVector = new Vec3d(player.posX, player.posY + player.getEyeHeight(), player.posZ);
-		Vec3d lookVector = player.getLookVec();
-		Vec3d locationVector = playerVector.add(lookVector.x * range, lookVector.y * range, lookVector.z * range);
-		return player.world.rayTraceBlocks(playerVector, locationVector, liquidMode, ignoreBlockWithoutBoundingBox, returnLastUncollidableBlock);
+	public static RayTraceResult rayTraceServerSide(PlayerEntity player, double range, BlockMode blockMode, FluidMode fluidMode) {
+		final Vec3d playerVector = new Vec3d(player.posX, player.posY + player.getEyeHeight(), player.posZ);
+		final Vec3d lookVector = player.getLookVec();
+		final Vec3d locationVector = playerVector.add(lookVector.x * range, lookVector.y * range, lookVector.z * range);
+		return player.world.rayTraceBlocks(new RayTraceContext(playerVector, locationVector, blockMode, fluidMode, player));
 	}
 	
-	public static <T extends WorldSavedData> T getSaveData(World world, String name, Function<String, T> function) {
-		final DimensionType type = world.getDimension().getType();
-		T instance = world.getSavedData(type, function, name);
-		if (instance == null) {
-			instance = function.apply(name);
-			world.setSavedData(type, name, instance);
-		}
-		return instance;
+	public static <T extends WorldSavedData> T getSaveData(ServerWorld world, String name, Supplier<T> function) {
+		return world.getSavedData().getOrCreate(function, name);
 	}
 }
