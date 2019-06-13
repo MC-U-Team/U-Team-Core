7d4ef3e3527c6400b1d69cf79b2ee6f31077ed30
HyCraftHD <hycrafthd@live.de>
2019-06-13 23:36:13 +0200

    Remove test for BaseRegistryUtil

--- src/main/java/info/u_team/u_team_core/api/registry/IUArrayRegistryType.java
+++ src/main/java/info/u_team/u_team_core/api/registry/IUArrayRegistryType.java
@@ -1,18 +1,16 @@
 package info.u_team.u_team_core.api.registry;
 
 import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
-import net.minecraftforge.registries.*;
 
 /**
- * Implement this in your class which holds {@link IForgeRegistryEntry} entries so the {@link BaseRegistryUtil} can
- * register them all.
+ * Implement this in your class which holds entries so the {@link BaseRegistryUtil} can register them all.
  */
-public interface IUArrayRegistryType<T extends IForgeRegistryEntry<T>> {
+public interface IUArrayRegistryType<T> {
 	
 	/**
 	 * Must always returns the same registry type array copy. Because of mutability this array must be copied.
 	 * 
-	 * @return Array of {@link IForgeRegistryEntry}
+	 * @return Array of entries
 	 */
 	T[] getArray();
 	
