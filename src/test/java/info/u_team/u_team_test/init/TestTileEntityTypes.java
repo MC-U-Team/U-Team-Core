07d84c47da47c48a5f341b164824ea1a3540b302
HyCraftHD <hycrafthd@live.de>
2019-06-15 21:40:21 +0200

    Readded UTileEntityType

--- src/test/java/info/u_team/u_team_test/init/TestTileEntityTypes.java
+++ src/test/java/info/u_team/u_team_test/init/TestTileEntityTypes.java
@@ -1,14 +1,23 @@
 package info.u_team.u_team_test.init;
 
+import info.u_team.u_team_core.tileentitytype.UTileEntityType;
+import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
+import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.tileentity.TileEntityTileEntity;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraftforge.event.RegistryEvent.Register;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
+@EventBusSubscriber(modid = TestMod.modid, bus = Bus.MOD)
 public class TestTileEntityTypes {
 	
-	public static final TileEntityType<TileEntityTileEntity> tileentity = null;//TileEntityType.Builder.create("tileentity", TileEntityTileEntity::new, TestBlocks.tileentity).build();
+	public static final TileEntityType<TileEntityTileEntity> tileentity = UTileEntityType.UBuilder.create("tileentity", TileEntityTileEntity::new, TestBlocks.tileentity).build();
 	
-	public static void construct() {
-//		TileEntityTypeRegistry.register(TestMod.modid, TestTileEntityTypes.class);
+	@SubscribeEvent
+	public static void register(Register<TileEntityType<?>> event) {
+		BaseRegistryUtil.getAllGenericRegistryEntriesAndApplyNames(TestMod.modid, TileEntityType.class).forEach(event.getRegistry()::register);
 	}
 	
 }
