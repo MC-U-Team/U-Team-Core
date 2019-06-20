5d543a4244582fe70785b73c825499360652a63e
HyCraftHD <hycrafthd@live.de>
2019-06-20 12:23:36 +0200

    Updated changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,17 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## [1.14.2-2.5.1.112-SNAPSHOT] - 2019-06-20
+### Added 
+- IDyeableItem for easy usage with colored items
+- Custom dye recipe
+- Color util for wool blocks
+- New at lines for extending shaped recipe without need to copy many methods
+
+### Changed
+- Fixed server crash with containers
+- Apply convention to write static final fields in caps with underscores
+
 ## [1.14.2-2.5.0.111-SNAPSHOT] - 2019-06-18
 ### Changed
 - Updated to 1.14.2
--- src/test/java/info/u_team/u_team_test/init/TestDimensions.java
+++ src/test/java/info/u_team/u_team_test/init/TestDimensions.java
@@ -1,7 +1,6 @@
 package info.u_team.u_team_test.init;
 
 import info.u_team.u_team_test.TestMod;
-import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
 import net.minecraftforge.event.world.RegisterDimensionsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,12 +11,13 @@ import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerChangedDimensio
 @EventBusSubscriber(modid = TestMod.MODID, bus = Bus.FORGE)
 public class TestDimensions {
 	
-	@SuppressWarnings("deprecation")
 	@SubscribeEvent
 	public static void on(RegisterDimensionsEvent event) {
-		if (!DimensionManager.getRegistry().containsKey(TestModDimensions.BASIC.getRegistryName())) { // How do we know when the dimension needs to be registered??
-			DimensionManager.registerDimension(TestModDimensions.BASIC.getRegistryName(), TestModDimensions.BASIC, null, true);
-		}
+		// Dimension are broken currently
+		// if (!DimensionManager.getRegistry().containsKey(TestModDimensions.BASIC.getRegistryName())) { // How do we know when
+		// the dimension needs to be registered??
+		// DimensionManager.registerDimension(TestModDimensions.BASIC.getRegistryName(), TestModDimensions.BASIC, null, true);
+		// }
 	}
 	
 	@SubscribeEvent
