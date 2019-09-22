fbc07fff57dd22212a1f66d7e77c7873b52e99d7
HyCraftHD <hycrafthd@live.de>
2019-09-22 12:41:37 +0200

    Add ModelUtil that can change state container models

---
+++ src/main/java/info/u_team/u_team_core/util/ModelUtil.java
@@ -0,0 +1,29 @@
+package info.u_team.u_team_core.util;
+
+import static net.minecraft.client.renderer.model.ModelBakery.STATE_CONTAINER_OVERRIDES;
+
+import java.util.*;
+
+import com.google.common.collect.ImmutableMap;
+
+import net.minecraft.block.*;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.*;
+
+@OnlyIn(Dist.CLIENT)
+public class ModelUtil {
+	
+	static {
+		if (STATE_CONTAINER_OVERRIDES instanceof ImmutableMap) {
+			final Map<ResourceLocation, StateContainer<Block, BlockState>> mutableMap = new HashMap<>();
+			STATE_CONTAINER_OVERRIDES.forEach(mutableMap::put);
+			STATE_CONTAINER_OVERRIDES = mutableMap;
+		}
+	}
+	
+	public static void addCustomStateContainer(ResourceLocation location, StateContainer<Block, BlockState> container) {
+		STATE_CONTAINER_OVERRIDES.put(location, container);
+	}
+	
+}
