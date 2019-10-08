a8818d301f72feb8e5caa3b3da70ceb1c0c6ac05
HyCraftHD <hycrafthd@live.de>
2019-10-08 20:53:21 +0200

    Add commons recipe provider

---
+++ src/main/java/info/u_team/u_team_core/data/CommonRecipesProvider.java
@@ -0,0 +1,78 @@
+package info.u_team.u_team_core.data;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.function.Consumer;
+
+import info.u_team.u_team_core.util.TagUtil;
+import net.minecraft.advancements.criterion.*;
+import net.minecraft.advancements.criterion.MinMaxBounds.IntBound;
+import net.minecraft.data.*;
+import net.minecraft.item.Item;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.tags.Tag;
+import net.minecraft.util.*;
+
+public abstract class CommonRecipesProvider extends CommonProvider {
+	
+	public CommonRecipesProvider(DataGenerator generator) {
+		super("Recipes", generator);
+	}
+	
+	@Override
+	public void act(DirectoryCache cache) throws IOException {
+		addRecipes(recipe -> generateRecipe(cache, recipe, false));
+		addDefaultAdvancementsRecipes(recipe -> generateRecipe(cache, recipe, true));
+	}
+	
+	private void generateRecipe(DirectoryCache cache, IFinishedRecipe recipe, boolean vanillaAdvancement) {
+		try {
+			final ResourceLocation recipeID = recipe.getID();
+			write(cache, recipe.getRecipeJson(), path.resolve(recipeID.getNamespace()).resolve("recipes").resolve(recipe.getID().getPath() + ".json"));
+			if (recipe.getAdvancementJson() != null) {
+				final Path advancementPath;
+				if (vanillaAdvancement) {
+					final ResourceLocation advancementID = recipe.getAdvancementID();
+					advancementPath = path.resolve(advancementID.getNamespace()).resolve("advancements").resolve(advancementID.getPath() + ".json");
+				} else {
+					advancementPath = path.resolve(recipeID.getNamespace()).resolve("advancements").resolve("recipes").resolve(recipeID.getPath() + ".json");
+				}
+				write(cache, recipe.getAdvancementJson(), advancementPath);
+			}
+		} catch (IOException ex) {
+			LOGGER.error(marker, "Could not write data.", ex);
+		}
+	}
+	
+	protected abstract void addRecipes(Consumer<IFinishedRecipe> consumer);
+	
+	/**
+	 * Override this method if you want to add recipes that have the vanilla path for advancements
+	 * 
+	 * @param consumer
+	 */
+	protected void addDefaultAdvancementsRecipes(Consumer<IFinishedRecipe> consumer) {
+	}
+	
+	@Override
+	protected Path resolvePath(Path outputFolder) {
+		return outputFolder.resolve("data");
+	}
+	
+	protected InventoryChangeTrigger.Instance hasItem(Tag<Item> tag) {
+		return hasItem(ItemPredicate.Builder.create().tag(tag).build());
+	}
+	
+	protected InventoryChangeTrigger.Instance hasItem(IItemProvider item) {
+		return hasItem(ItemPredicate.Builder.create().item(item).build());
+	}
+	
+	protected InventoryChangeTrigger.Instance hasItem(ItemPredicate... predicates) {
+		return new InventoryChangeTrigger.Instance(IntBound.UNBOUNDED, IntBound.UNBOUNDED, IntBound.UNBOUNDED, predicates);
+	}
+	
+	public static Ingredient getIngredientOfTag(Tag<Item> tag) {
+		return TagUtil.getSerializableIngredientOfTag(tag);
+	}
+	
+}
--- src/main/java/info/u_team/u_team_core/util/TagUtil.java
+++ src/main/java/info/u_team/u_team_core/util/TagUtil.java
@@ -1,7 +1,12 @@
 package info.u_team.u_team_core.util;
 
+import java.util.*;
+import java.util.stream.Stream;
+
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.crafting.Ingredient.TagList;
 import net.minecraft.tags.*;
 import net.minecraft.util.ResourceLocation;
 
@@ -21,5 +26,15 @@ public class TagUtil {
 	
 	public static Tag<Item> fromBlockTag(Tag<Block> block) {
 		return new ItemTags.Wrapper(block.getId());
+	}
+	
+	public static Ingredient getSerializableIngredientOfTag(Tag<Item> tag) {
+		return Ingredient.fromItemListStream(Stream.of(new TagList(tag) {
+			
+			@Override
+			public Collection<ItemStack> getStacks() {
+				return Arrays.asList(new ItemStack(Items.ACACIA_BOAT)); // Return default value, so the ingredient will serialize our tag.
+			}
+		}));
 	}
 }
