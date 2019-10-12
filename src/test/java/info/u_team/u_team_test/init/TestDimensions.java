832a929ac671b4a97c1f1fb5d6fe2935aea3908b
HyCraftHD <hycrafthd@live.de>
2019-10-12 02:28:04 +0200

    Reenable dimension registry as you now can teleport with the usage of uteamcore commands

--- src/test/java/info/u_team/u_team_test/init/TestDimensions.java
+++ src/test/java/info/u_team/u_team_test/init/TestDimensions.java
@@ -1,8 +1,7 @@
 package info.u_team.u_team_test.init;
 
 import info.u_team.u_team_test.TestMod;
-import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent.PlayerChangedDimensionEvent;
+import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.event.world.RegisterDimensionsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -11,23 +10,11 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 @EventBusSubscriber(modid = TestMod.MODID, bus = Bus.FORGE)
 public class TestDimensions {
 	
+	@SuppressWarnings("deprecation")
 	@SubscribeEvent
 	public static void on(RegisterDimensionsEvent event) {
-		// Dimension are broken currently
-		// if (!DimensionManager.getRegistry().containsKey(TestModDimensions.BASIC.getRegistryName())) { // How do we know when
-		// the dimension needs to be registered??
-		// DimensionManager.registerDimension(TestModDimensions.BASIC.getRegistryName(), TestModDimensions.BASIC, null, true);
-		// }
+		if (!DimensionManager.getRegistry().containsKey(TestModDimensions.BASIC.getRegistryName())) { // How do we know when the dimension needs to be registered??
+			DimensionManager.registerDimension(TestModDimensions.BASIC.getRegistryName(), TestModDimensions.BASIC, null, true);
+		}
 	}
-	
-	@SubscribeEvent
-	public static void on(EntityTravelToDimensionEvent event) {
-		System.out.println("START TRAVELING TO " + event.getDimension() + " AS " + event.getEntity());
-	}
-	
-	@SubscribeEvent
-	public static void on(PlayerChangedDimensionEvent event) {
-		System.out.println("FINISHED TRAVELING TO: " + event.getTo() + " FROM " + event.getFrom() + " AS " + event.getPlayer());
-	}
-	
 }
