1f35a117ffdbd99a44df2717ea35dc8068e0493c
HyCraftHD <hycrafthd@live.de>
2019-10-12 14:20:31 +0200

    Add negative predicate for easy lambda method references

---
+++ src/main/java/info/u_team/u_team_core/util/Predicates.java
@@ -0,0 +1,11 @@
+package info.u_team.u_team_core.util;
+
+import java.util.function.Predicate;
+
+public class Predicates {
+	
+	public static <T> Predicate<T> not(Predicate<T> predicate) {
+		return predicate.negate();
+	}
+	
+}
