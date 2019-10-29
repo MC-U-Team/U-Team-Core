f65b6c42d9d4b85f2e95f07bbcf3bccaf1386fb8
HyCraftHD <hycrafthd@live.de>
2019-10-29 14:43:54 +0100

    Fix server crash in test mod

--- src/test/java/info/u_team/u_team_test/init/TestDimensions.java
+++ src/test/java/info/u_team/u_team_test/init/TestDimensions.java
@@ -13,7 +13,7 @@ public class TestDimensions {
 	@SuppressWarnings("deprecation")
 	@SubscribeEvent
 	public static void on(RegisterDimensionsEvent event) {
-		if (!DimensionManager.getRegistry().containsKey(TestModDimensions.BASIC.getRegistryName())) { // How do we know when the dimension needs to be registered??
+		if (!DimensionManager.getRegistry().keySet().contains(TestModDimensions.BASIC.getRegistryName())) { // How do we know when the dimension needs to be registered??
 			DimensionManager.registerDimension(TestModDimensions.BASIC.getRegistryName(), TestModDimensions.BASIC, null, true);
 		}
 	}
