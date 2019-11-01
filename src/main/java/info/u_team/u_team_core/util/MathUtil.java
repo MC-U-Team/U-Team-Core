bcf0419a924c72193b7a86d9001212778785d445
HyCraftHD <hycrafthd@live.de>
2019-11-01 17:48:26 +0100

    Add random methods in math util again

--- src/main/java/info/u_team/u_team_core/util/MathUtil.java
+++ src/main/java/info/u_team/u_team_core/util/MathUtil.java
@@ -1,8 +1,12 @@
 package info.u_team.u_team_core.util;
 
+import java.util.Random;
+
 import net.minecraft.util.math.Vec3d;
 
 public class MathUtil {
+	
+	public static final Random RANDOM = new Random();
 	
 	public static Vec3d rotateVectorAroundYCC(Vec3d vec, double angle) {
 		return rotateVectorCC(vec, new Vec3d(0, 1, 0), angle);
@@ -21,6 +25,30 @@ public class MathUtil {
 		final double rotationY = v * (u * x + v * y + w * z) * (1 - Math.cos(angle)) + y * Math.cos(angle) + (w * x - u * z) * Math.sin(angle);
 		final double rotationZ = w * (u * x + v * y + w * z) * (1 - Math.cos(angle)) + z * Math.cos(angle) + (-v * x + u * y) * Math.sin(angle);
 		return new Vec3d(rotationX, rotationY, rotationZ);
+	}
+	
+	public static int getRandomNumberInRange(int min, int max) {
+		return getRandomNumberInRange(RANDOM, min, max);
+	}
+	
+	public static int getRandomNumberInRange(Random random, int min, int max) {
+		return random.nextInt(max - min + 1) + min;
+	}
+	
+	public static float getRandomNumberInRange(float min, float max) {
+		return getRandomNumberInRange(RANDOM, min, max);
+	}
+	
+	public static float getRandomNumberInRange(Random random, float min, float max) {
+		return random.nextFloat() * (max - min) + min;
+	}
+	
+	public static double getRandomNumberInRange(double min, double max) {
+		return getRandomNumberInRange(RANDOM, min, max);
+	}
+	
+	public static double getRandomNumberInRange(Random random, double min, double max) {
+		return random.nextDouble() * (max - min) + min;
 	}
 	
 }
