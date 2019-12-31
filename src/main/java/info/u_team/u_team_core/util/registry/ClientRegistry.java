a6fe52278d4ff6c35e4775e9bf9cbdcef97a613d
HyCraftHD <hycrafthd@live.de>
2019-12-31 14:40:49 +0100

    Fix changes in the registry of tile entity renderer in the latest forge updates (30.0.19)

--- src/main/java/info/u_team/u_team_core/util/registry/ClientRegistry.java
+++ src/main/java/info/u_team/u_team_core/util/registry/ClientRegistry.java
@@ -1,6 +1,8 @@
 package info.u_team.u_team_core.util.registry;
 
-import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
+import java.util.function.Function;
+
+import net.minecraft.client.renderer.tileentity.*;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.*;
 import net.minecraft.tileentity.*;
@@ -15,7 +17,11 @@ public class ClientRegistry {
 	}
 	
 	public static <T extends TileEntity> void registerSpecialTileEntityRenderer(TileEntityType<T> clazz, TileEntityRenderer<? super T> renderer) {
-		net.minecraftforge.fml.client.registry.ClientRegistry.bindTileEntityRenderer(clazz, renderer);
+		registerSpecialTileEntityRenderer(clazz, dispatcher -> renderer);
+	}
+	
+	public static <T extends TileEntity> void registerSpecialTileEntityRenderer(TileEntityType<T> clazz, Function<? super TileEntityRendererDispatcher, ? extends TileEntityRenderer<? super T>> rendererFactory) {
+		net.minecraftforge.fml.client.registry.ClientRegistry.bindTileEntityRenderer(clazz, rendererFactory);
 	}
 	
 	public static void registerKeybinding(KeyBinding key) {
