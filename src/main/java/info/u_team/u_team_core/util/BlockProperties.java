3c5a85a4a16e530ade56a7110953c525a11af85d
HyCraftHD <hycrafthd@live.de>
2020-01-22 23:45:30 +0100

    Start to fix block properties add more to accesstransformer

--- src/main/java/info/u_team/u_team_core/util/BlockProperties.java
+++ src/main/java/info/u_team/u_team_core/util/BlockProperties.java
@@ -24,6 +24,7 @@ public class BlockProperties extends Properties {
 		slipperiness = properties.slipperiness;
 		lootTable = properties.lootTable;
 		variableOpacity = properties.variableOpacity;
+		
 		setValueHarvestLevel(getValueHarvestLevel(properties));
 		setValueHarvestTool(getValueHarvestTool(properties));
 	}
