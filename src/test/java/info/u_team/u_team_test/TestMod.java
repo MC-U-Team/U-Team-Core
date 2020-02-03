5a0ded797e56dc081a4ed73202cf327505a87a14
HyCraftHD <hycrafthd@live.de>
2020-02-03 04:27:10 +0100

    Check signed jar on testmod

--- src/test/java/info/u_team/u_team_test/TestMod.java
+++ src/test/java/info/u_team/u_team_test/TestMod.java
@@ -1,5 +1,6 @@
 package info.u_team.u_team_test;
 
+import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(TestMod.MODID)
@@ -8,6 +9,7 @@ public class TestMod {
 	public static final String MODID = "uteamtest";
 	
 	public TestMod() {
+		JarSignVerifier.checkSigned(MODID);
 		System.out.println("--------------------------------------- LOADING TEST MOD ---------------------------------------");
 	}
 }
