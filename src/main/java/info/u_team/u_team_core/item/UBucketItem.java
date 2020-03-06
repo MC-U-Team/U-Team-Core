9e785ac35e0fcb8562668a8d32b752b8ba7fee8c
HyCraftHD <hycrafthd@live.de>
2020-03-06 03:23:05 +0100

    Add a ctor with the itemgroup

--- src/main/java/info/u_team/u_team_core/item/UBucketItem.java
+++ src/main/java/info/u_team/u_team_core/item/UBucketItem.java
@@ -4,14 +4,18 @@ import java.util.function.Supplier;
 
 import info.u_team.u_team_core.api.registry.IURegistryType;
 import net.minecraft.fluid.Fluid;
-import net.minecraft.item.BucketItem;
+import net.minecraft.item.*;
 
 public class UBucketItem extends BucketItem implements IURegistryType {
 	
 	protected final String name;
 	
-	public UBucketItem(String name, Properties builder, Supplier<? extends Fluid> supplier) {
-		super(supplier, builder);
+	public UBucketItem(String name, Properties properties, Supplier<? extends Fluid> fluid) {
+		this(name, null, properties, fluid);
+	}
+	
+	public UBucketItem(String name, ItemGroup group, Properties properties, Supplier<? extends Fluid> fluid) {
+		super(fluid, group == null ? properties : properties.group(group));
 		this.name = name;
 	}
 	
