d1a9e7f51e3af7d5ff67d7fc5919e4e2ef71e440
HyCraftHD <hycrafthd@live.de>
2020-05-19 01:54:54 +0200

    Add TileEntityUFluidStackHandler to auto mark dirty

---
+++ src/main/java/info/u_team/u_team_core/inventory/TileEntityUFluidStackHandler.java
@@ -0,0 +1,24 @@
+package info.u_team.u_team_core.inventory;
+
+import net.minecraft.tileentity.TileEntity;
+
+public class TileEntityUFluidStackHandler extends UFluidStackHandler {
+	
+	protected final TileEntity tileEntity;
+	
+	public TileEntityUFluidStackHandler(int size, int capacity, TileEntity tileEntity) {
+		super(size, capacity);
+		this.tileEntity = tileEntity;
+	}
+	
+	public TileEntityUFluidStackHandler(int size, TileEntity tileEntity) {
+		super(size);
+		this.tileEntity = tileEntity;
+	}
+	
+	@Override
+	protected void onContentsChanged(int tank) {
+		tileEntity.markDirty();
+	}
+	
+}
