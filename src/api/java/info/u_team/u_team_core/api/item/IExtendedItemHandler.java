9fa863e7324b34507bacee80bf829dceb20d38aa
HyCraftHD <hycrafthd@live.de>
2020-05-18 16:01:07 +0200

    Add IExtendedItemHandler

---
+++ src/api/java/info/u_team/u_team_core/api/item/IExtendedItemHandler.java
@@ -0,0 +1,17 @@
+package info.u_team.u_team_core.api.item;
+
+import info.u_team.u_team_core.api.InteractionType;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.items.IItemHandlerModifiable;
+
+public interface IExtendedItemHandler extends IItemHandlerModifiable {
+	
+	default ItemStack insertItem(int slot, ItemStack stack, InteractionType action) {
+		return insertItem(slot, stack, action.isSimulate());
+	}
+	
+	default ItemStack extractItem(int slot, int amount, InteractionType action) {
+		return extractItem(slot, amount, action.isSimulate());
+	}
+	
+}
