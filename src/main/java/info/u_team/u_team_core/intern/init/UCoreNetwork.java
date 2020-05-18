30ed44172f8ecf715f916d6fe1b9ab7f2f8fae7d
HyCraftHD <hycrafthd@live.de>
2020-05-18 16:28:04 +0200

    Increase network version

--- src/main/java/info/u_team/u_team_core/intern/init/UCoreNetwork.java
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreNetwork.java
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 public class UCoreNetwork {
 	
-	public static final String PROTOCOL = "1.15.2-1";
+	public static final String PROTOCOL = "1.15.2-2";
 	
 	public static final SimpleChannel NETWORK = NetworkRegistry.newSimpleChannel(new ResourceLocation(UCoreMain.MODID, "network"), () -> PROTOCOL, PROTOCOL::equals, PROTOCOL::equals);
 	
