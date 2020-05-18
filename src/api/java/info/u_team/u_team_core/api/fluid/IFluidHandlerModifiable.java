83dd8d05b47855f6c9fefc0036c40472b237a62a
HyCraftHD <hycrafthd@live.de>
2020-05-18 14:19:04 +0200

    Add interface for modifiable fluid handlers with setFluidInTank method

---
+++ src/api/java/info/u_team/u_team_core/api/fluid/IFluidHandlerModifiable.java
@@ -0,0 +1,10 @@
+package info.u_team.u_team_core.api.fluid;
+
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+
+public interface IFluidHandlerModifiable extends IFluidHandler {
+	
+	void setFluidInTank(int tank, FluidStack stack);
+	
+}
