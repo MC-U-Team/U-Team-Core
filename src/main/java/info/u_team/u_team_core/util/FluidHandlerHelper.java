40a8bf2c3a14c0ca1890b798366b2711d407021e
HyCraftHD <hycrafthd@live.de>
2020-05-18 16:12:58 +0200

    Add fluid handler helper

---
+++ src/main/java/info/u_team/u_team_core/util/FluidHandlerHelper.java
@@ -0,0 +1,24 @@
+package info.u_team.u_team_core.util;
+
+import net.minecraftforge.fluids.FluidStack;
+
+public class FluidHandlerHelper {
+	
+	public static boolean canFluidStacksStack(FluidStack a, FluidStack b) {
+		if (a.isEmpty() || !(a.getFluid() == b.getFluid()) || a.hasTag() != b.hasTag()) {
+			return false;
+		}
+		
+		return (!a.hasTag() || a.getTag().equals(b.getTag()));
+	}
+	
+	public static FluidStack copyStackWithSize(FluidStack stack, int size) {
+		if (size == 0) {
+			return FluidStack.EMPTY;
+		}
+		final FluidStack copy = stack.copy();
+		copy.setAmount(size);
+		return copy;
+	}
+	
+}
