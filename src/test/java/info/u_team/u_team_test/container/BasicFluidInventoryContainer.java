1c258fad888c0cfd42400903294fdb967b3d8922
HyCraftHD <hycrafthd@live.de>
2020-05-20 11:59:40 +0200

    Add basic fluid inventory container

---
+++ src/test/java/info/u_team/u_team_test/container/BasicFluidInventoryContainer.java
@@ -0,0 +1,26 @@
+package info.u_team.u_team_test.container;
+
+import info.u_team.u_team_core.container.UTileEntityContainer;
+import info.u_team.u_team_test.init.TestContainers;
+import info.u_team.u_team_test.tileentity.BasicFluidInventoryTileEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.network.PacketBuffer;
+
+public class BasicFluidInventoryContainer extends UTileEntityContainer<BasicFluidInventoryTileEntity> {
+	
+	// Client
+	public BasicFluidInventoryContainer(int id, PlayerInventory playerInventory, PacketBuffer buffer) {
+		super(TestContainers.BASIC_FLUID_INVENTORY, id, playerInventory, buffer);
+	}
+	
+	// Server
+	public BasicFluidInventoryContainer(int id, PlayerInventory playerInventory, BasicFluidInventoryTileEntity tileEntity) {
+		super(TestContainers.BASIC_FLUID_INVENTORY, id, playerInventory, tileEntity);
+	}
+	
+	@Override
+	protected void init(boolean server) {
+		
+	}
+	
+}
