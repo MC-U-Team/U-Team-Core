0feced1d254e5555d5fb12ffa1b1565561fc26e7
HyCraftHD <hycrafthd@live.de>
2020-05-21 15:17:38 +0200

    Generate random fluids in the fluid tank

--- src/test/java/info/u_team/u_team_test/tileentity/BasicFluidInventoryTileEntity.java
+++ src/test/java/info/u_team/u_team_test/tileentity/BasicFluidInventoryTileEntity.java
@@ -1,5 +1,7 @@
 package info.u_team.u_team_test.tileentity;
 
+import java.util.*;
+
 import info.u_team.u_team_core.inventory.*;
 import info.u_team.u_team_core.tileentity.UTickableTileEntity;
 import info.u_team.u_team_test.container.BasicFluidInventoryContainer;
@@ -11,8 +13,10 @@ import net.minecraft.util.Direction;
 import net.minecraft.util.text.*;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class BasicFluidInventoryTileEntity extends UTickableTileEntity implements INamedContainerProvider {
 	
@@ -83,6 +87,38 @@ public class BasicFluidInventoryTileEntity extends UTickableTileEntity implement
 	@Override
 	public ITextComponent getDisplayName() {
 		return new StringTextComponent("Fluid Inventory");
+	}
+	
+	// DEBUG
+	int i = 0;
+	
+	@Override
+	protected void tickServer() {
+		super.tickServer();
+		if (i % 50 == 0) {
+			fluidTanks.setFluidInTank(0, new FluidStack(choice(ForgeRegistries.FLUIDS.getValues(), new Random()), 1000));
+			fluidTanks.setFluidInTank(1, new FluidStack(choice(ForgeRegistries.FLUIDS.getValues(), new Random()), 1000));
+			fluidTanks.setFluidInTank(2, new FluidStack(choice(ForgeRegistries.FLUIDS.getValues(), new Random()), 1000));
+			fluidTanks.setFluidInTank(3, new FluidStack(choice(ForgeRegistries.FLUIDS.getValues(), new Random()), 1000));
+		}
+		i++;
+	}
+	
+	public static <E> E choice(Collection<? extends E> coll, Random rand) {
+		if (coll.size() == 0) {
+			return null;
+		}
+		
+		int index = rand.nextInt(coll.size());
+		if (coll instanceof List) {
+			return ((List<? extends E>) coll).get(index);
+		} else {
+			Iterator<? extends E> iter = coll.iterator();
+			for (int i = 0; i < index; i++) {
+				iter.next();
+			}
+			return iter.next();
+		}
 	}
 	
 }
