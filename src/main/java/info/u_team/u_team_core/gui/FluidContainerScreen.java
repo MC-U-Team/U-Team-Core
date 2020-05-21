6a6e5c21d601692dfba01e4f59ac5fa46c99abe0
HyCraftHD <hycrafthd@live.de>
2020-05-21 18:47:33 +0200

    Draw only on fluid stack for testing

--- src/main/java/info/u_team/u_team_core/gui/FluidContainerScreen.java
+++ src/main/java/info/u_team/u_team_core/gui/FluidContainerScreen.java
@@ -6,6 +6,7 @@ import info.u_team.u_team_core.container.*;
 import info.u_team.u_team_core.gui.render.FluidInventoryRender;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.api.distmarker.*;
@@ -25,9 +26,17 @@ public abstract class FluidContainerScreen<T extends Container> extends Containe
 		if (container instanceof FluidContainer) {
 			final FluidContainer fluidContainer = (FluidContainer) container;
 			for (int index = 0; index < fluidContainer.fluidSlots.size(); index++) {
-				drawFluidSlot(fluidContainer.fluidSlots.get(index));
+				// drawFluidSlot(fluidContainer.fluidSlots.get(index));
 			}
 		}
+		
+//		RenderSystem.enableBlend();
+//		RenderSystem.enableAlphaTest();
+		
+		FLUID_RENDERER.drawFluid(8, 18, new FluidStack(Fluids.WATER, 10));
+		
+//		RenderSystem.disableAlphaTest();
+//		RenderSystem.disableBlend();
 	}
 	
 	protected void drawFluidSlot(FluidSlot slot) {
