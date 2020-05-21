5aa127d04b60cfe3888bad94bf571292ba65f672
HyCraftHD <hycrafthd@live.de>
2020-05-22 00:26:23 +0200

    Add new message for clicking on a fluid slot

---
+++ src/main/java/info/u_team/u_team_core/intern/network/FluidClickContainerMessage.java
@@ -0,0 +1,56 @@
+package info.u_team.u_team_core.intern.network;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import info.u_team.u_team_core.container.FluidContainer;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.fml.network.NetworkEvent.Context;
+
+public class FluidClickContainerMessage {
+	
+	private final int id;
+	private final int slot;
+	private final ItemStack stack;
+	
+	public FluidClickContainerMessage(int id, int slot, ItemStack stack) {
+		this.id = id;
+		this.slot = slot;
+		this.stack = stack;
+	}
+	
+	public static void encode(FluidClickContainerMessage message, PacketBuffer sendBuffer) {
+		sendBuffer.writeByte(message.id);
+		sendBuffer.writeShort(message.slot);
+		sendBuffer.writeItemStack(message.stack, false);
+	}
+	
+	public static FluidClickContainerMessage decode(PacketBuffer sendBuffer) {
+		final int id = sendBuffer.readByte();
+		final int slot = sendBuffer.readShort();
+		final ItemStack stack = sendBuffer.readItemStack();
+		
+		return new FluidClickContainerMessage(id, slot, stack);
+	}
+	
+	public static class Handler {
+		
+		public static void handle(FluidClickContainerMessage message, Supplier<Context> contextSupplier) {
+			final Context context = contextSupplier.get();
+			context.enqueueWork(() -> {
+				getFluidContainer(context.getSender().openContainer, message.id).ifPresent(container -> container.fluidSlotClick(message.slot, message.stack));
+			});
+			context.setPacketHandled(true);
+		}
+		
+		private static final Optional<FluidContainer> getFluidContainer(Container container, int id) {
+			if (container instanceof FluidContainer && container.windowId == id) {
+				return Optional.of((FluidContainer) container);
+			}
+			return Optional.empty();
+		}
+	}
+	
+}
