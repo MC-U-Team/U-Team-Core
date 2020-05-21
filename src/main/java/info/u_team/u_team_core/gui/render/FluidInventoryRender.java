9b41697baca2cabb07bb8673c321a9bbb1d1809c
HyCraftHD <hycrafthd@live.de>
2020-05-21 17:39:39 +0200

    Add fluid inventory render class

---
+++ src/main/java/info/u_team/u_team_core/gui/render/FluidInventoryRender.java
@@ -0,0 +1,13 @@
+package info.u_team.u_team_core.gui.render;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.inventory.container.PlayerContainer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.FluidStack;
+
+public class FluidInventoryRender {
+	
+	private static final ResourceLocation ATLAS = PlayerContainer.LOCATION_BLOCKS_TEXTURE;
+	
+}
