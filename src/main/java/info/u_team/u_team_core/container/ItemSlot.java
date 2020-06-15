f048d7d497c8d085dfb1b3dd0a536be0a0aba3ab
HyCraftHD <hycrafthd@live.de>
2020-06-15 15:09:19 +0200

    Add item slot with the IItemHandler that calls the onSlotChanged method

---
+++ src/main/java/info/u_team/u_team_core/container/ItemSlot.java
@@ -0,0 +1,24 @@
+package info.u_team.u_team_core.container;
+
+import info.u_team.u_team_core.inventory.UItemStackHandler;
+import net.minecraftforge.items.*;
+
+public class ItemSlot extends SlotItemHandler {
+	
+	private final IItemHandler itemHandler;
+	private final int index;
+	
+	public ItemSlot(IItemHandler itemHandler, int index, int x, int y) {
+		super(itemHandler, index, x, y);
+		this.itemHandler = itemHandler;
+		this.index = index;
+	}
+	
+	@Override
+	public void onSlotChanged() {
+		if (itemHandler instanceof UItemStackHandler) {
+			((UItemStackHandler) itemHandler).onContentsChanged(index);
+		}
+	}
+	
+}
