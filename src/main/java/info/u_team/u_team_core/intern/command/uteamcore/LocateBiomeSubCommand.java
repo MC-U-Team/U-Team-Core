97f45261f35e7acb4b2d5987b15353952d63aedd
HyCraftHD <hycrafthd@live.de>
2020-06-16 02:24:19 +0200

    Add basic location biome command

---
+++ src/main/java/info/u_team/u_team_core/intern/command/uteamcore/LocateBiomeSubCommand.java
@@ -0,0 +1,37 @@
+package info.u_team.u_team_core.intern.command.uteamcore;
+
+import java.util.*;
+
+import com.mojang.brigadier.builder.ArgumentBuilder;
+
+import net.minecraft.command.*;
+import net.minecraft.command.arguments.ResourceLocationArgument;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.biome.provider.BiomeProvider;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class LocateBiomeSubCommand {
+	
+	public static ArgumentBuilder<CommandSource, ?> register() {
+		return Commands.literal("locatebiome") //
+				.requires(source -> source.hasPermissionLevel(2)) //
+				.then(Commands.argument("biome", ResourceLocationArgument.resourceLocation()) //
+						.suggests((context, builder) -> ISuggestionProvider.suggest(ForgeRegistries.BIOMES.getKeys().stream().map(ResourceLocation::toString), builder)) //
+						.executes(context -> locateBiome(context.getSource(), context.getArgument("biome", ResourceLocation.class))));
+	}
+	
+	private static int locateBiome(CommandSource source, ResourceLocation biome) {
+		final ServerWorld world = source.getWorld();
+		final BiomeProvider biomeProvider = world.getChunkProvider().getChunkGenerator().getBiomeProvider();
+		final Random random = new Random(world.getSeed());
+
+		final BlockPos pos = biomeProvider.func_225531_a_(0, world.getSeaLevel(), 0, 256, Arrays.asList(ForgeRegistries.BIOMES.getValue(biome)), random);
+
+		
+		
+		return 0;
+	}
+	
+}
