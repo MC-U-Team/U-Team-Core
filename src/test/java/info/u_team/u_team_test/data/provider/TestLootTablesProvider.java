814ccafee8343b1904f386e6e41a5dea5a134ad7
HyCraftHD <hycrafthd@live.de>
2020-06-19 13:00:38 +0200

    Register basic tile entity with tile entity loot table

--- src/test/java/info/u_team/u_team_test/data/provider/TestLootTablesProvider.java
+++ src/test/java/info/u_team/u_team_test/data/provider/TestLootTablesProvider.java
@@ -3,17 +3,19 @@ package info.u_team.u_team_test.data.provider;
 import java.util.function.BiConsumer;
 
 import info.u_team.u_team_core.data.*;
+import info.u_team.u_team_test.init.TestBlocks;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.storage.loot.LootTable;
 
 public class TestLootTablesProvider extends CommonLootTablesProvider {
-
+	
 	public TestLootTablesProvider(GenerationData data) {
 		super(data);
 	}
-
+	
 	@Override
 	protected void registerLootTables(BiConsumer<ResourceLocation, LootTable> consumer) {
+		registerBlock(TestBlocks.BASIC_TILEENTITY, addTileEntityBlockLootTable(TestBlocks.BASIC_TILEENTITY), consumer);
 	}
 	
 }
