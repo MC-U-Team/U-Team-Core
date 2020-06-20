4dae9b5ad7546e13939ef3e0e4cc2bd65b456794
HyCraftHD <hycrafthd@live.de>
2020-06-20 17:53:58 +0200

    Remove unused import

--- src/test/java/info/u_team/u_team_test/enchantment/AutoSmeltEnchantment.java
+++ src/test/java/info/u_team/u_team_test/enchantment/AutoSmeltEnchantment.java
@@ -3,7 +3,6 @@ package info.u_team.u_team_test.enchantment;
 import info.u_team.u_team_core.enchantment.UEnchantment;
 import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraftforge.common.MinecraftForge;
 
 public class AutoSmeltEnchantment extends UEnchantment {
 	
