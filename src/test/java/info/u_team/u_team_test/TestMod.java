318939f02775e6764ad7e4576635d247575c168b
HyCraftHD <hycrafthd@live.de>
2020-06-22 02:59:53 +0200

    Register deferred registry

--- src/test/java/info/u_team/u_team_test/TestMod.java
+++ src/test/java/info/u_team/u_team_test/TestMod.java
@@ -1,7 +1,9 @@
 package info.u_team.u_team_test;
 
 import info.u_team.u_team_core.util.verify.JarSignVerifier;
+import info.u_team.u_team_test.init.TestBiomes;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(TestMod.MODID)
 public class TestMod {
@@ -11,5 +13,6 @@ public class TestMod {
 	public TestMod() {
 		JarSignVerifier.checkSigned(MODID);
 		System.out.println("--------------------------------------- LOADING TEST MOD ---------------------------------------");
+		TestBiomes.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
 	}
 }
