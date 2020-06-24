53eac56448920bad837198bea9aa275195c601e9
HyCraftHD <hycrafthd@live.de>
2020-06-25 01:02:31 +0200

    Remove extends ubiome. Use the normal biome now

--- src/test/java/info/u_team/u_team_test/biome/BasicBiome.java
+++ src/test/java/info/u_team/u_team_test/biome/BasicBiome.java
@@ -1,6 +1,7 @@
 package info.u_team.u_team_test.biome;
 
-import info.u_team.u_team_core.biome.UBiome;
+import net.minecraft.world.biome.Biome;
+
 import info.u_team.u_team_test.init.TestBlocks;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.*;
@@ -9,12 +10,12 @@ import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.structure.VillageConfig;
 import net.minecraft.world.gen.surfacebuilders.*;
 
-public class BasicBiome extends UBiome {
+public class BasicBiome extends Biome {
 	
 	public static final SurfaceBuilderConfig BASIC_BLOCK_SURVACE = new SurfaceBuilderConfig(TestBlocks.BASIC.getDefaultState(), TestBlocks.BASIC.getDefaultState(), TestBlocks.BASIC.getDefaultState());
 	
-	public BasicBiome(String name) {
-		super(name, new Builder().surfaceBuilder(SurfaceBuilder.SHATTERED_SAVANNA, new SurfaceBuilderConfig(TestBlocks.BASIC.getDefaultState(), Blocks.END_STONE.getDefaultState(), TestBlocks.BASIC.getDefaultState())).precipitation(RainType.RAIN).category(Category.PLAINS).depth(0.125F).scale(0.05F).temperature(1.0F).downfall(1.0F).waterColor(4159204).waterFogColor(329011).parent((String) null));
+	public BasicBiome() {
+		super(new Builder().surfaceBuilder(SurfaceBuilder.SHATTERED_SAVANNA, new SurfaceBuilderConfig(TestBlocks.BASIC.getDefaultState(), Blocks.END_STONE.getDefaultState(), TestBlocks.BASIC.getDefaultState())).precipitation(RainType.RAIN).category(Category.PLAINS).depth(0.125F).scale(0.05F).temperature(1.0F).downfall(1.0F).waterColor(4159204).waterFogColor(329011).parent((String) null));
 		this.addStructure(Feature.VILLAGE.withConfiguration(new VillageConfig("village/desert/town_centers", 6)));
 		DefaultBiomeFeatures.addCarvers(this);
 		DefaultBiomeFeatures.addStructures(this);
