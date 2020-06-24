459fdfdd71becd4f1ec0f28ee242f9a09304f3b9
HyCraftHD <hycrafthd@live.de>
2020-06-25 01:20:37 +0200

    Add deferred registry to the test effects class

--- src/test/java/info/u_team/u_team_test/init/TestEffects.java
+++ src/test/java/info/u_team/u_team_test/init/TestEffects.java
@@ -1,22 +1,22 @@
 package info.u_team.u_team_test.init;
 
-import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
 import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.effect.RadiationEffect;
 import net.minecraft.potion.Effect;
-import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.registries.*;
 
 @EventBusSubscriber(modid = TestMod.MODID, bus = Bus.MOD)
 public class TestEffects {
 	
+	public static final DeferredRegister<Effect> EFFECTS = DeferredRegister.create(ForgeRegistries.POTIONS, TestMod.MODID);
+	
 	public static final Effect RADIATION = new RadiationEffect("radiation");
 	
-	@SubscribeEvent
-	public static void register(Register<Effect> event) {
-		BaseRegistryUtil.getAllRegistryEntriesAndApplyNames(TestMod.MODID, Effect.class).forEach(event.getRegistry()::register);
+	public static void register(IEventBus bus) {
+		EFFECTS.register(bus);
 	}
 	
 }
