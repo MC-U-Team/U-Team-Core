01da8c7dcd59525dadbc36ab1622e7ebc52eaabf
HyCraftHD <hycrafthd@live.de>
2020-06-25 01:21:53 +0200

    Register effect

--- src/test/java/info/u_team/u_team_test/init/TestEffects.java
+++ src/test/java/info/u_team/u_team_test/init/TestEffects.java
@@ -4,6 +4,7 @@ import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.effect.RadiationEffect;
 import net.minecraft.potion.Effect;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.*;
@@ -13,7 +14,7 @@ public class TestEffects {
 	
 	public static final DeferredRegister<Effect> EFFECTS = DeferredRegister.create(ForgeRegistries.POTIONS, TestMod.MODID);
 	
-	public static final Effect RADIATION = new RadiationEffect("radiation");
+	public static final RegistryObject<Effect> RADIATION = EFFECTS.register("radiation", () -> new RadiationEffect("radiation"));
 	
 	public static void register(IEventBus bus) {
 		EFFECTS.register(bus);
