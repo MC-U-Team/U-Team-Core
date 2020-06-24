9d0fec5007ddff958cf9ea4c9277ccca366983ca
HyCraftHD <hycrafthd@live.de>
2020-06-25 01:46:08 +0200

    Remove unused annotations for test enchantments

--- src/test/java/info/u_team/u_team_test/init/TestEnchantments.java
+++ src/test/java/info/u_team/u_team_test/init/TestEnchantments.java
@@ -1,22 +1,11 @@
 package info.u_team.u_team_test.init;
 
-import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
-import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.enchantment.AutoSmeltEnchantment;
 import net.minecraft.enchantment.Enchantment;
-import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
-@EventBusSubscriber(modid = TestMod.MODID, bus = Bus.MOD)
 public class TestEnchantments {
 	
 	public static final Enchantment AUTO_SMELT = new AutoSmeltEnchantment("auto_smelt");
 	
-	@SubscribeEvent
-	public static void register(Register<Enchantment> event) {
-		BaseRegistryUtil.getAllRegistryEntriesAndApplyNames(TestMod.MODID, Enchantment.class).forEach(event.getRegistry()::register);
-	}
 	
 }
