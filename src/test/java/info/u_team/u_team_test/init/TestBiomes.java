3bc34c933c74db005c19916b46c642810fad615c
HyCraftHD <hycrafthd@live.de>
2020-06-25 00:58:43 +0200

    Register the deferred registry again

--- src/test/java/info/u_team/u_team_test/init/TestBiomes.java
+++ src/test/java/info/u_team/u_team_test/init/TestBiomes.java
@@ -5,14 +5,9 @@ import info.u_team.u_team_test.biome.BasicBiome;
 import net.minecraft.world.biome.Biome;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.*;
 
-@EventBusSubscriber(modid = TestMod.MODID, bus = Bus.MOD)
 public class TestBiomes {
-	
-	// public static final Biome BASIC = new BasicBiome("basic");
 	
 	public static final DeferredRegister<Biome> BLOCKS = DeferredRegister.create(ForgeRegistries.BIOMES, TestMod.MODID);
 	
@@ -22,6 +17,6 @@ public class TestBiomes {
 	});
 	
 	public static void register(IEventBus bus) {
-		
+		BLOCKS.register(bus);
 	}
 }
