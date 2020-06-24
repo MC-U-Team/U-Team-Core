f9180357107104f9ce7bef80211abd5a1c3afedf
HyCraftHD <hycrafthd@live.de>
2020-06-25 00:55:19 +0200

    Can register easier with the bus register

--- src/test/java/info/u_team/u_team_test/TestMod.java
+++ src/test/java/info/u_team/u_team_test/TestMod.java
@@ -1,9 +1,9 @@
 package info.u_team.u_team_test;
 
+import info.u_team.u_team_core.util.registry.BusRegister;
 import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import info.u_team.u_team_test.init.TestBiomes;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(TestMod.MODID)
 public class TestMod {
@@ -13,6 +13,6 @@ public class TestMod {
 	public TestMod() {
 		JarSignVerifier.checkSigned(MODID);
 		System.out.println("--------------------------------------- LOADING TEST MOD ---------------------------------------");
-		TestBiomes.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+		BusRegister.registerMod(TestBiomes::init);
 	}
 }
