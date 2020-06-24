20a633cb66a69909484ac46c99783318dc7be59e
HyCraftHD <hycrafthd@live.de>
2020-06-25 00:52:22 +0200

    Add for forge

--- src/main/java/info/u_team/u_team_core/util/registry/BusRegistry.java
+++ src/main/java/info/u_team/u_team_core/util/registry/BusRegistry.java
@@ -2,6 +2,7 @@ package info.u_team.u_team_core.util.registry;
 
 import java.util.function.Consumer;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -9,6 +10,10 @@ public class BusRegistry {
 	
 	public static void registerMod(Consumer<IEventBus> consumer) {
 		register(FMLJavaModLoadingContext.get().getModEventBus(), consumer);
+	}
+	
+	public static void registerForge(Consumer<IEventBus> consumer) {
+		register(MinecraftForge.EVENT_BUS, consumer);
 	}
 	
 	public static void register(IEventBus bus, Consumer<IEventBus> consumer) {
