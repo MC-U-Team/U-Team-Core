3f1c421fc336b77c2d3f66c4ef05730fe3b190ce
HyCraftHD <hycrafthd@live.de>
2020-06-25 01:37:40 +0200

    Use the deferred register now for potions

--- src/test/java/info/u_team/u_team_test/init/TestPotions.java
+++ src/test/java/info/u_team/u_team_test/init/TestPotions.java
@@ -4,6 +4,7 @@ import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.potion.RadiationPotion;
 import net.minecraft.potion.Potion;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.*;
@@ -13,9 +14,9 @@ public class TestPotions {
 	
 	public static final DeferredRegister<Potion> POTIONS = DeferredRegister.create(ForgeRegistries.POTION_TYPES, TestMod.MODID);
 	
-	public static final Potion RADIATION = new RadiationPotion("radiation", 1200, 0);
-	public static final Potion RADIATION_LONG = new RadiationPotion("radiation_long", 2400, 1);
-	public static final Potion RADIATION_EXTREME = new RadiationPotion("radiation_extreme", 1200, 2);
+	public static final RegistryObject<Potion> RADIATION = POTIONS.register("radiation", () -> new RadiationPotion(1200, 0));
+	public static final RegistryObject<Potion> RADIATION_LONG = POTIONS.register("radiation", () -> new RadiationPotion(2400, 1));
+	public static final RegistryObject<Potion> RADIATION_EXTREME = POTIONS.register("radiation", () -> new RadiationPotion(1200, 2));
 	
 	public static void register(IEventBus bus) {
 		POTIONS.register(bus);
