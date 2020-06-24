af6f48dba7860bb4c6d77d784e4ccff03692426c
HyCraftHD <hycrafthd@live.de>
2020-06-25 01:32:24 +0200

    Add deferred register to potions as they crash the game cuz of the effects

--- src/test/java/info/u_team/u_team_test/init/TestPotions.java
+++ src/test/java/info/u_team/u_team_test/init/TestPotions.java
@@ -1,24 +1,24 @@
 package info.u_team.u_team_test.init;
 
-import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
 import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.potion.RadiationPotion;
 import net.minecraft.potion.Potion;
-import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.registries.*;
 
 @EventBusSubscriber(modid = TestMod.MODID, bus = Bus.MOD)
 public class TestPotions {
+	
+	public static final DeferredRegister<Potion> POTIONS = DeferredRegister.create(ForgeRegistries.POTION_TYPES, TestMod.MODID);
 	
 	public static final Potion RADIATION = new RadiationPotion("radiation", 1200, 0);
 	public static final Potion RADIATION_LONG = new RadiationPotion("radiation_long", 2400, 1);
 	public static final Potion RADIATION_EXTREME = new RadiationPotion("radiation_extreme", 1200, 2);
 	
-	@SubscribeEvent
-	public static void register(Register<Potion> event) {
-		BaseRegistryUtil.getAllRegistryEntriesAndApplyNames(TestMod.MODID, Potion.class).forEach(event.getRegistry()::register);
+	public static void register(IEventBus bus) {
+		POTIONS.register(bus);
 	}
 	
 }
