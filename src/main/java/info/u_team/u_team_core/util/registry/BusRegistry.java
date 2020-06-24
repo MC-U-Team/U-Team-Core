a9d6890841c8930984f69799092309888fc412c9
HyCraftHD <hycrafthd@live.de>
2020-06-25 00:51:34 +0200

    Add bus registry for mod

--- src/main/java/info/u_team/u_team_core/util/registry/BusRegistry.java
+++ src/main/java/info/u_team/u_team_core/util/registry/BusRegistry.java
@@ -3,8 +3,13 @@ package info.u_team.u_team_core.util.registry;
 import java.util.function.Consumer;
 
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 public class BusRegistry {
+	
+	public static void registerMod(Consumer<IEventBus> consumer) {
+		register(FMLJavaModLoadingContext.get().getModEventBus(), consumer);
+	}
 	
 	public static void register(IEventBus bus, Consumer<IEventBus> consumer) {
 		consumer.accept(bus);
