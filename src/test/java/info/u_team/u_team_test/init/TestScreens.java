fb824bfaae4bd7ceaf5ee81dae2d28a9fe321cff
HyCraftHD <hycrafthd@live.de>
2020-06-25 00:44:48 +0200

    Fix class rename in caller class

--- src/test/java/info/u_team/u_team_test/init/TestScreens.java
+++ src/test/java/info/u_team/u_team_test/init/TestScreens.java
@@ -1,6 +1,6 @@
 package info.u_team.u_team_test.init;
 
-import info.u_team.u_team_core.util.registry.RegistryWorker;
+import info.u_team.u_team_core.util.registry.SyncedWorker;
 import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.screen.*;
 import net.minecraft.client.gui.ScreenManager;
@@ -15,7 +15,7 @@ public class TestScreens {
 	
 	@SubscribeEvent
 	public static void register(FMLClientSetupEvent event) {
-		RegistryWorker.runOnMainThread(() -> {
+		SyncedWorker.runOnMainThread(() -> {
 			ScreenManager.registerFactory(TestContainers.BASIC, BasicTileEntityScreen::new);
 			ScreenManager.registerFactory(TestContainers.BASIC_ENERGY_CREATOR, BasicEnergyCreatorScreen::new);
 			ScreenManager.registerFactory(TestContainers.BASIC_FLUID_INVENTORY, BasicFluidInventoryScreen::new);
