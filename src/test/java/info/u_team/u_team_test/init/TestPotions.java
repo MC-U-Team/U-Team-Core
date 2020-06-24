3272a4c153c8e4c07949c658778ad6304bef0f81
HyCraftHD <hycrafthd@live.de>
2020-06-25 01:40:33 +0200

    Fix name not containing the e

--- src/test/java/info/u_team/u_team_test/init/TestPotions.java
+++ src/test/java/info/u_team/u_team_test/init/TestPotions.java
@@ -16,7 +16,7 @@ public class TestPotions {
 	
 	public static final RegistryObject<Potion> RADIATION = POTIONS.register("radiation", () -> new RadiationPotion(1200, 0));
 	public static final RegistryObject<Potion> RADIATION_LONG = POTIONS.register("radiation_long", () -> new RadiationPotion(2400, 1));
-	public static final RegistryObject<Potion> RADIATION_EXTREME = POTIONS.register("radiation_extrem", () -> new RadiationPotion(1200, 2));
+	public static final RegistryObject<Potion> RADIATION_EXTREME = POTIONS.register("radiation_extreme", () -> new RadiationPotion(1200, 2));
 	
 	public static void register(IEventBus bus) {
 		POTIONS.register(bus);
