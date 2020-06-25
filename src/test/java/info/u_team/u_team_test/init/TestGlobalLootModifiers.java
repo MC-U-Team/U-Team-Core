2006f08a9590caa3af21a2b0c2a9e4acaa37bf8d
HyCraftHD <hycrafthd@live.de>
2020-06-25 14:05:13 +0200

    Remove unused imports

--- src/test/java/info/u_team/u_team_test/init/TestGlobalLootModifiers.java
+++ src/test/java/info/u_team/u_team_test/init/TestGlobalLootModifiers.java
@@ -1,22 +1,17 @@
 package info.u_team.u_team_test.init;
 
 import info.u_team.u_team_test.TestMod;
-import info.u_team.u_team_test.effect.RadiationEffect;
 import info.u_team.u_team_test.global_loot_modifier.AutoSmeltLootModifier;
-import net.minecraft.potion.Effect;
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
-import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.eventbus.api.*;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.*;
 
 public class TestGlobalLootModifiers {
 	
 	public static final DeferredRegister<GlobalLootModifierSerializer<?>> GLOBAL_LOOT_MODIFIER_SERIALIZERS = DeferredRegister.create(ForgeRegistries.LOOT_MODIFIER_SERIALIZERS, TestMod.MODID);
 	
-	public static final RegistryObject<AutoSmeltLootModifier.Serializer> AUTO_SMELT = GLOBAL_LOOT_MODIFIER_SERIALIZERS.register("auto_smelt", AutoSmeltLootModifier.Serializer::new);	
+	public static final RegistryObject<AutoSmeltLootModifier.Serializer> AUTO_SMELT = GLOBAL_LOOT_MODIFIER_SERIALIZERS.register("auto_smelt", AutoSmeltLootModifier.Serializer::new);
 	
 	public static void register(IEventBus bus) {
 		GLOBAL_LOOT_MODIFIER_SERIALIZERS.register(bus);
