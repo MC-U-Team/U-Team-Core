cd025e47c0580573f23bf9b6b8c6ff29d9db2264
HyCraftHD <hycrafthd@live.de>
2020-06-25 13:21:43 +0200

    Remove the old registry way from test entity types

--- src/test/java/info/u_team/u_team_test/init/TestEntityTypes.java
+++ src/test/java/info/u_team/u_team_test/init/TestEntityTypes.java
@@ -1,23 +1,11 @@
 package info.u_team.u_team_test.init;
 
 import info.u_team.u_team_core.entitytype.UEntityType;
-import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
-import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.entity.BetterEnderPearlEntity;
 import net.minecraft.entity.*;
-import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
-@EventBusSubscriber(modid = TestMod.MODID, bus = Bus.MOD)
 public class TestEntityTypes {
 	
 	public static final EntityType<BetterEnderPearlEntity> BETTER_ENDERPEARL = UEntityType.UBuilder.<BetterEnderPearlEntity> create("better_enderpearl", BetterEnderPearlEntity::new, EntityClassification.MISC).size(0.25F, 0.25F).setTrackingRange(128).setUpdateInterval(20).setShouldReceiveVelocityUpdates(true).build();
-	
-	@SubscribeEvent
-	public static void register(Register<EntityType<?>> event) {
-		BaseRegistryUtil.getAllGenericRegistryEntriesAndApplyNames(TestMod.MODID, EntityType.class).forEach(event.getRegistry()::register);
-	}
 	
 }
