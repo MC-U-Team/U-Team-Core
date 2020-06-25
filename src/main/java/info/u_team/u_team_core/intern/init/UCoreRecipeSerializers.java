4147a896fd3735351e04fd4f8030cf7d9f88e164
HyCraftHD <hycrafthd@live.de>
2020-06-26 01:51:56 +0200

    Use Methodreference in UCoreRecipeSerializers now

--- src/main/java/info/u_team/u_team_core/intern/init/UCoreRecipeSerializers.java
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreRecipeSerializers.java
@@ -20,7 +20,7 @@ public class UCoreRecipeSerializers {
 	
 	public static final RegistryObject<SpecialRecipeSerializer<DyeableItemDyeRecipe>> CRAFTING_SPECIAL_ITEMDYE = RECIPE_SERIALIZERS.register("crafting_special_itemdye", () -> new SpecialRecipeSerializer<>(DyeableItemDyeRecipe::new));
 	
-	public static final RegistryObject<NoMirrorShapedRecipe.Serializer> NO_MIRROR_SHAPED = RECIPE_SERIALIZERS.register("crafting_shaped_no_mirror", () -> new NoMirrorShapedRecipe.Serializer("crafting_shaped_no_mirror"));
+	public static final RegistryObject<NoMirrorShapedRecipe.Serializer> NO_MIRROR_SHAPED = RECIPE_SERIALIZERS.register("crafting_shaped_no_mirror", NoMirrorShapedRecipe.Serializer::new	);
 	
 	public static void register(IEventBus bus) {
 		RECIPE_SERIALIZERS.register(bus);
