6ed56af7469f074ffc5779d92a75a0cfd5258977
HyCraftHD <hycrafthd@live.de>
2020-06-26 01:59:53 +0200

    Update test block with new method references

--- src/test/java/info/u_team/u_team_test/init/TestBlocks.java
+++ src/test/java/info/u_team/u_team_test/init/TestBlocks.java
@@ -10,13 +10,13 @@ public class TestBlocks {
 	
 	public static final BlockDeferredRegister BLOCKS = BlockDeferredRegister.create(TestMod.MODID);
 	
-	public static final BlockRegistryObject<BasicBlock, BlockItem> BASIC = BLOCKS.register("basicblock", () -> new BasicBlock("basicblock"));
+	public static final BlockRegistryObject<BasicBlock, BlockItem> BASIC = BLOCKS.register("basicblock", BasicBlock::new);
 	
-	public static final BlockRegistryObject<BasicTileEntityBlock, BlockItem> BASIC_TILEENTITY = BLOCKS.register("tileentity", () -> new BasicTileEntityBlock("tileentity"));
+	public static final BlockRegistryObject<BasicTileEntityBlock, BlockItem> BASIC_TILEENTITY = BLOCKS.register("tileentity", BasicTileEntityBlock::new);
 	
-	public static final BlockRegistryObject<BasicEnergyCreatorBlock, BlockItem> BASIC_ENERGY_CREATOR = BLOCKS.register("energy_creator", () -> new BasicEnergyCreatorBlock("energy_creator"));
+	public static final BlockRegistryObject<BasicEnergyCreatorBlock, BlockItem> BASIC_ENERGY_CREATOR = BLOCKS.register("energy_creator", BasicEnergyCreatorBlock::new);
 	
-	public static final BlockRegistryObject<BasicFluidInventoryBlock, BlockItem> BASIC_FLUID_INVENTORY = BLOCKS.register("fluid_inventory", () -> new BasicFluidInventoryBlock("fluid_inventory"));
+	public static final BlockRegistryObject<BasicFluidInventoryBlock, BlockItem> BASIC_FLUID_INVENTORY = BLOCKS.register("fluid_inventory", BasicFluidInventoryBlock::new);
 	
 	public static void register(IEventBus bus) {
 		BLOCKS.register(bus);
