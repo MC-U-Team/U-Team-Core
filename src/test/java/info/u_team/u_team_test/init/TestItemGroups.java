53a93b3284556ad6ec53a845b0670dc09f2a02f0
HyCraftHD <hycrafthd@live.de>
2020-06-25 19:56:40 +0200

    Fix compile errors

--- src/test/java/info/u_team/u_team_test/init/TestItemGroups.java
+++ src/test/java/info/u_team/u_team_test/init/TestItemGroups.java
@@ -6,6 +6,6 @@ import net.minecraft.item.ItemGroup;
 
 public class TestItemGroups {
 	
-	public static final ItemGroup GROUP = new UItemGroup(TestMod.MODID, "group", () -> TestBlocks.BASIC);
+	public static final ItemGroup GROUP = new UItemGroup(TestMod.MODID, "group", () -> TestBlocks.BASIC.get());
 	
 }
