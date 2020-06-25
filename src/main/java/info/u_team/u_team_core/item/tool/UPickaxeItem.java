29fb8363314fa57a8692d4dc3d0a983c2ac6f16d
HyCraftHD <hycrafthd@live.de>
2020-06-26 00:49:48 +0200

    Clear up UPickaxeItem

--- src/main/java/info/u_team/u_team_core/item/tool/UPickaxeItem.java
+++ src/main/java/info/u_team/u_team_core/item/tool/UPickaxeItem.java
@@ -2,26 +2,17 @@ package info.u_team.u_team_core.item.tool;
 
 import info.u_team.u_team_core.api.IToolMaterial;
 import info.u_team.u_team_core.api.IToolMaterial.Tools;
-import info.u_team.u_team_core.api.registry.IURegistryType;
 import net.minecraft.item.*;
 import net.minecraftforge.common.ToolType;
 
-public class UPickaxeItem extends PickaxeItem implements IURegistryType {
+public class UPickaxeItem extends PickaxeItem {
 	
-	protected final String name;
-	
-	public UPickaxeItem(String name, Properties properties, IToolMaterial material) {
-		this(name, null, properties, material);
+	public UPickaxeItem(Properties properties, IToolMaterial material) {
+		this(null, properties, material);
 	}
 	
-	public UPickaxeItem(String name, ItemGroup group, Properties properties, IToolMaterial material) {
+	public UPickaxeItem(ItemGroup group, Properties properties, IToolMaterial material) {
 		super(material, (int) material.getAdditionalDamage(Tools.PICKAXE), material.getAttackSpeed(Tools.PICKAXE), applyToolType(group == null ? properties : properties.group(group), material));
-		this.name = name;
-	}
-	
-	@Override
-	public String getEntryName() {
-		return name;
 	}
 	
 	private static Properties applyToolType(Properties properties, IToolMaterial material) {
