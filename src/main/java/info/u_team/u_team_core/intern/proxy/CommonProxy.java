f3f2506d92ae3ebc57bc52d94757d97744ac3c7a
HyCraftHD <hycrafthd@live.de>
2020-06-25 23:06:59 +0200

    Register recipe serializers

--- src/main/java/info/u_team/u_team_core/intern/proxy/CommonProxy.java
+++ src/main/java/info/u_team/u_team_core/intern/proxy/CommonProxy.java
@@ -2,7 +2,8 @@ package info.u_team.u_team_core.intern.proxy;
 
 import info.u_team.u_team_core.api.IModProxy;
 import info.u_team.u_team_core.intern.config.ClientConfig;
-import info.u_team.u_team_core.intern.init.UCoreNetwork;
+import info.u_team.u_team_core.intern.init.*;
+import info.u_team.u_team_core.util.registry.BusRegister;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig.Type;
 
@@ -12,6 +13,7 @@ public class CommonProxy implements IModProxy {
 	public void construct() {
 		ModLoadingContext.get().registerConfig(Type.CLIENT, ClientConfig.CONFIG);
 		UCoreNetwork.construct();
+		BusRegister.registerMod(UCoreRecipeSerializers::register);
 	}
 	
 	@Override
