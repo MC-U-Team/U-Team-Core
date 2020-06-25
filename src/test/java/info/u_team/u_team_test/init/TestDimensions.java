5d5ab0f2c8aeba6ad379b06c0e2af2108b1a1354
HyCraftHD <hycrafthd@live.de>
2020-06-25 14:15:51 +0200

    Fix the test dimensions class to register a test dimension for our basic mod dimension type

--- src/test/java/info/u_team/u_team_test/init/TestDimensions.java
+++ src/test/java/info/u_team/u_team_test/init/TestDimensions.java
@@ -13,8 +13,8 @@ public class TestDimensions {
 	@SuppressWarnings("deprecation")
 	@SubscribeEvent
 	public static void on(RegisterDimensionsEvent event) {
-		if (!DimensionManager.getRegistry().keySet().contains(TestModDimensions.BASIC.getRegistryName())) { // How do we know when the dimension needs to be registered??
-			DimensionManager.registerDimension(TestModDimensions.BASIC.getRegistryName(), TestModDimensions.BASIC, null, true);
+		if (!DimensionManager.getRegistry().keySet().contains(TestModDimensions.BASIC.getId())) { // How do we know when the dimension needs to be registered??
+			DimensionManager.registerDimension(TestModDimensions.BASIC.getId(), TestModDimensions.BASIC.get(), null, true);
 		}
 	}
 }
