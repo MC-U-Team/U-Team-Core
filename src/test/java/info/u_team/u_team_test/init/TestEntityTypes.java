79d816ee4857cb002b38647c8529d134908b3022
HyCraftHD <hycrafthd@live.de>
2020-06-25 14:00:38 +0200

    Change the test entity type register for the better ender pearl to use the deferred register

--- src/test/java/info/u_team/u_team_test/init/TestEntityTypes.java
+++ src/test/java/info/u_team/u_team_test/init/TestEntityTypes.java
@@ -5,14 +5,14 @@ import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.entity.BetterEnderPearlEntity;
 import net.minecraft.entity.*;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.*;
 
 public class TestEntityTypes {
 	
 	public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES, TestMod.MODID);
 	
-	public static final EntityType<BetterEnderPearlEntity> BETTER_ENDERPEARL = UEntityType.UBuilder.<BetterEnderPearlEntity> create("better_enderpearl", BetterEnderPearlEntity::new, EntityClassification.MISC).size(0.25F, 0.25F).setTrackingRange(128).setUpdateInterval(20).setShouldReceiveVelocityUpdates(true).build();
-	
+	public static final RegistryObject<EntityType<BetterEnderPearlEntity>> BETTER_ENDERPEARL = ENTITY_TYPES.register("better_enderpearl", () -> UEntityType.UBuilder.<BetterEnderPearlEntity> create("better_enderpearl", BetterEnderPearlEntity::new, EntityClassification.MISC).size(0.25F, 0.25F).setTrackingRange(128).setUpdateInterval(20).setShouldReceiveVelocityUpdates(true).build());
 	
 	public static void register(IEventBus bus) {
 		ENTITY_TYPES.register(bus);
