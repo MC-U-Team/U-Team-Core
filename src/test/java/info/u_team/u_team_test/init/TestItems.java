cfc3d24f4c91cb3883cdf8a89c31c572546b746e
HyCraftHD <hycrafthd@live.de>
2020-06-25 22:49:18 +0200

    Add deferred register to items

--- src/test/java/info/u_team/u_team_test/init/TestItems.java
+++ src/test/java/info/u_team/u_team_test/init/TestItems.java
@@ -2,18 +2,16 @@ package info.u_team.u_team_test.init;
 
 import info.u_team.u_team_core.item.armor.*;
 import info.u_team.u_team_core.item.tool.*;
-import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
 import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.item.*;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.Properties;
-import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.*;
 
-@EventBusSubscriber(modid = TestMod.MODID, bus = Bus.MOD)
 public class TestItems {
+	
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MODID);
 	
 	public static final Item BASIC = new BasicItem("basicitem");
 	
@@ -24,9 +22,7 @@ public class TestItems {
 	public static final ToolSet BASIC_TOOL = ToolSetCreator.create("basictool", TestItemGroups.GROUP, new Properties(), TestToolMaterial.BASIC);
 	public static final ArmorSet BASIC_ARMOR = ArmorSetCreator.create("basicarmor", TestItemGroups.GROUP, new Properties(), TestArmorMaterial.BASIC);
 	
-	@SubscribeEvent
-	public static void registerBlockItem(Register<Item> event) {
-		BaseRegistryUtil.getAllRegistryEntriesAndApplyNames(TestMod.MODID, Item.class).forEach(event.getRegistry()::register);
+	public static void register(IEventBus bus) {
+		ITEMS.register(bus);
 	}
-	
 }
