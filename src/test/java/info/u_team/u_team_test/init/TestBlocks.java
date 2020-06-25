e5374ae56759a3e91f2940f8d08c73aaa52757a3
HyCraftHD <hycrafthd@live.de>
2020-06-25 19:53:55 +0200

    Add block deferred register

--- src/test/java/info/u_team/u_team_test/init/TestBlocks.java
+++ src/test/java/info/u_team/u_team_test/init/TestBlocks.java
@@ -1,8 +1,13 @@
 package info.u_team.u_team_test.init;
 
+import info.u_team.u_team_core.util.registry.BlockDeferredRegister;
+import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.block.*;
+import net.minecraftforge.eventbus.api.IEventBus;
 
 public class TestBlocks {
+	
+	public static final BlockDeferredRegister BLOCKS = BlockDeferredRegister.create(TestMod.MODID);
 	
 	public static final BasicBlock BASIC = new BasicBlock("basicblock");
 	
@@ -11,5 +16,9 @@ public class TestBlocks {
 	public static final BasicEnergyCreatorBlock BASIC_ENERGY_CREATOR = new BasicEnergyCreatorBlock("energy_creator");
 	
 	public static final BasicFluidInventoryBlock BASIC_FLUID_INVENTORY = new BasicFluidInventoryBlock("fluid_inventory");
+	
+	public static void register(IEventBus bus) {
+		BLOCKS.register(bus);
+	}
 	
 }
