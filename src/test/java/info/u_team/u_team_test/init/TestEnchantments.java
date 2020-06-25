60cfb9623f4d7bab29633e45a63de02f024def6d
HyCraftHD <hycrafthd@live.de>
2020-06-25 13:16:38 +0200

    Add deferred register to enchantments

--- src/test/java/info/u_team/u_team_test/init/TestEnchantments.java
+++ src/test/java/info/u_team/u_team_test/init/TestEnchantments.java
@@ -1,11 +1,19 @@
 package info.u_team.u_team_test.init;
 
+import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.enchantment.AutoSmeltEnchantment;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.*;
 
 public class TestEnchantments {
 	
+	public static final DeferredRegister<Enchantment> ENCHANTMENTS = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, TestMod.MODID);
+	
 	public static final Enchantment AUTO_SMELT = new AutoSmeltEnchantment("auto_smelt");
 	
+	public static void register(IEventBus bus) {
+		ENCHANTMENTS.register(bus);
+	}
 	
 }
