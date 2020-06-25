e9c13363ae932222e6cae5566e56f4827d295d70
HyCraftHD <hycrafthd@live.de>
2020-06-25 13:17:58 +0200

    Register autosmelt using the deferred register

--- src/test/java/info/u_team/u_team_test/init/TestEnchantments.java
+++ src/test/java/info/u_team/u_team_test/init/TestEnchantments.java
@@ -4,13 +4,14 @@ import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.enchantment.AutoSmeltEnchantment;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.*;
 
 public class TestEnchantments {
 	
 	public static final DeferredRegister<Enchantment> ENCHANTMENTS = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, TestMod.MODID);
 	
-	public static final Enchantment AUTO_SMELT = new AutoSmeltEnchantment("auto_smelt");
+	public static final RegistryObject<Enchantment> AUTO_SMELT = ENCHANTMENTS.register("auto_smelt", () -> new AutoSmeltEnchantment("auto_smelt"));
 	
 	public static void register(IEventBus bus) {
 		ENCHANTMENTS.register(bus);
