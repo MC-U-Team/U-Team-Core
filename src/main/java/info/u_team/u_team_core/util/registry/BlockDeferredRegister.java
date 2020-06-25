7c243f6292bc0f8e390efd516ceb0f7334ca6ae2
HyCraftHD <hycrafthd@live.de>
2020-06-25 19:52:21 +0200

    Register the items now too

--- src/main/java/info/u_team/u_team_core/util/registry/BlockDeferredRegister.java
+++ src/main/java/info/u_team/u_team_core/util/registry/BlockDeferredRegister.java
@@ -4,8 +4,9 @@ import java.util.function.Supplier;
 
 import info.u_team.u_team_core.api.registry.IUBlockRegistryType;
 import net.minecraft.block.Block;
-import net.minecraft.item.BlockItem;
+import net.minecraft.item.*;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent.Register;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.*;
@@ -16,21 +17,34 @@ public class BlockDeferredRegister {
 		return new BlockDeferredRegister(modid);
 	}
 	
-	private final DeferredRegister<Block> blocks;
+	private final String modid;
+	
+	private final DeferredRegister<Block> blocksWithItemBlockProvider;
 	
 	protected BlockDeferredRegister(String modid) {
-		blocks = DeferredRegister.create(ForgeRegistries.BLOCKS, modid);
+		this.modid = modid;
+		blocksWithItemBlockProvider = DeferredRegister.create(ForgeRegistries.BLOCKS, modid);
 	}
 	
 	public <B extends Block & IUBlockRegistryType, I extends BlockItem> BlockRegistryObject<B, I> register(String name, Supplier<? extends B> supplier) {
-		final RegistryObject<B> block = blocks.register(name, supplier);
-		final RegistryObject<I> item = RegistryObject.of(new ResourceLocation(name), ForgeRegistries.ITEMS);
+		final RegistryObject<B> block = blocksWithItemBlockProvider.register(name, supplier);
+		final RegistryObject<I> item = RegistryObject.of(new ResourceLocation(modid, name), ForgeRegistries.ITEMS);
 		
 		return new BlockRegistryObject<B, I>(block, item);
 	}
 	
 	public void register(IEventBus bus) {
-		blocks.register(bus);
+		blocksWithItemBlockProvider.register(bus);
+		bus.addGenericListener(Item.class, this::addItems);
+	}
+	
+	private void addItems(Register<Item> event) {
+		final IForgeRegistry<Item> registry = event.getRegistry();
+		blocksWithItemBlockProvider.getEntries() //
+				.stream() //
+				.map(RegistryObject::get) //
+				.filter(IUBlockRegistryType.class::isInstance) //
+				.forEach(block -> registry.register(((IUBlockRegistryType) block).getBlockItem().setRegistryName(block.getRegistryName())));
 	}
 	
 }
