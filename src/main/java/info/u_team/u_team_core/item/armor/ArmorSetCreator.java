295d6c35ebf10e7901734086f723aad1954f25d0
HyCraftHD <hycrafthd@live.de>
2020-06-26 01:06:12 +0200

    Update armor set with new register

--- src/main/java/info/u_team/u_team_core/item/armor/ArmorSetCreator.java
+++ src/main/java/info/u_team/u_team_core/item/armor/ArmorSetCreator.java
@@ -3,14 +3,21 @@ package info.u_team.u_team_core.item.armor;
 import info.u_team.u_team_core.util.ItemProperties;
 import net.minecraft.item.*;
 import net.minecraft.item.Item.Properties;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
 
 public class ArmorSetCreator {
 	
-	public static ArmorSet create(String name, Properties properties, IArmorMaterial material) {
-		return create(name, null, properties, material);
+	public static ArmorSet create(DeferredRegister<Item> register, String name, Properties properties, IArmorMaterial material) {
+		return create(register, name, null, properties, material);
 	}
 	
-	public static ArmorSet create(String name, ItemGroup group, Properties properties, IArmorMaterial material) {
-		return new ArmorSet(new UHelmetItem(name, group, new ItemProperties(properties), material), new UChestplateItem(name, group, new ItemProperties(properties), material), new ULeggingsItem(name, group, new ItemProperties(properties), material), new UBootsItem(name, group, new ItemProperties(properties), material));
+	public static ArmorSet create(DeferredRegister<Item> register, String name, ItemGroup group, Properties properties, IArmorMaterial material) {
+		final RegistryObject<UHelmetItem> helmet = register.register(name + "_helmet", () -> new UHelmetItem(name, group, new ItemProperties(properties), material));
+		final RegistryObject<UChestplateItem> chestplate = register.register(name + "_chestplate", () -> new UChestplateItem(name, group, new ItemProperties(properties), material));
+		final RegistryObject<ULeggingsItem> leggings = register.register(name + "_leggings", () -> new ULeggingsItem(name, group, new ItemProperties(properties), material));
+		final RegistryObject<UBootsItem> boots = register.register(name + "_boots", () -> new UBootsItem(name, group, new ItemProperties(properties), material));
+		
+		return new ArmorSet(helmet, chestplate, leggings, boots);
 	}
 }
