759ca471a1b67b79f0ca7dc6e73ce0076d37728d
HyCraftHD <hycrafthd@live.de>
2020-06-25 15:12:16 +0200

    Remove unused imports

--- src/test/java/info/u_team/u_team_test/init/TestSounds.java
+++ src/test/java/info/u_team/u_team_test/init/TestSounds.java
@@ -4,8 +4,6 @@ import info.u_team.u_team_test.TestMod;
 import net.minecraft.util.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.*;
 
 public class TestSounds {
