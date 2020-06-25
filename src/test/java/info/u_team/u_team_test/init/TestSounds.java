27f505fa21c1d3969d23ffe65bc5897f53366e54
HyCraftHD <hycrafthd@live.de>
2020-06-25 14:25:32 +0200

    Register sound event better enderpearl use with the deferred register

--- src/test/java/info/u_team/u_team_test/init/TestSounds.java
+++ src/test/java/info/u_team/u_team_test/init/TestSounds.java
@@ -4,6 +4,7 @@ import info.u_team.u_team_core.soundevent.USoundEvent;
 import info.u_team.u_team_test.TestMod;
 import net.minecraft.util.SoundEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.*;
@@ -13,7 +14,7 @@ public class TestSounds {
 	
 	public static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, TestMod.MODID);
 	
-	public static final SoundEvent BETTER_ENDERPEARL_USE = new USoundEvent("better_enderpearl_use");
+	public static final RegistryObject<SoundEvent> BETTER_ENDERPEARL_USE = SOUND_EVENTS.register("better_enderpearl_use", () -> new USoundEvent("better_enderpearl_use"));
 	
 	public static void register(IEventBus bus) {
 		SOUND_EVENTS.register(bus);
