ba95f4c93eb2cb5d9b0479f0c644f14f2221301b
HyCraftHD <hycrafthd@live.de>
2020-06-26 00:48:49 +0200

    Clear up UHoeItem

--- src/main/java/info/u_team/u_team_core/item/tool/UHoeItem.java
+++ src/main/java/info/u_team/u_team_core/item/tool/UHoeItem.java
@@ -2,25 +2,16 @@ package info.u_team.u_team_core.item.tool;
 
 import info.u_team.u_team_core.api.IToolMaterial;
 import info.u_team.u_team_core.api.IToolMaterial.Tools;
-import info.u_team.u_team_core.api.registry.IURegistryType;
 import net.minecraft.item.*;
 
-public class UHoeItem extends HoeItem implements IURegistryType {
+public class UHoeItem extends HoeItem {
 	
-	protected final String name;
-	
-	public UHoeItem(String name, Properties properties, IToolMaterial material) {
-		this(name, null, properties, material);
+	public UHoeItem(Properties properties, IToolMaterial material) {
+		this(null, properties, material);
 	}
 	
-	public UHoeItem(String name, ItemGroup group, Properties properties, IToolMaterial material) {
+	public UHoeItem(ItemGroup group, Properties properties, IToolMaterial material) {
 		super(material, material.getAttackSpeed(Tools.HOE), group == null ? properties : properties.group(group));
-		this.name = name;
-	}
-	
-	@Override
-	public String getEntryName() {
-		return name;
 	}
 	
 }
