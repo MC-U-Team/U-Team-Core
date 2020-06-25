6b8fd905f0b6f05149a043cd3c4a1aaa02e366b5
HyCraftHD <hycrafthd@live.de>
2020-06-26 01:31:17 +0200

    Update UBlock

--- src/main/java/info/u_team/u_team_core/block/UBlock.java
+++ src/main/java/info/u_team/u_team_core/block/UBlock.java
@@ -1,40 +1,32 @@
 package info.u_team.u_team_core.block;
 
-import info.u_team.u_team_core.api.registry.IUBlockRegistryType;
+import info.u_team.u_team_core.api.registry.IBlockItemProvider;
 import net.minecraft.block.Block;
 import net.minecraft.item.*;
 
-public class UBlock extends Block implements IUBlockRegistryType {
-	
-	protected final String name;
+public class UBlock extends Block implements IBlockItemProvider {
 	
 	protected final BlockItem blockItem;
 	
-	public UBlock(String name, Properties properties) {
-		this(name, null, properties);
+	public UBlock(Properties properties) {
+		this(null, properties);
 	}
 	
-	public UBlock(String name, ItemGroup group, Properties properties) {
-		this(name, group, properties, null);
+	public UBlock(ItemGroup group, Properties properties) {
+		this(group, properties, null);
 	}
 	
-	public UBlock(String name, Properties properties, Item.Properties blockItemProperties) {
-		this(name, null, properties, blockItemProperties);
+	public UBlock(Properties properties, Item.Properties blockItemProperties) {
+		this(null, properties, blockItemProperties);
 	}
 	
-	public UBlock(String name, ItemGroup group, Properties properties, Item.Properties blockItemProperties) {
+	public UBlock(ItemGroup group, Properties properties, Item.Properties blockItemProperties) {
 		super(properties);
-		this.name = name;
 		blockItem = createBlockItem(blockItemProperties == null ? new Item.Properties().group(group) : group == null ? blockItemProperties : blockItemProperties.group(group));
 	}
 	
 	protected BlockItem createBlockItem(Item.Properties blockItemProperties) {
 		return new BlockItem(this, blockItemProperties);
-	}
-	
-	@Override
-	public String getEntryName() {
-		return name;
 	}
 	
 	@Override
