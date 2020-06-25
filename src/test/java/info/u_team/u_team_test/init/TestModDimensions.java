9746bab682a149506a8ab84a893c76725a863a65
HyCraftHD <hycrafthd@live.de>
2020-06-25 14:14:39 +0200

    Register basic dimension with deferred register now. Also make UModDimension useless

--- src/test/java/info/u_team/u_team_test/init/TestModDimensions.java
+++ src/test/java/info/u_team/u_team_test/init/TestModDimensions.java
@@ -1,13 +1,10 @@
 package info.u_team.u_team_test.init;
 
-import info.u_team.u_team_core.dimension.UModDimension;
-import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
 import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.dimension.BasicDimension;
-import net.minecraft.potion.Effect;
 import net.minecraftforge.common.ModDimension;
-import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.eventbus.api.*;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.*;
@@ -17,15 +14,16 @@ public class TestModDimensions {
 	
 	public static final DeferredRegister<ModDimension> MOD_DIMENSIONS = DeferredRegister.create(ForgeRegistries.MOD_DIMENSIONS, TestMod.MODID);
 	
-	public static final ModDimension BASIC = new UModDimension("basic", BasicDimension::new);
+	public static final RegistryObject<ModDimension> BASIC = MOD_DIMENSIONS.register("basic", () -> ModDimension.withFactory(BasicDimension::new));
 	
 	public static void register(IEventBus bus) {
 		MOD_DIMENSIONS.register(bus);
 	}
 	
-//	@SubscribeEvent
-//	public static void register(Register<ModDimension> event) {
-//		BaseRegistryUtil.getAllRegistryEntriesAndApplyNames(TestMod.MODID, ModDimension.class).forEach(event.getRegistry()::register);
-//	}
-//	
+	// @SubscribeEvent
+	// public static void register(Register<ModDimension> event) {
+	// BaseRegistryUtil.getAllRegistryEntriesAndApplyNames(TestMod.MODID,
+	// ModDimension.class).forEach(event.getRegistry()::register);
+	// }
+	//
 }
