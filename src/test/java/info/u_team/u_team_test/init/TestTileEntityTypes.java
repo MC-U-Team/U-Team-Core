f4f87fb00dfb9597c44a1f0fa30752603e40196f
HyCraftHD <hycrafthd@live.de>
2020-06-25 15:50:55 +0200

    Register test tile entity types with the deferred register now

--- src/test/java/info/u_team/u_team_test/init/TestTileEntityTypes.java
+++ src/test/java/info/u_team/u_team_test/init/TestTileEntityTypes.java
@@ -5,17 +5,18 @@ import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.tileentity.*;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.*;
 
 public class TestTileEntityTypes {
 	
 	public static final DeferredRegister<TileEntityType<?>> TILE_ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, TestMod.MODID);
 	
-	public static final TileEntityType<BasicTileEntityTileEntity> BASIC = UTileEntityType.UBuilder.create("tileentity", BasicTileEntityTileEntity::new, TestBlocks.BASIC_TILEENTITY).build();
+	public static final RegistryObject<TileEntityType<BasicTileEntityTileEntity>> BASIC = TILE_ENTITY_TYPES.register("tileentity", () -> UTileEntityType.UBuilder.create("tileentity", BasicTileEntityTileEntity::new, TestBlocks.BASIC_TILEENTITY).build());
 	
-	public static final TileEntityType<BasicEnergyCreatorTileEntity> BASIC_ENERGY_CREATOR = UTileEntityType.UBuilder.create("energy_creator", BasicEnergyCreatorTileEntity::new, TestBlocks.BASIC_ENERGY_CREATOR).build();
+	public static final RegistryObject<TileEntityType<BasicEnergyCreatorTileEntity>> BASIC_ENERGY_CREATOR = TILE_ENTITY_TYPES.register("energy_creator", () -> UTileEntityType.UBuilder.create("energy_creator", BasicEnergyCreatorTileEntity::new, TestBlocks.BASIC_ENERGY_CREATOR).build());
 	
-	public static final TileEntityType<BasicFluidInventoryTileEntity> BASIC_FLUID_INVENTORY = UTileEntityType.UBuilder.create("fluid_inventory", BasicFluidInventoryTileEntity::new, TestBlocks.BASIC_FLUID_INVENTORY).build();
+	public static final RegistryObject<TileEntityType<BasicFluidInventoryTileEntity>> BASIC_FLUID_INVENTORY = TILE_ENTITY_TYPES.register("fluid_inventory", () -> UTileEntityType.UBuilder.create("fluid_inventory", BasicFluidInventoryTileEntity::new, TestBlocks.BASIC_FLUID_INVENTORY).build());
 	
 	public static void register(IEventBus bus) {
 		TILE_ENTITY_TYPES.register(bus);
