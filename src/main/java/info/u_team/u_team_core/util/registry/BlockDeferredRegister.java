1f9d7139da97efdbdd079669da70dead1e42d5b9
HyCraftHD <hycrafthd@live.de>
2020-06-25 19:09:44 +0200

    Add block deferred register

---
+++ src/main/java/info/u_team/u_team_core/util/registry/BlockDeferredRegister.java
@@ -0,0 +1,24 @@
+package info.u_team.u_team_core.util.registry;
+
+import java.util.function.Supplier;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.*;
+
+public class BlockDeferredRegister {
+	
+	public final DeferredRegister<Block> blocks;
+	public final DeferredRegister<Item> items;
+	
+	public BlockDeferredRegister(String modid) {
+		blocks = DeferredRegister.create(ForgeRegistries.BLOCKS, modid);
+		items = DeferredRegister.create(ForgeRegistries.ITEMS, modid);
+	}
+	
+	public <I extends Block> RegistryObject<I> registerCommon(String name, Supplier<? extends I> supplier) {
+		return blocks.register(name, supplier);
+	}
+	
+}
