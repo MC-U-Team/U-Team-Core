84f823fa1fcab616641f617b8d320566632f1804
HyCraftHD <hycrafthd@live.de>
2020-06-26 00:56:51 +0200

    Update tool set creator

--- src/main/java/info/u_team/u_team_core/item/tool/ToolSetCreator.java
+++ src/main/java/info/u_team/u_team_core/item/tool/ToolSetCreator.java
@@ -2,17 +2,25 @@ package info.u_team.u_team_core.item.tool;
 
 import info.u_team.u_team_core.api.IToolMaterial;
 import info.u_team.u_team_core.util.ItemProperties;
+import net.minecraft.item.*;
 import net.minecraft.item.Item.Properties;
-import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
 
 public class ToolSetCreator {
 	
-	public static ToolSet create(String name, Properties properties, IToolMaterial material) {
-		return create(name, null, properties, material);
+	public static ToolSet create(DeferredRegister<Item> register, String name, Properties properties, IToolMaterial material) {
+		return create(register, name, null, properties, material);
 	}
 	
-	public static ToolSet create(String name, ItemGroup group, Properties properties, IToolMaterial material) {
-		return new ToolSet(new UAxeItem(name + "_axe", group, new ItemProperties(properties), material), new UHoeItem(name + "_hoe", group, new ItemProperties(properties), material), new UPickaxeItem(name + "_pickaxe", group, new ItemProperties(properties), material), new UShovelItem(name + "_shovel", group, new ItemProperties(properties), material), new USwordItem(name + "_sword", group, new ItemProperties(properties), material));
+	public static ToolSet create(DeferredRegister<Item> register, String name, ItemGroup group, Properties properties, IToolMaterial material) {
+		final RegistryObject<UAxeItem> axe = register.register(name + "_axe", () -> new UAxeItem(group, new ItemProperties(properties), material));
+		final RegistryObject<UHoeItem> hoe = register.register(name + "_hoe", () -> new UHoeItem(group, new ItemProperties(properties), material));
+		final RegistryObject<UPickaxeItem> pickaxe = register.register(name + "_pickaxe", () -> new UPickaxeItem(group, new ItemProperties(properties), material));
+		final RegistryObject<UShovelItem> shovel = register.register(name + "_shovel", () -> new UShovelItem(group, new ItemProperties(properties), material));
+		final RegistryObject<USwordItem> sword = register.register(name + "_sword", () -> new USwordItem(group, new ItemProperties(properties), material));
+		
+		return new ToolSet(axe, hoe, pickaxe, shovel, sword);
 	}
 	
 }
