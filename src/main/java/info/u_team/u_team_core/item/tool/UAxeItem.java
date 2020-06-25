8aaf0c5660c73628e932fc3542b384ab11e8a5fe
HyCraftHD <hycrafthd@live.de>
2020-06-26 00:48:12 +0200

    Clear UAxeItem up

--- src/main/java/info/u_team/u_team_core/item/tool/UAxeItem.java
+++ src/main/java/info/u_team/u_team_core/item/tool/UAxeItem.java
@@ -2,26 +2,17 @@ package info.u_team.u_team_core.item.tool;
 
 import info.u_team.u_team_core.api.IToolMaterial;
 import info.u_team.u_team_core.api.IToolMaterial.Tools;
-import info.u_team.u_team_core.api.registry.IURegistryType;
 import net.minecraft.item.*;
 import net.minecraftforge.common.ToolType;
 
-public class UAxeItem extends AxeItem implements IURegistryType {
+public class UAxeItem extends AxeItem {
 	
-	protected final String name;
-	
-	public UAxeItem(String name, Properties properties, IToolMaterial material) {
-		this(name, null, properties, material);
+	public UAxeItem(Properties properties, IToolMaterial material) {
+		this(null, properties, material);
 	}
 	
-	public UAxeItem(String name, ItemGroup group, Properties properties, IToolMaterial material) {
+	public UAxeItem(ItemGroup group, Properties properties, IToolMaterial material) {
 		super(material, material.getAdditionalDamage(Tools.AXE), material.getAttackSpeed(Tools.AXE), applyToolType(group == null ? properties : properties.group(group), material));
-		this.name = name;
-	}
-	
-	@Override
-	public String getEntryName() {
-		return name;
 	}
 	
 	private static Properties applyToolType(Properties properties, IToolMaterial material) {
