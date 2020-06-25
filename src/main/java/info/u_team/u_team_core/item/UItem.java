904fbbddbf22bf4083d7feac0d38ba66a2f3f29f
HyCraftHD <hycrafthd@live.de>
2020-06-26 01:42:19 +0200

    Update UItem

--- src/main/java/info/u_team/u_team_core/item/UItem.java
+++ src/main/java/info/u_team/u_team_core/item/UItem.java
@@ -1,23 +1,15 @@
 package info.u_team.u_team_core.item;
 
-import info.u_team.u_team_core.api.registry.IURegistryType;
 import net.minecraft.item.*;
 
-public class UItem extends Item implements IURegistryType {
+public class UItem extends Item {
 	
-	protected final String name;
-	
-	public UItem(String name, Properties properties) {
-		this(name, null, properties);
+	public UItem(Properties properties) {
+		this(null, properties);
 	}
 	
-	public UItem(String name, ItemGroup group, Properties properties) {
+	public UItem(ItemGroup group, Properties properties) {
 		super(group == null ? properties : properties.group(group));
-		this.name = name;
 	}
 	
-	@Override
-	public String getEntryName() {
-		return name;
-	}
 }
