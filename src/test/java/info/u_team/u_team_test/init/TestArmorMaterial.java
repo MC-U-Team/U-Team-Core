35d35abce3f4ce6b5ff9f7a029d5a9e2edfaf5cf
HyCraftHD <hycrafthd@live.de>
2020-06-25 22:53:35 +0200

    Fix tool and armor material

--- src/test/java/info/u_team/u_team_test/init/TestArmorMaterial.java
+++ src/test/java/info/u_team/u_team_test/init/TestArmorMaterial.java
@@ -6,6 +6,6 @@ import net.minecraft.item.crafting.Ingredient;
 
 public class TestArmorMaterial {
 	
-	public static final IArmorMaterial BASIC = new UArmorMaterialVanilla(20, new int[] { 5, 6, 8, 2 }, 20, TestSounds.BETTER_ENDERPEARL_USE, 1, () -> Ingredient.fromItems(TestItems.BASIC));
+	public static final IArmorMaterial BASIC = new UArmorMaterialVanilla(20, new int[] { 5, 6, 8, 2 }, 20, TestSounds.BETTER_ENDERPEARL_USE, 1, () -> Ingredient.fromItems(TestItems.BASIC.get()));
 	
 }
