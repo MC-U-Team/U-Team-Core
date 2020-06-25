c7939cae779db40c996d136006f6546e8789f9cd
HyCraftHD <hycrafthd@live.de>
2020-06-25 23:10:34 +0200

    Remove unused U recipe serializer

--- src/main/java/info/u_team/u_team_core/intern/init/UCoreRecipeSerializers.java
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreRecipeSerializers.java
@@ -2,8 +2,7 @@ package info.u_team.u_team_core.intern.init;
 
 import info.u_team.u_team_core.UCoreMain;
 import info.u_team.u_team_core.intern.recipe.*;
-import info.u_team.u_team_core.recipeserializer.USpecialRecipeSerializer;
-import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.item.crafting.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -15,7 +14,7 @@ public class UCoreRecipeSerializers {
 	
 	public static final DeferredRegister<IRecipeSerializer<?>> RECIPE_SERIALIZERS = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, UCoreMain.MODID);
 	
-	public static final RegistryObject<USpecialRecipeSerializer<DyeableItemDyeRecipe>> CRAFTING_SPECIAL_ITEMDYE = RECIPE_SERIALIZERS.register("crafting_special_itemdye", () -> new USpecialRecipeSerializer<>("crafting_special_itemdye", DyeableItemDyeRecipe::new));
+	public static final RegistryObject<SpecialRecipeSerializer<DyeableItemDyeRecipe>> CRAFTING_SPECIAL_ITEMDYE = RECIPE_SERIALIZERS.register("crafting_special_itemdye", () -> new SpecialRecipeSerializer<>(DyeableItemDyeRecipe::new));
 	
 	public static final RegistryObject<NoMirrorShapedRecipe.Serializer> NO_MIRROR_SHAPED = RECIPE_SERIALIZERS.register("crafting_shaped_no_mirror", () -> new NoMirrorShapedRecipe.Serializer("crafting_shaped_no_mirror"));
 	
