670c4aaf8446e1d8639a713e7ad6ffa18f783200
HyCraftHD <hycrafthd@live.de>
2020-06-25 23:08:16 +0200

    Register with deferred register now

--- src/main/java/info/u_team/u_team_core/intern/init/UCoreRecipeSerializers.java
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreRecipeSerializers.java
@@ -5,6 +5,7 @@ import info.u_team.u_team_core.intern.recipe.*;
 import info.u_team.u_team_core.recipeserializer.USpecialRecipeSerializer;
 import net.minecraft.item.crafting.IRecipeSerializer;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.*;
@@ -14,15 +15,15 @@ public class UCoreRecipeSerializers {
 	
 	public static final DeferredRegister<IRecipeSerializer<?>> RECIPE_SERIALIZERS = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, UCoreMain.MODID);
 	
-	public static final USpecialRecipeSerializer<DyeableItemDyeRecipe> CRAFTING_SPECIAL_ITEMDYE = new USpecialRecipeSerializer<>("crafting_special_itemdye", DyeableItemDyeRecipe::new);
+	public static final RegistryObject<USpecialRecipeSerializer<DyeableItemDyeRecipe>> CRAFTING_SPECIAL_ITEMDYE = RECIPE_SERIALIZERS.register("crafting_special_itemdye", () -> new USpecialRecipeSerializer<>("crafting_special_itemdye", DyeableItemDyeRecipe::new));
 	
-	public static final NoMirrorShapedRecipe.Serializer NO_MIRROR_SHAPED = new NoMirrorShapedRecipe.Serializer("crafting_shaped_no_mirror");
+	public static final RegistryObject<NoMirrorShapedRecipe.Serializer> NO_MIRROR_SHAPED = RECIPE_SERIALIZERS.register("crafting_shaped_no_mirror", () -> new NoMirrorShapedRecipe.Serializer("crafting_shaped_no_mirror"));
 	
 	public static void register(IEventBus bus) {
 		RECIPE_SERIALIZERS.register(bus);
 	}
 	
-//	@SubscribeEvent
+	// @SubscribeEvent
 //	public static void register(Register<IRecipeSerializer<?>> event) {
 //		BaseRegistryUtil.getAllGenericRegistryEntriesAndApplyNames(UCoreMain.MODID, IRecipeSerializer.class).forEach(event.getRegistry()::register);
 //		CraftingHelper.register(new ResourceLocation(UCoreMain.MODID, "item"), ItemIngredient.Serializer.INSTANCE);
