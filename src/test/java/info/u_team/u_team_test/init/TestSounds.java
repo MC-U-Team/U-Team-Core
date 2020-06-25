8e4230e8012a5dbfab3aef784a9e5495a7be6fbc
HyCraftHD <hycrafthd@live.de>
2020-06-25 14:21:31 +0200

    Add deferred register to test sounds

--- src/test/java/info/u_team/u_team_test/init/TestSounds.java
+++ src/test/java/info/u_team/u_team_test/init/TestSounds.java
@@ -1,22 +1,22 @@
 package info.u_team.u_team_test.init;
 
 import info.u_team.u_team_core.soundevent.USoundEvent;
-import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
 import info.u_team.u_team_test.TestMod;
 import net.minecraft.util.SoundEvent;
-import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.registries.*;
 
 @EventBusSubscriber(modid = TestMod.MODID, bus = Bus.MOD)
 public class TestSounds {
 	
+	public static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, TestMod.MODID);
+	
 	public static final SoundEvent BETTER_ENDERPEARL_USE = new USoundEvent("better_enderpearl_use");
 	
-	@SubscribeEvent
-	public static void register(Register<SoundEvent> event) {
-		BaseRegistryUtil.getAllRegistryEntriesAndApplyNames(TestMod.MODID, SoundEvent.class).forEach(event.getRegistry()::register);
+	public static void register(IEventBus bus) {
+		SOUND_EVENTS.register(bus);
 	}
 	
 }
