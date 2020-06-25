23799a2640b61509dd36271d60b6bc080eb40357
HyCraftHD <hycrafthd@live.de>
2020-06-25 14:11:33 +0200

    Add deferred register to mod dimensions

--- src/test/java/info/u_team/u_team_test/init/TestModDimensions.java
+++ src/test/java/info/u_team/u_team_test/init/TestModDimensions.java
@@ -4,20 +4,28 @@ import info.u_team.u_team_core.dimension.UModDimension;
 import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
 import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.dimension.BasicDimension;
+import net.minecraft.potion.Effect;
 import net.minecraftforge.common.ModDimension;
 import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.*;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.registries.*;
 
 @EventBusSubscriber(modid = TestMod.MODID, bus = Bus.MOD)
 public class TestModDimensions {
 	
+	public static final DeferredRegister<ModDimension> MOD_DIMENSIONS = DeferredRegister.create(ForgeRegistries.MOD_DIMENSIONS, TestMod.MODID);
+	
 	public static final ModDimension BASIC = new UModDimension("basic", BasicDimension::new);
 	
-	@SubscribeEvent
-	public static void register(Register<ModDimension> event) {
-		BaseRegistryUtil.getAllRegistryEntriesAndApplyNames(TestMod.MODID, ModDimension.class).forEach(event.getRegistry()::register);
+	public static void register(IEventBus bus) {
+		MOD_DIMENSIONS.register(bus);
 	}
 	
+//	@SubscribeEvent
+//	public static void register(Register<ModDimension> event) {
+//		BaseRegistryUtil.getAllRegistryEntriesAndApplyNames(TestMod.MODID, ModDimension.class).forEach(event.getRegistry()::register);
+//	}
+//	
 }
