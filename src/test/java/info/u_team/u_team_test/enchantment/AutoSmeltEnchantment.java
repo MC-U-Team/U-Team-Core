2addf224fac721e16fb9bced8b861d85f44e9388
HyCraftHD <hycrafthd@live.de>
2020-06-25 13:19:53 +0200

    Remove UEnchantment as its not unnessessary for the autosmelt enchantment using the deferred register

--- src/test/java/info/u_team/u_team_test/enchantment/AutoSmeltEnchantment.java
+++ src/test/java/info/u_team/u_team_test/enchantment/AutoSmeltEnchantment.java
@@ -1,12 +1,11 @@
 package info.u_team.u_team_test.enchantment;
 
-import info.u_team.u_team_core.enchantment.UEnchantment;
-import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.enchantment.*;
 import net.minecraft.inventory.EquipmentSlotType;
 
-public class AutoSmeltEnchantment extends UEnchantment {
+public class AutoSmeltEnchantment extends Enchantment {
 	
-	public AutoSmeltEnchantment(String name) {
-		super(name, Rarity.COMMON, EnchantmentType.DIGGER, new EquipmentSlotType[] { EquipmentSlotType.MAINHAND });
+	public AutoSmeltEnchantment() {
+		super(Rarity.COMMON, EnchantmentType.DIGGER, new EquipmentSlotType[] { EquipmentSlotType.MAINHAND });
 	}
 }
