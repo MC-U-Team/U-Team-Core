c68fae25d00ca1663d8ce26c26d85bb26d8fe326
HyCraftHD <hycrafthd@live.de>
2020-06-25 14:03:07 +0200

    Add deferred register to test global loot modifiers

--- src/test/java/info/u_team/u_team_test/init/TestGlobalLootModifiers.java
+++ src/test/java/info/u_team/u_team_test/init/TestGlobalLootModifiers.java
@@ -4,16 +4,22 @@ import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.global_loot_modifier.AutoSmeltLootModifier;
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.*;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.registries.*;
 
-@EventBusSubscriber(modid = TestMod.MODID, bus = Bus.MOD)
 public class TestGlobalLootModifiers {
 	
-	@SubscribeEvent
-	public static void register(Register<GlobalLootModifierSerializer<?>> event) {
-		event.getRegistry().register(new AutoSmeltLootModifier.Serializer().setRegistryName(TestMod.MODID, "auto_smelt"));
+	public static final DeferredRegister<GlobalLootModifierSerializer<?>> GLOBAL_LOOT_MODIFIER_SERIALIZERS = DeferredRegister.create(ForgeRegistries.LOOT_MODIFIER_SERIALIZERS, TestMod.MODID);
+	
+	public static void register(IEventBus bus) {
+		GLOBAL_LOOT_MODIFIER_SERIALIZERS.register(bus);
 	}
+//	
+//	@SubscribeEvent
+//	public static void register(Register<GlobalLootModifierSerializer<?>> event) {
+//		event.getRegistry().register(new AutoSmeltLootModifier.Serializer().setRegistryName(TestMod.MODID, "auto_smelt"));
+//	}
 	
 }
