5e41868411e7c48500971c15511a16583f2602db
HyCraftHD <hycrafthd@live.de>
2020-06-26 16:55:48 +0200

    Use the common deferred register now in UCoreRecipeSerializers

--- src/main/java/info/u_team/u_team_core/intern/init/UCoreRecipeSerializers.java
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreRecipeSerializers.java
@@ -3,6 +3,7 @@ package info.u_team.u_team_core.intern.init;
 import info.u_team.u_team_core.UCoreMain;
 import info.u_team.u_team_core.ingredient.ItemIngredient;
 import info.u_team.u_team_core.intern.recipe.*;
+import info.u_team.u_team_core.util.registry.CommonDeferredRegister;
 import net.minecraft.item.crafting.*;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.crafting.CraftingHelper;
@@ -11,12 +12,12 @@ import net.minecraftforge.eventbus.api.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.registries.*;
+import net.minecraftforge.registries.ForgeRegistries;
 
 @EventBusSubscriber(modid = UCoreMain.MODID, bus = Bus.MOD)
 public class UCoreRecipeSerializers {
 	
-	public static final DeferredRegister<IRecipeSerializer<?>> RECIPE_SERIALIZERS = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, UCoreMain.MODID);
+	public static final CommonDeferredRegister<IRecipeSerializer<?>> RECIPE_SERIALIZERS = CommonDeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, UCoreMain.MODID);
 	
 	public static final RegistryObject<SpecialRecipeSerializer<DyeableItemDyeRecipe>> CRAFTING_SPECIAL_ITEMDYE = RECIPE_SERIALIZERS.register("crafting_special_itemdye", () -> new SpecialRecipeSerializer<>(DyeableItemDyeRecipe::new));
 	
