5ea60da0cc937450464b2cb3e1e2af112225ff99
HyCraftHD <hycrafthd@live.de>
2020-06-26 17:12:57 +0200

    Add registerScreen Method in the client registry with a supplier type

--- src/main/java/info/u_team/u_team_core/util/registry/ClientRegistry.java
+++ src/main/java/info/u_team/u_team_core/util/registry/ClientRegistry.java
@@ -2,15 +2,22 @@ package info.u_team.u_team_core.util.registry;
 
 import java.util.function.*;
 
+import net.minecraft.client.gui.*;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.renderer.tileentity.*;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.*;
+import net.minecraft.inventory.container.*;
 import net.minecraft.tileentity.*;
 import net.minecraftforge.api.distmarker.*;
 import net.minecraftforge.fml.client.registry.*;
 
 @OnlyIn(Dist.CLIENT)
 public class ClientRegistry {
+	
+	public static <M extends Container, U extends Screen & IHasContainer<M>> void registryScreen(Supplier<? extends ContainerType<? extends M>> supplier, ScreenManager.IScreenFactory<M, U> factory) {
+		ScreenManager.registerFactory(supplier.get(), factory);
+	}
 	
 	public static <T extends Entity> void registerEntityRenderer(Supplier<? extends EntityType<T>> supplier, IRenderFactory<? super T> rendererFactory) {
 		RenderingRegistry.registerEntityRenderingHandler(supplier.get(), rendererFactory);
