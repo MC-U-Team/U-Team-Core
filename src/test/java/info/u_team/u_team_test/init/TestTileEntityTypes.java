0c4b66d45523a936186f2b9c04b587043931cad5
HyCraftHD <hycrafthd@live.de>
2020-06-26 16:44:16 +0200

    Use the new tile entity type deferred register now in test tile entity types

--- src/test/java/info/u_team/u_team_test/init/TestTileEntityTypes.java
+++ src/test/java/info/u_team/u_team_test/init/TestTileEntityTypes.java
@@ -1,22 +1,21 @@
 package info.u_team.u_team_test.init;
 
-import info.u_team.u_team_core.util.registry.CommonDeferredRegister;
+import info.u_team.u_team_core.util.registry.TileEntityTypeDeferredRegister;
 import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.tileentity.*;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.registries.ForgeRegistries;
 
 public class TestTileEntityTypes {
 	
-	public static final CommonDeferredRegister<TileEntityType<?>> TILE_ENTITY_TYPES = CommonDeferredRegister.create(ForgeRegistries.TILE_ENTITIES, TestMod.MODID);
+	public static final TileEntityTypeDeferredRegister TILE_ENTITY_TYPES = TileEntityTypeDeferredRegister.create(TestMod.MODID);
 	
-	public static final RegistryObject<TileEntityType<BasicTileEntityTileEntity>> BASIC = TILE_ENTITY_TYPES.register("tileentity", () -> TileEntityType.Builder.create(BasicTileEntityTileEntity::new, TestBlocks.BASIC_TILEENTITY.get()).build(null));
+	public static final RegistryObject<TileEntityType<BasicTileEntityTileEntity>> BASIC = TILE_ENTITY_TYPES.register("tileentity", () -> TileEntityType.Builder.create(BasicTileEntityTileEntity::new, TestBlocks.BASIC_TILEENTITY.get()));
 	
-	public static final RegistryObject<TileEntityType<BasicEnergyCreatorTileEntity>> BASIC_ENERGY_CREATOR = TILE_ENTITY_TYPES.register("energy_creator", () -> TileEntityType.Builder.create(BasicEnergyCreatorTileEntity::new, TestBlocks.BASIC_ENERGY_CREATOR.get()).build(null));
+	public static final RegistryObject<TileEntityType<BasicEnergyCreatorTileEntity>> BASIC_ENERGY_CREATOR = TILE_ENTITY_TYPES.register("energy_creator", () -> TileEntityType.Builder.create(BasicEnergyCreatorTileEntity::new, TestBlocks.BASIC_ENERGY_CREATOR.get()));
 	
-	public static final RegistryObject<TileEntityType<BasicFluidInventoryTileEntity>> BASIC_FLUID_INVENTORY = TILE_ENTITY_TYPES.register("fluid_inventory", () -> TileEntityType.Builder.create(BasicFluidInventoryTileEntity::new, TestBlocks.BASIC_FLUID_INVENTORY.get()).build(null));
+	public static final RegistryObject<TileEntityType<BasicFluidInventoryTileEntity>> BASIC_FLUID_INVENTORY = TILE_ENTITY_TYPES.register("fluid_inventory", () -> TileEntityType.Builder.create(BasicFluidInventoryTileEntity::new, TestBlocks.BASIC_FLUID_INVENTORY.get()));
 	
 	public static void register(IEventBus bus) {
 		TILE_ENTITY_TYPES.register(bus);
