5ac931c8b8e105e88d39b58ae5c49b9bfcf4c13f
HyCraftHD <hycrafthd@live.de>
2020-06-26 16:44:45 +0200

    Fix imports in test sounds

--- src/test/java/info/u_team/u_team_test/init/TestSounds.java
+++ src/test/java/info/u_team/u_team_test/init/TestSounds.java
@@ -2,7 +2,7 @@ package info.u_team.u_team_test.init;
 
 import info.u_team.u_team_core.util.registry.CommonDeferredRegister;
 import info.u_team.u_team_test.TestMod;
-import net.minecraft.util.*;
+import net.minecraft.util.SoundEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
