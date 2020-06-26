ccfce36fdf276c07c4c160f0d15091581b91aa35
HyCraftHD <hycrafthd@live.de>
2020-06-26 16:20:16 +0200

    Use the CommonDeferredRegister in the armor and tool set creator

--- src/main/java/info/u_team/u_team_core/item/armor/ArmorSetCreator.java
+++ src/main/java/info/u_team/u_team_core/item/armor/ArmorSetCreator.java
@@ -1,18 +1,18 @@
 package info.u_team.u_team_core.item.armor;
 
 import info.u_team.u_team_core.util.ItemProperties;
+import info.u_team.u_team_core.util.registry.CommonDeferredRegister;
 import net.minecraft.item.*;
 import net.minecraft.item.Item.Properties;
 import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.registries.DeferredRegister;
 
 public class ArmorSetCreator {
 	
-	public static ArmorSet create(DeferredRegister<Item> register, String name, Properties properties, IArmorMaterial material) {
+	public static ArmorSet create(CommonDeferredRegister<Item> register, String name, Properties properties, IArmorMaterial material) {
 		return create(register, name, null, properties, material);
 	}
 	
-	public static ArmorSet create(DeferredRegister<Item> register, String name, ItemGroup group, Properties properties, IArmorMaterial material) {
+	public static ArmorSet create(CommonDeferredRegister<Item> register, String name, ItemGroup group, Properties properties, IArmorMaterial material) {
 		final RegistryObject<UHelmetItem> helmet = register.register(name + "_helmet", () -> new UHelmetItem(name, group, new ItemProperties(properties), material));
 		final RegistryObject<UChestplateItem> chestplate = register.register(name + "_chestplate", () -> new UChestplateItem(name, group, new ItemProperties(properties), material));
 		final RegistryObject<ULeggingsItem> leggings = register.register(name + "_leggings", () -> new ULeggingsItem(name, group, new ItemProperties(properties), material));
