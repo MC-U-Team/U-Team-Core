2edb24d16bbcc6d1996c8f69b074290fa524a625
HyCraftHD <hycrafthd@live.de>
2020-06-26 13:13:24 +0200

    Rearange some methods. Also use the lazy optional only for the cap

--- src/test/java/info/u_team/u_team_test/tileentity/BasicTileEntityTileEntity.java
+++ src/test/java/info/u_team/u_team_test/tileentity/BasicTileEntityTileEntity.java
@@ -19,22 +19,16 @@ import net.minecraftforge.items.CapabilityItemHandler;
 
 public class BasicTileEntityTileEntity extends UTileEntity implements IInitSyncedTileEntity, ITickableTileEntity {
 	
-	private final LazyOptional<TileEntityUItemStackHandler> slots = LazyOptional.of(() -> new TileEntityUItemStackHandler(18, this));
+	private final TileEntityUItemStackHandler slots;
+	
+	private final LazyOptional<TileEntityUItemStackHandler> slotsOptional;
 	
 	public int cooldown, value;
 	
 	public BasicTileEntityTileEntity() {
 		super(TestTileEntityTypes.BASIC.get());
-	}
-	
-	@Override
-	public Container createMenu(int windowid, PlayerInventory playerInventory, PlayerEntity player) {
-		return new BasicTileEntityContainer(windowid, playerInventory, this);
-	}
-	
-	@Override
-	public ITextComponent getDisplayName() {
-		return new StringTextComponent("Tile Entity");
+		slots = new TileEntityUItemStackHandler(18, this);
+		slotsOptional = LazyOptional.of(() -> slots);
 	}
 	
 	@Override
@@ -68,34 +62,49 @@ public class BasicTileEntityTileEntity extends UTileEntity implements IInitSynce
 	}
 	
 	@Override
-	public void readNBT(CompoundNBT compound) {
-		slots.ifPresent(handler -> handler.deserializeNBT(compound.getCompound("inventory")));
-		value = compound.getInt("value");
-		cooldown = compound.getInt("cooldown");
-	}
-	
-	@Override
 	public void writeNBT(CompoundNBT compound) {
-		slots.ifPresent(handler -> compound.put("inventory", handler.serializeNBT()));
+		compound.put("inventory", slots.serializeNBT());
 		compound.putInt("value", value);
 		compound.putInt("cooldown", cooldown);
 	}
 	
 	@Override
-	public <T> LazyOptional<T> getCapability(Capability<T> capability, Direction side) {
-		if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
-			return slots.cast();
-		}
-		return super.getCapability(capability, side);
+	public void readNBT(CompoundNBT compound) {
+		value = compound.getInt("value");
+		cooldown = compound.getInt("cooldown");
+		slots.deserializeNBT(compound.getCompound("inventory"));
 	}
 	
-	public LazyOptional<TileEntityUItemStackHandler> getSlots() {
+	public TileEntityUItemStackHandler getSlots() {
 		return slots;
+	}
+	
+	// Capability
+	
+	@Override
+	public <X> LazyOptional<X> getCapability(Capability<X> capability, Direction side) {
+		if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+			return slotsOptional.cast();
+		} else {
+			return super.getCapability(capability, side);
+		}
 	}
 	
 	@Override
 	public void remove() {
 		super.remove();
-		slots.invalidate();
+		slotsOptional.invalidate();
+	}
+	
+	// Container
+	
+	@Override
+	public Container createMenu(int windowid, PlayerInventory playerInventory, PlayerEntity player) {
+		return new BasicTileEntityContainer(windowid, playerInventory, this);
+	}
+	
+	@Override
+	public ITextComponent getDisplayName() {
+		return new StringTextComponent("Tile Entity");
 	}
 }
