e34f0202f23329ed3b73db41237d36ae41f3fb7c
HyCraftHD <hycrafthd@live.de>
2020-06-26 17:14:04 +0200

    Use the ClientRegistry Method in screen register to make it allow the registry object supplier

--- src/test/java/info/u_team/u_team_test/init/TestScreens.java
+++ src/test/java/info/u_team/u_team_test/init/TestScreens.java
@@ -1,9 +1,8 @@
 package info.u_team.u_team_test.init;
 
-import info.u_team.u_team_core.util.registry.MainThreadWorker;
+import info.u_team.u_team_core.util.registry.*;
 import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.screen.*;
-import net.minecraft.client.gui.ScreenManager;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -16,9 +15,9 @@ public class TestScreens {
 	@SubscribeEvent
 	public static void register(FMLClientSetupEvent event) {
 		MainThreadWorker.run(() -> {
-			ScreenManager.registerFactory(TestContainers.BASIC.get(), BasicTileEntityScreen::new);
-			ScreenManager.registerFactory(TestContainers.BASIC_ENERGY_CREATOR.get(), BasicEnergyCreatorScreen::new);
-			ScreenManager.registerFactory(TestContainers.BASIC_FLUID_INVENTORY.get(), BasicFluidInventoryScreen::new);
+			ClientRegistry.registryScreen(TestContainers.BASIC, BasicTileEntityScreen::new);
+			ClientRegistry.registryScreen(TestContainers.BASIC_ENERGY_CREATOR, BasicEnergyCreatorScreen::new);
+			ClientRegistry.registryScreen(TestContainers.BASIC_FLUID_INVENTORY, BasicFluidInventoryScreen::new);
 		});
 	}
 }
