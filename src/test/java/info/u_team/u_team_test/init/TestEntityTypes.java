09b47e8149f6090d5604a53b0d64a17f7f4df2ac
HyCraftHD <hycrafthd@live.de>
2020-06-26 02:15:36 +0200

    Use the normal builder now (maybe we make a custom deferred register for that for easier use)

--- src/test/java/info/u_team/u_team_test/init/TestEntityTypes.java
+++ src/test/java/info/u_team/u_team_test/init/TestEntityTypes.java
@@ -1,6 +1,5 @@
 package info.u_team.u_team_test.init;
 
-import info.u_team.u_team_core.entitytype.UEntityType;
 import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.entity.BetterEnderPearlEntity;
 import net.minecraft.entity.*;
@@ -12,7 +11,7 @@ public class TestEntityTypes {
 	
 	public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES, TestMod.MODID);
 	
-	public static final RegistryObject<EntityType<BetterEnderPearlEntity>> BETTER_ENDERPEARL = ENTITY_TYPES.register("better_enderpearl", () -> UEntityType.UBuilder.<BetterEnderPearlEntity> create("better_enderpearl", BetterEnderPearlEntity::new, EntityClassification.MISC).size(0.25F, 0.25F).setTrackingRange(128).setUpdateInterval(20).setShouldReceiveVelocityUpdates(true).build());
+	public static final RegistryObject<EntityType<BetterEnderPearlEntity>> BETTER_ENDERPEARL = ENTITY_TYPES.register("better_enderpearl", () -> EntityType.Builder.<BetterEnderPearlEntity> create(BetterEnderPearlEntity::new, EntityClassification.MISC).size(0.25F, 0.25F).setTrackingRange(128).setUpdateInterval(20).setShouldReceiveVelocityUpdates(true).disableSerialization().build(null));
 	
 	public static void register(IEventBus bus) {
 		ENTITY_TYPES.register(bus);
