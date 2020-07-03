4cb0732f4aabfc62299aa6d4609d23e401346b6d
HyCraftHD <hycrafthd@live.de>
2020-07-04 00:44:32 +0200

    Add matrix stack to gui util

--- src/main/java/info/u_team/u_team_core/util/GuiUtil.java
+++ src/main/java/info/u_team/u_team_core/util/GuiUtil.java
@@ -1,5 +1,7 @@
 package info.u_team.u_team_core.util;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+
 import net.minecraft.client.gui.AbstractGui;
 import net.minecraftforge.api.distmarker.*;
 
@@ -19,21 +21,21 @@ public class GuiUtil {
 	 * @param width
 	 * @param height
 	 */
-	public static void drawContainerBorder(int x, int y, int width, int height) {
+	public static void drawContainerBorder(MatrixStack matrixStack, int x, int y, int width, int height) {
 		// ARGB ... Strange formal
 		
 		final int darkColor = 0xFF373737;
 		final int mediumColor = 0xFF8B8B8B;
 		final int brightColor = 0xFFFFFFFF;
 		
-		AbstractGui.fill(x, y, x + width - 1, y + 1, darkColor);
-		AbstractGui.fill(x, y, x + 1, y + height - 1, darkColor);
+		AbstractGui.func_238467_a_(matrixStack, x, y, x + width - 1, y + 1, darkColor);
+		AbstractGui.func_238467_a_(matrixStack, x, y, x + 1, y + height - 1, darkColor);
 		
-		AbstractGui.fill(x + width - 1, y, x + width, y + 1, mediumColor);
-		AbstractGui.fill(x, y + height - 1, x + 1, y + height, mediumColor);
+		AbstractGui.func_238467_a_(matrixStack, x + width - 1, y, x + width, y + 1, mediumColor);
+		AbstractGui.func_238467_a_(matrixStack, x, y + height - 1, x + 1, y + height, mediumColor);
 		
-		AbstractGui.fill(x + 1, y + height, x + width - 1, y + height - 1, brightColor);
-		AbstractGui.fill(x + width - 1, y + 1, x + width, y + height, brightColor);
+		AbstractGui.func_238467_a_(matrixStack, x + 1, y + height, x + width - 1, y + height - 1, brightColor);
+		AbstractGui.func_238467_a_(matrixStack, x + width - 1, y + 1, x + width, y + height, brightColor);
 	}
 	
 }
