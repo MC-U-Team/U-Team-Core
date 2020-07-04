7b6d0f832e983d7a8e00cfea456d7caa8101eb0a
HyCraftHD <hycrafthd@live.de>
2020-07-05 00:14:48 +0200

    Fix image button

--- src/main/java/info/u_team/u_team_core/gui/elements/ImageButton.java
+++ src/main/java/info/u_team/u_team_core/gui/elements/ImageButton.java
@@ -2,8 +2,11 @@ package info.u_team.u_team_core.gui.elements;
 
 import org.lwjgl.opengl.GL11;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.*;
 
 public class ImageButton extends UButton {
 	
@@ -14,7 +17,7 @@ public class ImageButton extends UButton {
 	}
 	
 	public ImageButton(int x, int y, int width, int height, ResourceLocation resource, IPressable pressable) {
-		super(x, y, width, height, "", pressable);
+		super(x, y, width, height, ITextComponent.func_241827_a_(null), pressable);
 		this.resource = resource;
 	}
 	
@@ -27,11 +30,11 @@ public class ImageButton extends UButton {
 	}
 	
 	@Override
-	public void renderButton(int mouseX, int mouseY, float partial) {
-		super.renderButton(mouseX, mouseY, partial);
+	public void func_230431_b_(MatrixStack matrixStack, int mouseX, int mouseY, float partial) {
+		super.func_230431_b_(matrixStack, mouseX, mouseY, partial);
 		resetColor();
 		Minecraft.getInstance().getTextureManager().bindTexture(resource);
-		innerBlit(x + 2, x + width - 2, y + 2, y + height - 2, 0, 0, 1, 0, 1);
+		func_238461_a_(matrixStack.getLast().getMatrix(), field_230690_l_ + 2, field_230690_l_ + field_230688_j_ - 2, field_230691_m_ + 2, field_230691_m_ + field_230689_k_ - 2, 0, 0, 1, 0, 1);
 	}
 	
 	protected void resetColor() {
