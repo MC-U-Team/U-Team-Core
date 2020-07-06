457081057e39b2be27941b8606f77ba125020d39
HyCraftHD <hycrafthd@live.de>
2020-07-06 23:16:45 +0200

    Remove old language keys from the language provider

--- src/main/java/info/u_team/u_team_core/intern/data/provider/UCoreLanguagesProvider.java
+++ src/main/java/info/u_team/u_team_core/intern/data/provider/UCoreLanguagesProvider.java
@@ -38,19 +38,5 @@ public class UCoreLanguagesProvider extends CommonLanguagesProvider {
 		add(dimTeleportSuccess + "multiple", "Teleported %s entities to %s");
 		add(dimTeleportSuccess + "position.single", "Teleported %s to %s at %s, %s, %s");
 		add(dimTeleportSuccess + "position.multiple", "Teleported %s entities to %s at %s, %s, %s");
-		
-		// Locate biome
-		final String locateBiomeSuccess = "commands.uteamcore.locatebiome.success";
-		final String locateBiomeFailed = "commands.uteamcore.locatebiome.failed";
-		
-		add(locateBiomeSuccess, "The nearest %s is at %s (%s blocks away)");
-		add(locateBiomeFailed, "Could not find a biome of type \"%s\" within reasonable distance");
-		
-		// Locate structure
-		final String locateStructureSuccess = "commands.uteamcore.locatestructure.success";
-		final String locateStructureFailed = "commands.uteamcore.locatestructure.failed";
-		
-		add(locateStructureSuccess, "The nearest %s is at %s (%s blocks away)");
-		add(locateStructureFailed, "Could not find that structure nearby");
 	}
 }
