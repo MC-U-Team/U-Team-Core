0af8b3ab0f1d509fd9425d031877870217f83eb1
HyCraftHD <hycrafthd@live.de>
2020-07-06 23:40:30 +0200

    Fix imports in auto smelt loot modifier

--- src/test/java/info/u_team/u_team_test/global_loot_modifier/AutoSmeltLootModifier.java
+++ src/test/java/info/u_team/u_team_test/global_loot_modifier/AutoSmeltLootModifier.java
@@ -8,9 +8,9 @@ import com.google.gson.JsonObject;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.*;
+import net.minecraft.loot.LootContext;
+import net.minecraft.loot.conditions.ILootCondition;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.storage.loot.LootContext;
-import net.minecraft.world.storage.loot.conditions.ILootCondition;
 import net.minecraftforge.common.loot.*;
 import net.minecraftforge.items.ItemHandlerHelper;
 
