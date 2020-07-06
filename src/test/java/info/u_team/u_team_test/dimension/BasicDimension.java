d4bc0930bb49a758eaf9b3fd26d16184f5fff961
HyCraftHD <hycrafthd@live.de>
2020-07-06 23:47:49 +0200

    Comment out basic dimension

--- src/test/java/info/u_team/u_team_test/dimension/BasicDimension.java
+++ src/test/java/info/u_team/u_team_test/dimension/BasicDimension.java
@@ -1,14 +1,8 @@
 package info.u_team.u_team_test.dimension;
 
-import info.u_team.u_team_test.init.TestBiomes;
-import net.minecraft.world.World;
-import net.minecraft.world.biome.provider.*;
-import net.minecraft.world.dimension.*;
-import net.minecraft.world.gen.*;
-
-public class BasicDimension extends OverworldDimension {
-	
-	public BasicDimension(World world, DimensionType type) {
+public class BasicDimension /*extends OverworldDimension*/ {
+	// TODO wait for forge replacement
+	/*public BasicDimension(World world, DimensionType type) {
 		super(world, type);
 	}
 	
@@ -20,6 +14,6 @@ public class BasicDimension extends OverworldDimension {
 		final OverworldGenSettings overworldgensettings = chunkgenerator.createSettings();
 		final SingleBiomeProviderSettings overworldbiomeprovidersettings = biomeprovidertype.createSettings(world.getWorldInfo()).setBiome(TestBiomes.BASIC.get());
 		return chunkgenerator.create(this.world, biomeprovidertype.create(overworldbiomeprovidersettings), overworldgensettings);
-	}
+	}*/
 	
 }
