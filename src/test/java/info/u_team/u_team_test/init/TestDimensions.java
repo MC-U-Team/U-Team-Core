3e2c8d19a348aa429bf9949cd41ac50fad231e67
HyCraftHD <hycrafthd@live.de>
2020-07-06 23:47:15 +0200

    Comment out dimensions, as forges replacement is not there yet

--- src/test/java/info/u_team/u_team_test/init/TestDimensions.java
+++ src/test/java/info/u_team/u_team_test/init/TestDimensions.java
@@ -1,20 +1,19 @@
 package info.u_team.u_team_test.init;
 
 import info.u_team.u_team_test.TestMod;
-import net.minecraftforge.common.DimensionManager;
-import net.minecraftforge.event.world.RegisterDimensionsEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
 @EventBusSubscriber(modid = TestMod.MODID, bus = Bus.FORGE)
 public class TestDimensions {
 	
-	@SuppressWarnings("deprecation")
-	@SubscribeEvent
-	public static void on(RegisterDimensionsEvent event) {
-		if (!DimensionManager.getRegistry().keySet().contains(TestModDimensions.BASIC.getId())) { // How do we know when the dimension needs to be registered??
-			DimensionManager.registerDimension(TestModDimensions.BASIC.getId(), TestModDimensions.BASIC.get(), null, true);
-		}
-	}
+	// TODO wait for forge replacement
+	// @SuppressWarnings("deprecation")
+	// @SubscribeEvent
+	// public static void on(RegisterDimensionsEvent event) {
+	// if (!DimensionManager.getRegistry().keySet().contains(TestModDimensions.BASIC.getId())) { // How do we know when the
+	// dimension needs to be registered??
+	// DimensionManager.registerDimension(TestModDimensions.BASIC.getId(), TestModDimensions.BASIC.get(), null, true);
+	// }
+	// }
 }
--- src/test/java/info/u_team/u_team_test/init/TestModDimensions.java
+++ src/test/java/info/u_team/u_team_test/init/TestModDimensions.java
@@ -1,20 +1,16 @@
 package info.u_team.u_team_test.init;
 
-import info.u_team.u_team_core.util.registry.CommonDeferredRegister;
-import info.u_team.u_team_test.TestMod;
-import info.u_team.u_team_test.dimension.BasicDimension;
-import net.minecraftforge.common.ModDimension;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.registries.ForgeRegistries;
 
 public class TestModDimensions {
-	
-	public static final CommonDeferredRegister<ModDimension> MOD_DIMENSIONS = CommonDeferredRegister.create(ForgeRegistries.MOD_DIMENSIONS, TestMod.MODID);
-	
-	public static final RegistryObject<ModDimension> BASIC = MOD_DIMENSIONS.register("basic", () -> ModDimension.withFactory(BasicDimension::new));
+	// TODO wait for forge replacement
+	// public static final CommonDeferredRegister<ModDimension> MOD_DIMENSIONS =
+	// CommonDeferredRegister.create(ForgeRegistries.MOD_DIMENSIONS, TestMod.MODID);
+	//
+	// public static final RegistryObject<ModDimension> BASIC = MOD_DIMENSIONS.register("basic", () ->
+	// ModDimension.withFactory(BasicDimension::new));
 	
 	public static void register(IEventBus bus) {
-		MOD_DIMENSIONS.register(bus);
+		// MOD_DIMENSIONS.register(bus);
 	}
 }
