df6a9b95bfbc2748361e27ea37268a0ddd09f21c
HyCraftHD <hycrafthd@live.de>
2020-07-06 23:40:58 +0200

    Fix class rename in basic item

--- src/test/java/info/u_team/u_team_test/item/BasicItem.java
+++ src/test/java/info/u_team/u_team_test/item/BasicItem.java
@@ -6,8 +6,9 @@ import info.u_team.u_team_test.init.TestItemGroups;
 import net.minecraft.entity.player.*;
 import net.minecraft.item.*;
 import net.minecraft.util.*;
-import net.minecraft.util.math.*;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.RayTraceResult.Type;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 
@@ -29,7 +30,7 @@ public class BasicItem extends UItem {
 				return new ActionResult<>(ActionResultType.FAIL, stack);
 			}
 			
-			final Vec3d pos = rayTrace.getHitVec();
+			final Vector3d pos = rayTrace.getHitVec();
 			((ServerPlayerEntity) player).connection.setPlayerLocation(pos.getX(), pos.getY() + 1, pos.getZ(), player.rotationYaw, player.rotationPitch);
 			stack.damageItem(1, player, unused -> {
 			});
