7afcacb4bd334e6eebe3253c94a9f5428b66e896
HyCraftHD <hycrafthd@live.de>
2020-07-07 12:39:59 +0200

    Update basic fluid inventory screen to UBasicContainerScreen and reduce code

--- src/test/java/info/u_team/u_team_test/screen/BasicFluidInventoryScreen.java
+++ src/test/java/info/u_team/u_team_test/screen/BasicFluidInventoryScreen.java
@@ -1,25 +1,15 @@
 package info.u_team.u_team_test.screen;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
-
-import info.u_team.u_team_core.screen.UContainerScreen;
+import info.u_team.u_team_core.screen.UBasicContainerScreen;
 import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.container.BasicFluidInventoryContainer;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 
-public class BasicFluidInventoryScreen extends UContainerScreen<BasicFluidInventoryContainer> {
+public class BasicFluidInventoryScreen extends UBasicContainerScreen<BasicFluidInventoryContainer> {
 	
 	public BasicFluidInventoryScreen(BasicFluidInventoryContainer container, PlayerInventory playerInventory, ITextComponent title) {
 		super(container, playerInventory, title, new ResourceLocation(TestMod.MODID, "textures/gui/fluid_inventory.png"));
-	}
-	
-	@Override
-	public void func_230430_a_(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
-		func_230446_a_(matrixStack);
-		super.func_230430_a_(matrixStack, mouseX, mouseY, partialTicks);
-		field_230710_m_.forEach(widget -> widget.func_230443_a_(matrixStack, mouseX, mouseY));
-		func_230459_a_(matrixStack, mouseX, mouseY);
 	}
 }
