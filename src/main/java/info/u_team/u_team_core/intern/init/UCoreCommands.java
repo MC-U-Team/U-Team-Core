8140ac85ce3e29b981805552ca33c5592d96a2ba
HyCraftHD <hycrafthd@live.de>
2020-07-16 13:02:29 +0200

    Fix command registry

--- src/main/java/info/u_team/u_team_core/intern/init/UCoreCommands.java
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreCommands.java
@@ -2,17 +2,17 @@ package info.u_team.u_team_core.intern.init;
 
 import info.u_team.u_team_core.UCoreMain;
 import info.u_team.u_team_core.intern.command.UTeamCoreCommand;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 
 @EventBusSubscriber(modid = UCoreMain.MODID, bus = Bus.FORGE)
 public class UCoreCommands {
 	
 	@SubscribeEvent
-	public static void on(FMLServerStartingEvent event) {
-		new UTeamCoreCommand(event.getCommandDispatcher());
+	public static void on(RegisterCommandsEvent event) {
+		new UTeamCoreCommand(event.getDispatcher());
 	}
 	
 }
