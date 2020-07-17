8e503433909f2339de907780526b6592d9037238
HyCraftHD <hycrafthd@live.de>
2020-07-17 17:45:29 +0200

    Add jei plugin

---
+++ src/integration/java/info/u_team/u_team_core/integration/jei/UTeamCoreJeiPlugin.java
@@ -0,0 +1,31 @@
+package info.u_team.u_team_core.integration.jei;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import info.u_team.u_team_core.UCoreMain;
+import info.u_team.u_team_core.api.dye.IDyeableItem;
+import mezz.jei.api.*;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.item.*;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@JeiPlugin
+public class UTeamCoreJeiPlugin implements IModPlugin {
+	
+	private final ResourceLocation id = new ResourceLocation(UCoreMain.MODID, "jei");
+	
+	@Override
+	public ResourceLocation getPluginUid() {
+		return id;
+	}
+	
+	@Override
+	public void registerRecipes(IRecipeRegistration registration) {
+		final List<ItemStack> items = ForgeRegistries.ITEMS.getValues().stream().filter(item -> item instanceof IDyeableItem || item instanceof IDyeableArmorItem).map(ItemStack::new).collect(Collectors.toList());
+		registration.addIngredientInfo(items, VanillaTypes.ITEM, "jei.uteamcore.dyeable.info");
+	}
+	
+}
