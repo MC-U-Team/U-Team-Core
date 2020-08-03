fbfeea425d9d607cd3b1b6dde3154d2b611d1a43
HyCraftHD <hycrafthd@live.de>
2020-08-03 17:13:24 +0200

    Add client and common bus register classes

---
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreClientBusRegister.java
@@ -0,0 +1,8 @@
+package info.u_team.u_team_core.intern.init;
+
+public class UCoreClientBusRegister {
+	
+	public static void register() {
+	}
+	
+}
---
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreCommonBusRegister.java
@@ -0,0 +1,7 @@
+package info.u_team.u_team_core.intern.init;
+
+public class UCoreCommonBusRegister {
+	
+	public static void register() {
+	}
+}
