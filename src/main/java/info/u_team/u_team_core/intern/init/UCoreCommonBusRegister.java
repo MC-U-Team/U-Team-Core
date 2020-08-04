e31d57ffb488edcc57476525e5cf461c5652706c
HyCraftHD <hycrafthd@live.de>
2020-08-04 17:16:39 +0200

    Now call the registerMod class in the common bus register

--- src/main/java/info/u_team/u_team_core/intern/init/UCoreCommonBusRegister.java
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreCommonBusRegister.java
@@ -1,7 +1,10 @@
 package info.u_team.u_team_core.intern.init;
 
+import info.u_team.u_team_core.util.registry.BusRegister;
+
 public class UCoreCommonBusRegister {
 	
 	public static void register() {
+		BusRegister.registerMod(UCoreNetwork::registerMod);
 	}
 }
