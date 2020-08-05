42c301d132d8d5c5be78d3049e5c33a05cb4f84d
HyCraftHD <hycrafthd@live.de>
2020-08-05 17:22:56 +0200

    Move the common register to the common bus register class

--- src/test/java/info/u_team/u_team_test/TestMod.java
+++ src/test/java/info/u_team/u_team_test/TestMod.java
@@ -1,6 +1,5 @@
 package info.u_team.u_team_test;
 
-import info.u_team.u_team_core.util.registry.BusRegister;
 import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import info.u_team.u_team_test.init.*;
 import net.minecraftforge.api.distmarker.Dist;
@@ -19,20 +18,5 @@ public class TestMod {
 		
 		TestCommonBusRegister.register();
 		DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> TestClientBusRegister::register);
-	}
-	
-	private void register() {
-		BusRegister.registerMod(TestBiomes::register);
-		BusRegister.registerMod(TestBlocks::register);
-		BusRegister.registerMod(TestContainers::register);
-		BusRegister.registerMod(TestEffects::register);
-		BusRegister.registerMod(TestEnchantments::register);
-		BusRegister.registerMod(TestEntityTypes::register);
-		BusRegister.registerMod(TestGlobalLootModifiers::register);
-		BusRegister.registerMod(TestItems::register);
-		BusRegister.registerMod(TestModDimensions::register);
-		BusRegister.registerMod(TestPotions::register);
-		BusRegister.registerMod(TestSounds::register);
-		BusRegister.registerMod(TestTileEntityTypes::register);
 	}
 }
--- src/test/java/info/u_team/u_team_test/init/TestCommonBusRegister.java
+++ src/test/java/info/u_team/u_team_test/init/TestCommonBusRegister.java
@@ -1,10 +1,22 @@
 package info.u_team.u_team_test.init;
 
+import info.u_team.u_team_core.util.registry.BusRegister;
 
 public class TestCommonBusRegister {
 	
 	public static void register() {
-		
+		BusRegister.registerMod(TestBiomes::register);
+		BusRegister.registerMod(TestBlocks::register);
+		BusRegister.registerMod(TestContainers::register);
+		BusRegister.registerMod(TestEffects::register);
+		BusRegister.registerMod(TestEnchantments::register);
+		BusRegister.registerMod(TestEntityTypes::register);
+		BusRegister.registerMod(TestGlobalLootModifiers::register);
+		BusRegister.registerMod(TestItems::register);
+		BusRegister.registerMod(TestModDimensions::register);
+		BusRegister.registerMod(TestPotions::register);
+		BusRegister.registerMod(TestSounds::register);
+		BusRegister.registerMod(TestTileEntityTypes::register);
 	}
 	
 }
