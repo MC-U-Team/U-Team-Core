81d94d6bb2a8f0a981b715586e5f4293f095c1f5
HyCraftHD <hycrafthd@live.de>
2020-08-05 17:32:36 +0200

    Add new method to test keys register

--- src/test/java/info/u_team/u_team_test/init/TestKeys.java
+++ src/test/java/info/u_team/u_team_test/init/TestKeys.java
@@ -10,19 +10,21 @@ import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.client.util.InputMappings;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.GuiScreenEvent.KeyboardKeyPressedEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.*;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@EventBusSubscriber(modid = TestMod.MODID, bus = Bus.MOD, value = Dist.CLIENT)
 public class TestKeys {
 	
 	public static final KeyBinding BASIC = new KeyBinding("Basic key", GLFW.GLFW_KEY_F8, "Basic category");
 	
-	@SubscribeEvent
-	public static void register(FMLClientSetupEvent event) {
+	public static void setup(FMLClientSetupEvent event) {
 		ClientRegistry.registerKeybinding(BASIC);
+	}
+	
+	public static void registerMod(IEventBus bus) {
+		bus.addListener(TestKeys::setup);
 	}
 	
 	@EventBusSubscriber(modid = TestMod.MODID, bus = Bus.FORGE, value = Dist.CLIENT)
