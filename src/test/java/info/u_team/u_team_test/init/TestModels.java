5647421faaaea16ad0e09b27897cd98f24eecd96
HyCraftHD <hycrafthd@live.de>
2020-08-05 17:35:05 +0200

    Use the new system now

--- src/test/java/info/u_team/u_team_test/init/TestModels.java
+++ src/test/java/info/u_team/u_team_test/init/TestModels.java
@@ -1,20 +1,18 @@
 package info.u_team.u_team_test.init;
 
 import info.u_team.u_team_core.util.registry.ClientRegistry;
-import info.u_team.u_team_test.TestMod;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.SpriteRenderer;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@EventBusSubscriber(modid = TestMod.MODID, bus = Bus.MOD, value = Dist.CLIENT)
 public class TestModels {
 	
-	@SubscribeEvent
-	public static void register(FMLClientSetupEvent event) {
+	private static void setup(FMLClientSetupEvent event) {
 		ClientRegistry.registerEntityRenderer(TestEntityTypes.BETTER_ENDERPEARL, manager -> new SpriteRenderer<>(manager, Minecraft.getInstance().getItemRenderer()));
+	}
+	
+	public static void registerMod(IEventBus bus) {
+		bus.addListener(TestModels::setup);
 	}
 }
