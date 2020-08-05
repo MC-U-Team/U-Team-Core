cc700e3188847b8fa44f442f9769bbd2a2b22494
HyCraftHD <hycrafthd@live.de>
2020-08-05 17:30:50 +0200

    Register test colors

--- src/test/java/info/u_team/u_team_test/init/TestClientBusRegister.java
+++ src/test/java/info/u_team/u_team_test/init/TestClientBusRegister.java
@@ -1,9 +1,11 @@
 package info.u_team.u_team_test.init;
 
+import info.u_team.u_team_core.util.registry.BusRegister;
+
 public class TestClientBusRegister {
 	
 	public static void register() {
-		
+		BusRegister.registerMod(TestColors::registerMod);
 	}
 	
 }
