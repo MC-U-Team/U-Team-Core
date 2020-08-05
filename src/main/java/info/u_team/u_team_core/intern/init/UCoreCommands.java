44daf36f3c98bac30a2a66d5e12d640c606e2ef7
HyCraftHD <hycrafthd@live.de>
2020-08-05 15:40:53 +0200

    Remove the annotations from u core commands and migrate to the new system

--- src/main/java/info/u_team/u_team_core/intern/init/UCoreCommands.java
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreCommands.java
@@ -1,18 +1,16 @@
 package info.u_team.u_team_core.intern.init;
 
-import info.u_team.u_team_core.UCoreMod;
 import info.u_team.u_team_core.intern.command.UTeamCoreCommand;
 import net.minecraftforge.event.RegisterCommandsEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.eventbus.api.IEventBus;
 
-@EventBusSubscriber(modid = UCoreMod.MODID, bus = Bus.FORGE)
 public class UCoreCommands {
 	
-	@SubscribeEvent
-	public static void on(RegisterCommandsEvent event) {
+	private static void onRegisterCommands(RegisterCommandsEvent event) {
 		new UTeamCoreCommand(event.getDispatcher());
 	}
 	
+	public static void registerForge(IEventBus bus) {
+		bus.addListener(UCoreCommands::onRegisterCommands);
+	}
 }
