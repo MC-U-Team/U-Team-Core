6451d56d21782dfce1acfd444f34c1e129068f84
HyCraftHD <hycrafthd@live.de>
2020-08-05 17:36:51 +0200

    Use the new system now for screens

--- src/test/java/info/u_team/u_team_test/init/TestScreens.java
+++ src/test/java/info/u_team/u_team_test/init/TestScreens.java
@@ -1,23 +1,21 @@
 package info.u_team.u_team_test.init;
 
 import info.u_team.u_team_core.util.registry.*;
-import info.u_team.u_team_test.TestMod;
 import info.u_team.u_team_test.screen.*;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@EventBusSubscriber(modid = TestMod.MODID, bus = Bus.MOD, value = Dist.CLIENT)
 public class TestScreens {
 	
-	@SubscribeEvent
-	public static void register(FMLClientSetupEvent event) {
+	public static void setup(FMLClientSetupEvent event) {
 		MainThreadWorker.run(() -> {
 			ClientRegistry.registerScreen(TestContainers.BASIC, BasicTileEntityScreen::new);
 			ClientRegistry.registerScreen(TestContainers.BASIC_ENERGY_CREATOR, BasicEnergyCreatorScreen::new);
 			ClientRegistry.registerScreen(TestContainers.BASIC_FLUID_INVENTORY, BasicFluidInventoryScreen::new);
 		});
+	}
+	
+	public static void registerMod(IEventBus bus) {
+		bus.addListener(TestScreens::setup);
 	}
 }
