1d9ea628ea3dc870327d08ab168b27e570ceae81
HyCraftHD <hycrafthd@live.de>
2020-08-07 16:31:39 +0200

    Add call to construct manager in test mod

--- src/test/java/info/u_team/u_team_test/TestMod.java
+++ src/test/java/info/u_team/u_team_test/TestMod.java
@@ -2,11 +2,9 @@ package info.u_team.u_team_test;
 
 import org.apache.logging.log4j.*;
 
+import info.u_team.u_team_core.construct.ConstructManager;
 import info.u_team.u_team_core.integration.IntegrationManager;
 import info.u_team.u_team_core.util.verify.JarSignVerifier;
-import info.u_team.u_team_test.init.*;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(TestMod.MODID)
@@ -20,8 +18,10 @@ public class TestMod {
 		
 		LOGGER.info("--------------------------------------- LOADING TEST MOD ---------------------------------------");
 		
-		TestCommonBusRegister.register();
-		DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> TestClientBusRegister::register);
+//		TestCommonBusRegister.register();
+//		DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> TestClientBusRegister::register);
+		
+		ConstructManager.constructConstructs(MODID);
 		
 		IntegrationManager.constructIntegrations(MODID);
 	}
