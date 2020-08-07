a5a8c0fce91e5ae4e1b4a5ca27b35e263072c1fe
HyCraftHD <hycrafthd@live.de>
2020-08-07 16:33:32 +0200

    Implement the client bus register in the construct annotation for test mod

--- src/test/java/info/u_team/u_team_test/init/TestClientBusRegister.java
+++ src/test/java/info/u_team/u_team_test/init/TestClientBusRegister.java
@@ -1,10 +1,13 @@
 package info.u_team.u_team_test.init;
 
+import info.u_team.u_team_core.api.construct.*;
 import info.u_team.u_team_core.util.registry.BusRegister;
 
-public class TestClientBusRegister {
+@Construct(client = true)
+public class TestClientBusRegister implements IModConstruct {
 	
-	public static void register() {
+	@Override
+	public void construct() {
 		BusRegister.registerMod(TestColors::registerMod);
 		BusRegister.registerMod(TestKeys::registerMod);
 		BusRegister.registerMod(TestModels::registerMod);
