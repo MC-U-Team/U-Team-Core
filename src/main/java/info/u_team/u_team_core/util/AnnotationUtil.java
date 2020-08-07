8314a2cecb1ab243e8504d823f6a70fdeef215d0
HyCraftHD <hycrafthd@live.de>
2020-08-07 16:17:42 +0200

    Add annotation util to get all annotations of a mod by type

---
+++ src/main/java/info/u_team/u_team_core/util/AnnotationUtil.java
@@ -0,0 +1,24 @@
+package info.u_team.u_team_core.util;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.objectweb.asm.Type;
+
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.forgespi.language.ModFileScanData.AnnotationData;
+
+public class AnnotationUtil {
+	
+	public static List<AnnotationData> getAnnotations(String modid, Type type) {
+		return ModList.get() //
+				.getModFileById(modid) //
+				.getFile() //
+				.getScanResult() //
+				.getAnnotations() //
+				.stream() //
+				.filter(data -> type.equals(data.getAnnotationType())) //
+				.collect(Collectors.toList());
+	}
+	
+}
