2061ea6f970d95825031bb15b5fb55363e43646a
HyCraftHD <hycrafthd@live.de>
2020-08-07 16:33:15 +0200

    Implement the construct annotation in the common bus register

--- src/test/java/info/u_team/u_team_test/init/TestCommonBusRegister.java
+++ src/test/java/info/u_team/u_team_test/init/TestCommonBusRegister.java
@@ -1,10 +1,13 @@
 package info.u_team.u_team_test.init;
 
+import info.u_team.u_team_core.api.construct.*;
 import info.u_team.u_team_core.util.registry.BusRegister;
 
-public class TestCommonBusRegister {
+@Construct()
+public class TestCommonBusRegister implements IModConstruct {
 	
-	public static void register() {
+	@Override
+	public void construct() {
 		BusRegister.registerMod(TestBiomes::registerMod);
 		BusRegister.registerMod(TestBlocks::registerMod);
 		BusRegister.registerMod(TestContainers::registerMod);
