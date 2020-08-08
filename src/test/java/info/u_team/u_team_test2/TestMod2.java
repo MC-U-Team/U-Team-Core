91cf317092bb0237211072a5e5038001eaf15af2
HyCraftHD <hycrafthd@live.de>
2020-08-08 20:56:52 +0200

    Call the construct manager and the integration manager

--- src/test/java/info/u_team/u_team_test2/TestMod2.java
+++ src/test/java/info/u_team/u_team_test2/TestMod2.java
@@ -2,6 +2,8 @@ package info.u_team.u_team_test2;
 
 import org.apache.logging.log4j.*;
 
+import info.u_team.u_team_core.construct.ConstructManager;
+import info.u_team.u_team_core.integration.IntegrationManager;
 import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import net.minecraftforge.fml.common.Mod;
 
@@ -15,5 +17,8 @@ public class TestMod2 {
 		JarSignVerifier.checkSigned(MODID);
 		
 		LOGGER.info("-------------------------------------- LOADING TEST MOD 2 -------------------------------------");
+		
+		ConstructManager.constructConstructs(MODID);
+		IntegrationManager.constructIntegrations(MODID);
 	}
 }
