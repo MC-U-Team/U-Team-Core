8bd7ad83ed219599c61597741047d8a47f71f3ee
HyCraftHD <hycrafthd@live.de>
2020-08-08 20:55:46 +0200

    Add test mod 2 class

---
+++ src/test/java/info/u_team/u_team_test2/TestMod2.java
@@ -0,0 +1,19 @@
+package info.u_team.u_team_test2;
+
+import org.apache.logging.log4j.*;
+
+import info.u_team.u_team_core.util.verify.JarSignVerifier;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(TestMod2.MODID)
+public class TestMod2 {
+	
+	public static final String MODID = "uteamtest2";
+	public static final Logger LOGGER = LogManager.getLogger("UTeamTest2");
+	
+	public TestMod2() {
+		JarSignVerifier.checkSigned(MODID);
+		
+		LOGGER.info("--------------------------------------- LOADING TEST MOD 2 ---------------------------------------");
+	}
+}
