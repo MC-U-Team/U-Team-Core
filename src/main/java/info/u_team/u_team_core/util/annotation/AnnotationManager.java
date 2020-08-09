259a61ab4276c19d2218c0e54e29d9768654af17
HyCraftHD <hycrafthd@live.de>
2020-08-09 14:52:26 +0200

    Add marker to logger

--- src/main/java/info/u_team/u_team_core/util/annotation/AnnotationManager.java
+++ src/main/java/info/u_team/u_team_core/util/annotation/AnnotationManager.java
@@ -13,15 +13,17 @@ import net.minecraftforge.forgespi.language.ModFileScanData.AnnotationData;
 public class AnnotationManager {
 	
 	private static final Logger LOGGER = LogManager.getLogger("AnnotationManager");
+	private static final Marker CONSTRUCT_MARKER = MarkerManager.getMarker("Construct");
+	private static final Marker INTEGRATION_MARKER = MarkerManager.getMarker("Integration");
 	
 	public static void callConstructs(String modid) {
 		for (final AnnotationData data : AnnotationUtil.getAnnotations(modid, Type.getType(Construct.class))) {
 			if (canBeCalled(modid, data)) {
-				LOGGER.info("Load construct (" + data.getMemberName() + ") for mod " + modid);
+				LOGGER.info(CONSTRUCT_MARKER, "Load construct (" + data.getMemberName() + ") for mod " + modid);
 				try {
 					Class.forName(data.getMemberName()).asSubclass(IModConstruct.class).newInstance().construct();
 				} catch (LinkageError | ClassNotFoundException | InstantiationException | IllegalAccessException | ClassCastException ex) {
-					LOGGER.error("Failed to load and call mod construct : {}", data.getMemberName(), ex);
+					LOGGER.error(CONSTRUCT_MARKER, "Failed to load and call mod construct : {}", data.getMemberName(), ex);
 					throw new RuntimeException(ex);
 				}
 			}
@@ -32,11 +34,11 @@ public class AnnotationManager {
 		for (final AnnotationData data : AnnotationUtil.getAnnotations(modid, Type.getType(Integration.class))) {
 			final String integrationModid = (String) data.getAnnotationData().get("integration");
 			if (canBeCalled(modid, data) && ModList.get().isLoaded(integrationModid)) {
-				LOGGER.info("Load " + integrationModid + " integration (" + data.getMemberName() + ") for mod " + modid);
+				LOGGER.info(INTEGRATION_MARKER, "Load " + integrationModid + " integration (" + data.getMemberName() + ") for mod " + modid);
 				try {
 					Class.forName(data.getMemberName()).asSubclass(IModIntegration.class).newInstance().construct();
 				} catch (LinkageError | ClassNotFoundException | InstantiationException | IllegalAccessException | ClassCastException ex) {
-					LOGGER.error("Failed to load and call integration : {}", data.getMemberName(), ex);
+					LOGGER.error(INTEGRATION_MARKER, "Failed to load and call integration : {}", data.getMemberName(), ex);
 					throw new RuntimeException(ex);
 				}
 			}
