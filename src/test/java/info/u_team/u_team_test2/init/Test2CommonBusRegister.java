b39a5a8c11ce145a3850cafd53b67074d8bf69d7
HyCraftHD <hycrafthd@live.de>
2020-08-09 15:04:24 +0200

    Add test common bus register for test mod 2

---
+++ src/test/java/info/u_team/u_team_test2/init/Test2CommonBusRegister.java
@@ -0,0 +1,23 @@
+package info.u_team.u_team_test2.init;
+
+import org.apache.logging.log4j.LogManager;
+
+import info.u_team.u_team_core.api.construct.*;
+import info.u_team.u_team_test2.TestMod2;
+import net.minecraft.block.Block;
+import net.minecraftforge.event.RegistryEvent.Register;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
+@Construct(modid = TestMod2.MODID)
+public class Test2CommonBusRegister implements IModConstruct {
+	
+	@Override
+	public void construct() {
+		Bus.MOD.bus().get().addGenericListener(Block.class, this::registerCall);
+	}
+	
+	private void registerCall(Register<Block> event) {
+		LogManager.getLogger().info("Hello from the register event for test mod 2");
+	}
+	
+}
