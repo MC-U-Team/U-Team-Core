13f6a955f448483fbbf5a24e32763ceece07095e
HyCraftHD <hycrafthd@live.de>
2020-08-09 15:12:25 +0200

    Same for the client thing

--- src/main/java/info/u_team/u_team_core/intern/init/UCoreClientBusRegister.java
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreClientBusRegister.java
@@ -1,11 +1,15 @@
 package info.u_team.u_team_core.intern.init;
 
+import info.u_team.u_team_core.UCoreMod;
+import info.u_team.u_team_core.api.construct.*;
 import info.u_team.u_team_core.intern.discord.UpdateDiscordEventHandler;
 import info.u_team.u_team_core.util.registry.BusRegister;
 
-public class UCoreClientBusRegister {
+@Construct(modid = UCoreMod.MODID, client = true)
+public class UCoreClientBusRegister implements IModConstruct {
 	
-	public static void register() {
+	@Override
+	public void construct() {
 		BusRegister.registerMod(UpdateDiscordEventHandler::registerMod);
 		BusRegister.registerMod(UCoreColors::registerMod);
 		
