b362db678d978d5d691427b7661bfeb979e567f8
HyCraftHD <hycrafthd@live.de>
2020-08-09 15:14:01 +0200

    Remove the old config register call

--- src/main/java/info/u_team/u_team_core/UCoreMod.java
+++ src/main/java/info/u_team/u_team_core/UCoreMod.java
@@ -2,12 +2,9 @@ package info.u_team.u_team_core;
 
 import org.apache.logging.log4j.*;
 
-import info.u_team.u_team_core.intern.config.ClientConfig;
 import info.u_team.u_team_core.util.annotation.AnnotationManager;
 import info.u_team.u_team_core.util.verify.JarSignVerifier;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig.Type;
 
 @Mod(UCoreMod.MODID)
 public class UCoreMod {
@@ -17,8 +14,6 @@ public class UCoreMod {
 	
 	public UCoreMod() {
 		JarSignVerifier.checkSigned(MODID);
-		
-		ModLoadingContext.get().registerConfig(Type.CLIENT, ClientConfig.CONFIG);
 		
 		AnnotationManager.callAnnotations(MODID);
 	}
