ef199eb61c33859b77e94556fca9a6caa451ffca
HyCraftHD <hycrafthd@live.de>
2020-08-09 15:13:48 +0200

    Register the config in the common register now

--- src/main/java/info/u_team/u_team_core/intern/init/UCoreCommonBusRegister.java
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreCommonBusRegister.java
@@ -2,13 +2,18 @@ package info.u_team.u_team_core.intern.init;
 
 import info.u_team.u_team_core.UCoreMod;
 import info.u_team.u_team_core.api.construct.*;
+import info.u_team.u_team_core.intern.config.ClientConfig;
 import info.u_team.u_team_core.util.registry.BusRegister;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.config.ModConfig.Type;
 
 @Construct(modid = UCoreMod.MODID)
 public class UCoreCommonBusRegister implements IModConstruct {
 	
 	@Override
 	public void construct() {
+		ModLoadingContext.get().registerConfig(Type.CLIENT, ClientConfig.CONFIG);
+		
 		BusRegister.registerMod(UCoreNetwork::registerMod);
 		BusRegister.registerMod(UCoreRecipeSerializers::registerMod);
 		BusRegister.registerMod(UCoreLootFunctions::registerMod);
