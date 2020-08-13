83788fe5c449970a3dbc3fb5aa6139cd3a5873ea
HyCraftHD <hycrafthd@live.de>
2020-08-13 14:22:21 +0200

    Fix fluid ingredient

--- src/main/java/info/u_team/u_team_core/ingredient/FluidIngredient.java
+++ src/main/java/info/u_team/u_team_core/ingredient/FluidIngredient.java
@@ -200,7 +200,7 @@ public class FluidIngredient implements Predicate<FluidStack> {
 		
 		public JsonObject serialize() {
 			final JsonObject jsonObject = new JsonObject();
-			jsonObject.addProperty("tag", TagCollectionManager.func_232928_e_().func_232926_c_().func_232975_b_(tag).toString());
+			jsonObject.addProperty("tag", TagCollectionManager.func_242178_a().func_241837_c().func_232975_b_(tag).toString());
 			return jsonObject;
 		}
 	}
