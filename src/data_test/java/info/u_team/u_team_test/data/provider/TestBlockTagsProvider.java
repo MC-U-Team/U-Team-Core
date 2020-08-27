b0ecfa2a1492794e7725b67aaa8897040c69e498
HyCraftHD <hycrafthd@live.de>
2020-08-27 22:37:40 +0200

    Add test block tags provider

---
+++ src/data_test/java/info/u_team/u_team_test/data/provider/TestBlockTagsProvider.java
@@ -0,0 +1,15 @@
+package info.u_team.u_team_test.data.provider;
+
+import info.u_team.u_team_core.data.*;
+
+public class TestBlockTagsProvider extends CommonBlockTagsProvider {
+	
+	public TestBlockTagsProvider(GenerationData data) {
+		super(data);
+	}
+	
+	@Override
+	protected void registerTags() {
+	}
+	
+}
