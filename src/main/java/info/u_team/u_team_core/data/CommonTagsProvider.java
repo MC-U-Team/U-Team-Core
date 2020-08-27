4908791ecd576c1c8ccb5f53f1b340c2ffac44bd
HyCraftHD <hycrafthd@live.de>
2020-08-27 21:42:22 +0200

    Add optional tag entry method

--- src/main/java/info/u_team/u_team_core/data/CommonTagsProvider.java
+++ src/main/java/info/u_team/u_team_core/data/CommonTagsProvider.java
@@ -86,6 +86,10 @@ public abstract class CommonTagsProvider<T> extends CommonProvider {
 			return addUnique(clazz, predicate, add);
 		}
 		
+		private <C extends OptionalTagEntry> Builder<T> addUniqueOptionalTagEntry(Class<C> clazz, Predicate<C> predicate, Supplier<Builder<T>> add) {
+			return addUnique(clazz, predicate, add);
+		}
+		
 		private <C extends ITagEntry> Builder<T> addUnique(Class<C> clazz, Predicate<C> predicate, Supplier<Builder<T>> add) {
 			final boolean duplicate = getInternalBuilder().getProxyStream() //
 					.map(Proxy::getEntry) //
