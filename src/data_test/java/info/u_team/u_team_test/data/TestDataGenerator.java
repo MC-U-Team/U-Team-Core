91cd19e048f624cf6fbceb2c4f8fd83fd0833080
HyCraftHD <hycrafthd@live.de>
2020-08-27 22:38:10 +0200

    Register test blocks provider

--- src/data_test/java/info/u_team/u_team_test/data/TestDataGenerator.java
+++ src/data_test/java/info/u_team/u_team_test/data/TestDataGenerator.java
@@ -15,6 +15,7 @@ public class TestDataGenerator {
 	public static void data(GatherDataEvent event) {
 		final GenerationData data = new GenerationData(TestMod.MODID, event);
 		if (event.includeServer()) {
+			data.addProvider(TestBlockTagsProvider::new);
 			data.addProvider(TestLootTablesProvider::new);
 		}
 		if (event.includeClient()) {
