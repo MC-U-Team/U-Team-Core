01b605bb2458649fbd11e4a3e8eeee75bf118809
HyCraftHD <hycrafthd@live.de>
2020-08-27 22:40:17 +0200

    Add test tags for blocks

--- src/test/java/info/u_team/u_team_test/init/TestTags.java
+++ src/test/java/info/u_team/u_team_test/init/TestTags.java
@@ -1,10 +1,15 @@
 package info.u_team.u_team_test.init;
 
+import info.u_team.u_team_core.util.TagUtil;
+import info.u_team.u_team_test.TestMod;
+import net.minecraft.block.Block;
+import net.minecraft.tags.ITag.INamedTag;
 
 public class TestTags {
 	
 	public static class Blocks {
-		
+		public static INamedTag<Block> TEST_TAG_1 = TagUtil.createBlockTag(TestMod.MODID, "test_1");
+		public static INamedTag<Block> TEST_TAG_2 = TagUtil.createBlockTag(TestMod.MODID, "test_2");
 	}
 	
 	public static class Items {
