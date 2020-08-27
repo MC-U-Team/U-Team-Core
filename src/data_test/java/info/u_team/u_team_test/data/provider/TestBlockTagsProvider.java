52f58b3594ce7c8f687b45d71c7528fbb905f35e
HyCraftHD <hycrafthd@live.de>
2020-08-27 22:43:13 +0200

    Add some stuff to the block tags provider

--- src/data_test/java/info/u_team/u_team_test/data/provider/TestBlockTagsProvider.java
+++ src/data_test/java/info/u_team/u_team_test/data/provider/TestBlockTagsProvider.java
@@ -1,6 +1,10 @@
 package info.u_team.u_team_test.data.provider;
 
+import static info.u_team.u_team_test.init.TestTags.Blocks.*;
 import info.u_team.u_team_core.data.*;
+import net.minecraft.block.Blocks;
+import net.minecraft.tags.*;
+
 
 public class TestBlockTagsProvider extends CommonBlockTagsProvider {
 	
@@ -10,6 +14,11 @@ public class TestBlockTagsProvider extends CommonBlockTagsProvider {
 	
 	@Override
 	protected void registerTags() {
+		getBuilder(TEST_TAG_1).add(Blocks.ACACIA_BUTTON, Blocks.ACACIA_DOOR);
+		getBuilder(TEST_TAG_2).addTag(TEST_TAG_1).add(Blocks.ACACIA_LOG);
+		
+		getBuilder(TEST_TAG_1).add(Blocks.BIRCH_LEAVES);
+		getBuilder(TEST_TAG_1).addTag(BlockTags.BEDS);
 	}
 	
 }
