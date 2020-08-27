f563ea593c05098179d98ce41baf6dde566d0f7b
HyCraftHD <hycrafthd@live.de>
2020-08-27 23:01:36 +0200

    Add test item tags provider

---
+++ src/data_test/java/info/u_team/u_team_test/data/provider/TestItemTagsProvider.java
@@ -0,0 +1,15 @@
+package info.u_team.u_team_test.data.provider;
+
+import info.u_team.u_team_core.data.*;
+
+public class TestItemTagsProvider extends CommonItemTagsProvider {
+	
+	public TestItemTagsProvider(GenerationData data, CommonBlockTagsProvider blockProvider) {
+		super(data, blockProvider);
+	}
+	
+	@Override
+	protected void registerTags() {
+	}
+	
+}
