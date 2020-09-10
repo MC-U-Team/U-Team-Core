b102657f9574b46c0fee98b4aeb5e9884bd30089
HyCraftHD <hycrafthd@live.de>
2020-09-10 18:29:16 +0200

    Add auto_smelt generator

--- src/data_test/java/info/u_team/u_team_test/data/provider/TestGlobalLootModifiersProvider.java
+++ src/data_test/java/info/u_team/u_team_test/data/provider/TestGlobalLootModifiersProvider.java
@@ -4,6 +4,10 @@ import java.util.function.Supplier;
 
 import info.u_team.u_team_core.data.*;
 import info.u_team.u_team_core.util.TriConsumer;
+import info.u_team.u_team_test.global_loot_modifier.AutoSmeltLootModifier;
+import info.u_team.u_team_test.init.*;
+import net.minecraft.advancements.criterion.*;
+import net.minecraft.loot.conditions.*;
 import net.minecraftforge.common.loot.*;
 
 public class TestGlobalLootModifiersProvider extends CommonGlobalLootModifiersProvider {
@@ -13,7 +17,7 @@ public class TestGlobalLootModifiersProvider extends CommonGlobalLootModifiersPr
 	}
 	
 	@Override
-	protected <T extends IGlobalLootModifier> void registerGlobalLootModifiers(TriConsumer<String, Supplier<GlobalLootModifierSerializer<T>>, T> consumer) {
+	protected void registerGlobalLootModifiers(TriConsumer<String, Supplier<? extends GlobalLootModifierSerializer<? extends IGlobalLootModifier>>, ? super IGlobalLootModifier> consumer) {
+		consumer.accept("auto_smelt", TestGlobalLootModifiers.AUTO_SMELT, new AutoSmeltLootModifier(new ILootCondition[] { MatchTool.builder(ItemPredicate.Builder.create().enchantment(new EnchantmentPredicate(TestEnchantments.AUTO_SMELT.get(), MinMaxBounds.IntBound.atLeast(1)))).build() }));
 	}
-	
 }
