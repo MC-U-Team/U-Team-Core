e368789f4fb7200cfee45b00a84b07e745b2fadc
HyCraftHD <hycrafthd@live.de>
2020-11-02 22:06:02 +0100

    Register entity spawn placement registry

---
+++ src/test/java/info/u_team/u_team_test/init/TestEntitySpawnPlacementRegistries.java
@@ -0,0 +1,22 @@
+package info.u_team.u_team_test.init;
+
+import info.u_team.u_team_test.entity.TestLivingEntity;
+import net.minecraft.entity.EntitySpawnPlacementRegistry;
+import net.minecraft.entity.EntitySpawnPlacementRegistry.PlacementType;
+import net.minecraft.world.gen.Heightmap.Type;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+public class TestEntitySpawnPlacementRegistries {
+	
+	private static void setup(FMLCommonSetupEvent event) {
+		event.enqueueWork(() -> {
+			EntitySpawnPlacementRegistry.register(TestEntityTypes.TEST_LIVING.get(), PlacementType.ON_GROUND, Type.MOTION_BLOCKING_NO_LEAVES, TestLivingEntity::canSpawn);
+		});
+	}
+	
+	public static void registerMod(IEventBus bus) {
+		bus.addListener(TestEntitySpawnPlacementRegistries::setup);
+	}
+	
+}
