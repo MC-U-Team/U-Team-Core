13f1b76fe274d7eee0795112db83fe73f97ca07b
HyCraftHD <hycrafthd@live.de>
2020-11-02 01:45:35 +0100

    Update constructor if u spawn egg item to match the other u items

--- src/main/java/info/u_team/u_team_core/item/USpawnEggItem.java
+++ src/main/java/info/u_team/u_team_core/item/USpawnEggItem.java
@@ -6,15 +6,19 @@ import java.util.function.Supplier;
 import org.apache.commons.lang3.tuple.Pair;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.item.SpawnEggItem;
+import net.minecraft.item.*;
 import net.minecraft.util.LazyValue;
 
 public class USpawnEggItem extends SpawnEggItem {
 	
 	public static final List<Pair<LazyValue<? extends EntityType<?>>, USpawnEggItem>> LAZY_EGGS = new ArrayList<>();
 	
-	public USpawnEggItem(Supplier<? extends EntityType<?>> entityType, int primaryColor, int secondaryColor, Properties builder) {
-		super(null, primaryColor, secondaryColor, builder);
+	public USpawnEggItem(Properties properties, Supplier<? extends EntityType<?>> entityType, int primaryColor, int secondaryColor) {
+		this(null, properties, entityType, primaryColor, secondaryColor);
+	}
+	
+	public USpawnEggItem(ItemGroup group, Properties properties, Supplier<? extends EntityType<?>> entityType, int primaryColor, int secondaryColor) {
+		super(null, primaryColor, secondaryColor, group == null ? properties : properties.group(group));
 		EGGS.remove(null);
 		LAZY_EGGS.add(Pair.of(new LazyValue<>(entityType), this));
 	}
