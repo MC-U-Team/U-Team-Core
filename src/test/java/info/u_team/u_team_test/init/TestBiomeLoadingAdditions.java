781d35e7e7c2bf7dc27f2eaab4a028b63a2799c7
HyCraftHD <hycrafthd@live.de>
2020-11-04 02:04:16 +0100

    Should add our mob to the spawn of biomes

--- src/test/java/info/u_team/u_team_test/init/TestBiomeLoadingAdditions.java
+++ src/test/java/info/u_team/u_team_test/init/TestBiomeLoadingAdditions.java
@@ -1,11 +1,17 @@
 package info.u_team.u_team_test.init;
 
+import net.minecraft.entity.*;
+import net.minecraft.world.biome.MobSpawnInfo;
+import net.minecraft.world.biome.Biome.Category;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.*;
 
 public class TestBiomeLoadingAdditions {
 	
 	private static void biomeLoadingAddition(BiomeLoadingEvent event) {
+		if (event.getCategory() != Category.THEEND && event.getCategory() != Category.NETHER) {
+			event.getSpawns().withSpawner(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(TestEntityTypes.TEST_LIVING.get(), 80, 4, 4));
+		}
 	}
 	
 	public static void registerForge(IEventBus bus) {
