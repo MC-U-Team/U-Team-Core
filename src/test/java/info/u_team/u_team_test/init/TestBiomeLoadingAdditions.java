c957b458338d9ae16663ea3152a5e0b1ef8fb25c
HyCraftHD <hycrafthd@live.de>
2020-11-04 01:46:20 +0100

    Rename

--- src/test/java/info/u_team/u_team_test/init/TestBiomeLoadingAdditions.java
+++ src/test/java/info/u_team/u_team_test/init/TestBiomeLoadingAdditions.java
@@ -3,13 +3,13 @@ package info.u_team.u_team_test.init;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.*;
 
-public class TestBiomeLoadingFeatures {
+public class TestBiomeLoadingAdditions {
 	
 	private static void biomeLoadingAddition(BiomeLoadingEvent event) {
 	}
 	
 	public static void registerForge(IEventBus bus) {
-		bus.addListener(EventPriority.HIGH, TestBiomeLoadingFeatures::biomeLoadingAddition);
+		bus.addListener(EventPriority.HIGH, TestBiomeLoadingAdditions::biomeLoadingAddition);
 	}
 	
 }
