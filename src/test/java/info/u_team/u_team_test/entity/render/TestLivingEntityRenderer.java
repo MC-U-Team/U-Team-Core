84472288152437af63d61079966794bc622bd737
HyCraftHD <hycrafthd@live.de>
2020-11-04 01:24:58 +0100

    Use texture for test living

--- src/test/java/info/u_team/u_team_test/entity/render/TestLivingEntityRenderer.java
+++ src/test/java/info/u_team/u_team_test/entity/render/TestLivingEntityRenderer.java
@@ -2,10 +2,14 @@ package info.u_team.u_team_test.entity.render;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 
+import info.u_team.u_team_test.TestMod;
 import net.minecraft.client.renderer.entity.*;
 import net.minecraft.entity.monster.ZombieEntity;
+import net.minecraft.util.ResourceLocation;
 
 public class TestLivingEntityRenderer extends ZombieRenderer {
+	
+	private static final ResourceLocation TEXTURE = new ResourceLocation(TestMod.MODID, "textures/entity/test_living/test_living.png");
 	
 	public TestLivingEntityRenderer(EntityRendererManager renderManager) {
 		super(renderManager);
@@ -15,6 +19,11 @@ public class TestLivingEntityRenderer extends ZombieRenderer {
 	protected void preRenderCallback(ZombieEntity zombie, MatrixStack matrixStack, float partialTickTime) {
 		matrixStack.scale(1.0625F, 1.0625F, 1.0625F);
 		super.preRenderCallback(zombie, matrixStack, partialTickTime);
+	}
+	
+	@Override
+	public ResourceLocation getEntityTexture(ZombieEntity entity) {
+		return TEXTURE;
 	}
 	
 }
