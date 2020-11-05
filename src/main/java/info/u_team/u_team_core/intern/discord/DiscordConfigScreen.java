23b691e955cb2f96064dff8e58a32ee12194c0b7
HyCraftHD <hycrafthd@live.de>
2020-11-05 13:15:20 +0100

    Add doen button

--- src/main/java/info/u_team/u_team_core/intern/discord/DiscordConfigScreen.java
+++ src/main/java/info/u_team/u_team_core/intern/discord/DiscordConfigScreen.java
@@ -1,8 +1,7 @@
 package info.u_team.u_team_core.intern.discord;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
-
-import info.u_team.u_team_core.gui.elements.ActiveButton;
+import info.u_team.u_team_core.gui.elements.*;
 import info.u_team.u_team_core.intern.config.ClientConfig;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.util.text.ITextComponent;
@@ -24,13 +23,15 @@ public class DiscordConfigScreen extends Screen {
 		final ITextComponent on = ITextComponent.getTextComponentOrEmpty("Discord connection is on");
 		final ITextComponent off = ITextComponent.getTextComponentOrEmpty("Discord connection is off");
 		
-		final ActiveButton button = addButton(new ActiveButton(30, 50, 200, 20, discordRichPresence.get() ? on : off, 0x006442FF));
-		button.setActive(discordRichPresence.get());
-		button.setPressable(() -> {
+		final ActiveButton toggleDiscordRichPresenceButton = addButton(new ActiveButton(30, 50, 200, 20, discordRichPresence.get() ? on : off, 0x006442FF));
+		toggleDiscordRichPresenceButton.setActive(discordRichPresence.get());
+		toggleDiscordRichPresenceButton.setPressable(() -> {
 			discordRichPresence.set(!discordRichPresence.get());
-			button.setActive(discordRichPresence.get());
-			button.setMessage(discordRichPresence.get() ? on : off);
+			toggleDiscordRichPresenceButton.setActive(discordRichPresence.get());
+			toggleDiscordRichPresenceButton.setMessage(discordRichPresence.get() ? on : off);
 		});
+		
+		addButton(new UButton(30, 80, 200, 20, ITextComponent.getTextComponentOrEmpty("Done"), button -> closeScreen()));
 	}
 	
 	@Override
