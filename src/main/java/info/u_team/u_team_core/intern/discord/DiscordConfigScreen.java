c67b18ad40c7280cc511048d7689c9833dc3ea18
HyCraftHD <hycrafthd@live.de>
2020-11-05 13:09:48 +0100

    This button should toggle the value in the config

--- src/main/java/info/u_team/u_team_core/intern/discord/DiscordConfigScreen.java
+++ src/main/java/info/u_team/u_team_core/intern/discord/DiscordConfigScreen.java
@@ -1,7 +1,10 @@
 package info.u_team.u_team_core.intern.discord;
 
+import info.u_team.u_team_core.gui.elements.ActiveButton;
+import info.u_team.u_team_core.intern.config.ClientConfig;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
 
 public class DiscordConfigScreen extends Screen {
 	
@@ -10,6 +13,22 @@ public class DiscordConfigScreen extends Screen {
 	public DiscordConfigScreen(Screen screenBefore) {
 		super(ITextComponent.getTextComponentOrEmpty("discord"));
 		this.screenBefore = screenBefore;
+	}
+	
+	@Override
+	protected void init() {
+		final BooleanValue discordRichPresence = ClientConfig.getInstance().discordRichPresence;
+		
+		final ITextComponent on = ITextComponent.getTextComponentOrEmpty("Discord connection is on");
+		final ITextComponent off = ITextComponent.getTextComponentOrEmpty("Discord connection is off");
+		
+		final ActiveButton button = addButton(new ActiveButton(30, 50, 200, 20, discordRichPresence.get() ? on : off, 0x006442FF));
+		button.setActive(discordRichPresence.get());
+		button.setPressable(() -> {
+			discordRichPresence.set(!discordRichPresence.get());
+			button.setActive(discordRichPresence.get());
+			button.setMessage(discordRichPresence.get() ? on : off);
+		});
 	}
 	
 	@Override
