ac1ab338b5558f0212d8b4108cb8bc25a321c3ff
HyCraftHD <hycrafthd@live.de>
2020-12-07 18:58:29 +0100

    Should now only add the items that have recipes to the dyeable tag

--- src/integration/java/info/u_team/u_team_core/integration/jei/UTeamCoreJeiPlugin.java
+++ src/integration/java/info/u_team/u_team_core/integration/jei/UTeamCoreJeiPlugin.java
@@ -5,11 +5,16 @@ import java.util.stream.Collectors;
 
 import info.u_team.u_team_core.UCoreMod;
 import info.u_team.u_team_core.api.dye.IDyeableItem;
+import info.u_team.u_team_core.inventory.*;
 import mezz.jei.api.*;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
 import net.minecraft.item.*;
+import net.minecraft.item.crafting.IRecipeType;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraftforge.items.IItemHandlerModifiable;
 import net.minecraftforge.registries.ForgeRegistries;
 
 @JeiPlugin
@@ -24,7 +29,20 @@ public class UTeamCoreJeiPlugin implements IModPlugin {
 	
 	@Override
 	public void registerRecipes(IRecipeRegistration registration) {
-		final List<ItemStack> items = ForgeRegistries.ITEMS.getValues().stream().filter(item -> item instanceof IDyeableItem || item instanceof IDyeableArmorItem).map(ItemStack::new).collect(Collectors.toList());
+		final Minecraft minecraft = Minecraft.getInstance();
+		final World world = minecraft.world;
+		
+		final IItemHandlerModifiable handler = new UItemStackHandler(9);
+		handler.setStackInSlot(1, new ItemStack(Items.WHITE_DYE));
+		
+		final List<ItemStack> items = ForgeRegistries.ITEMS.getValues().stream() //
+				.filter(item -> item instanceof IDyeableItem || item instanceof IDyeableArmorItem) //
+				.map(ItemStack::new) //
+				.filter(stack -> {
+					handler.setStackInSlot(0, stack.copy());
+					return world.getRecipeManager().getRecipe(IRecipeType.CRAFTING, new CraftingRecipeWrapper(handler, 3, 3), world).isPresent();
+				}).collect(Collectors.toList());
+		
 		registration.addIngredientInfo(items, VanillaTypes.ITEM, "jei.uteamcore.dyeable.info");
 	}
 	
