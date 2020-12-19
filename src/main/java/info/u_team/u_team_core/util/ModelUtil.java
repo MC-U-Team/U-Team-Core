be65f444532a28f2e1a5ad568bd40e6b9cb05da4
HyCraftHD <hycrafthd@live.de>
2020-12-19 15:07:55 +0100

    Add java doc

--- src/main/java/info/u_team/u_team_core/util/ModelUtil.java
+++ src/main/java/info/u_team/u_team_core/util/ModelUtil.java
@@ -7,10 +7,17 @@ import java.util.*;
 import com.google.common.collect.*;
 
 import net.minecraft.block.*;
-import net.minecraft.client.renderer.model.RenderMaterial;
+import net.minecraft.client.renderer.model.*;
 import net.minecraft.state.StateContainer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 
+/**
+ * Utility methods for interaction with {@link ModelBakery} private methods.<br>
+ * All methods should be called in the {@link ModelRegistryEvent} event.
+ * 
+ * @author HyCraftHD
+ */
 public class ModelUtil {
 	
 	static {
@@ -21,10 +28,21 @@ public class ModelUtil {
 		}
 	}
 	
+	/**
+	 * Replace the default state container with a custom one. Can remove or add states for the model
+	 * 
+	 * @param location Resource location of the model
+	 * @param container Custom state container
+	 */
 	public static void addCustomStateContainer(ResourceLocation location, StateContainer<Block, BlockState> container) {
 		STATE_CONTAINER_OVERRIDES.put(location, container);
 	}
 	
+	/**
+	 * Add {@link RenderMaterial} to be loaded to the atlas texture and stitched.
+	 * 
+	 * @param material Render Material
+	 */
 	public static void addTexture(RenderMaterial material) {
 		LOCATIONS_BUILTIN_TEXTURES.add(material);
 	}
