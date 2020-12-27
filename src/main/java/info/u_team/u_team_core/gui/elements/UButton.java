b9fdda1fa3301c1cf32f0b2a0a727b51c1fb9a67
HyCraftHD <hycrafthd@live.de>
2020-12-27 01:48:02 +0100

    Extend ubutton with ITooltip and update code in render button

--- src/main/java/info/u_team/u_team_core/gui/elements/UButton.java
+++ src/main/java/info/u_team/u_team_core/gui/elements/UButton.java
@@ -5,12 +5,11 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.widget.button.Button;
-import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.*;
 import net.minecraftforge.fml.client.gui.GuiUtils;
 
 /**
- * A button that fixes vanilla not drawing the continuous border if the button is smaller than 20 The code is adapted
- * from the 1.13.2 GuiButtonExt.
+ * A button that fixes vanilla not drawing the continuous border if the button is smaller than 20.
  * 
  * @author HyCraftHD
  */
@@ -19,11 +18,17 @@ public class UButton extends Button {
 	protected static IPressable EMTPY_PRESSABLE = button -> {
 	};
 	
+	protected static ITooltip EMPTY_TOOLTIP = field_238486_s_;
+	
 	public UButton(int x, int y, int width, int height, ITextComponent display) {
-		super(x, y, width, height, display, EMTPY_PRESSABLE);
+		this(x, y, width, height, display, EMTPY_PRESSABLE);
 	}
 	
 	public UButton(int x, int y, int width, int height, ITextComponent display, IPressable pessable) {
+		this(x, y, width, height, display, pessable, EMPTY_TOOLTIP);
+	}
+	
+	public UButton(int x, int y, int width, int height, ITextComponent display, IPressable pessable, ITooltip tooltip) {
 		super(x, y, width, height, display, pessable);
 	}
 	
@@ -35,21 +40,25 @@ public class UButton extends Button {
 		onPress = button -> runnable.run();
 	}
 	
+	public void setTooltip(ITooltip tooltip) {
+		onTooltip = tooltip;
+	}
+	
 	@Override
 	public void renderButton(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
 		final Minecraft minecraft = Minecraft.getInstance();
 		final FontRenderer fontRenderer = minecraft.fontRenderer;
 		
-		String message = getMessage().getString();
+		ITextComponent message = getMessage();
 		
 		GuiUtils.drawContinuousTexturedBox(matrixStack, WIDGETS_LOCATION, x, y, 0, 46 + getYImage(isHovered()) * 20, width, height, 200, 20, 2, 3, 2, 2, 0);
 		renderBg(matrixStack, minecraft, mouseX, mouseY);
 		
-		final int messageWidth = fontRenderer.getStringWidth(message);
+		final int messageWidth = fontRenderer.getStringPropertyWidth(message);
 		final int ellipsisWidth = fontRenderer.getStringWidth("...");
 		
 		if (messageWidth > width - 6 && messageWidth > ellipsisWidth) {
-			message = fontRenderer.func_238412_a_(message, width - 6 - ellipsisWidth).trim() + "...";
+			message = new StringTextComponent(fontRenderer.func_238417_a_(message, width - 6 - ellipsisWidth).getString() + "...");
 		}
 		
 		drawCenteredString(matrixStack, fontRenderer, message, x + width / 2, y + (height - 8) / 2, getFGColor());
