e50e893102edbf74ea1c000dc07a39d840e65c9a
HyCraftHD <hycrafthd@live.de>
2020-12-28 15:35:41 +0100

    Change button to activeted because in Widget an active field is present.

--- src/main/java/info/u_team/u_team_core/gui/elements/ScalableActiveButton.java
+++ src/main/java/info/u_team/u_team_core/gui/elements/ScalableActiveButton.java
@@ -7,46 +7,46 @@ import net.minecraft.util.text.ITextComponent;
 
 public class ScalableActiveButton extends ScalableButton {
 	
-	protected boolean active;
+	protected boolean activated;
 	
-	protected RGBA activeColor;
+	protected RGBA activatedColor;
 	
-	public ScalableActiveButton(int x, int y, int width, int height, ITextComponent text, float scale, boolean active, RGBA activeColor) {
-		this(x, y, width, height, text, scale, active, activeColor, EMTPY_PRESSABLE);
+	public ScalableActiveButton(int x, int y, int width, int height, ITextComponent text, float scale, boolean activated, RGBA activatedColor) {
+		this(x, y, width, height, text, scale, activated, activatedColor, EMTPY_PRESSABLE);
 	}
 	
-	public ScalableActiveButton(int x, int y, int width, int height, ITextComponent text, float scale, boolean active, RGBA activeColor, IPressable pessable) {
-		this(x, y, width, height, text, scale, active, activeColor, pessable, EMPTY_TOOLTIP);
+	public ScalableActiveButton(int x, int y, int width, int height, ITextComponent text, float scale, boolean activated, RGBA activatedColor, IPressable pessable) {
+		this(x, y, width, height, text, scale, activated, activatedColor, pessable, EMPTY_TOOLTIP);
 	}
 	
-	public ScalableActiveButton(int x, int y, int width, int height, ITextComponent text, float scale, boolean active, RGBA activeColor, ITooltip tooltip) {
-		this(x, y, width, height, text, scale, active, activeColor, EMTPY_PRESSABLE, tooltip);
+	public ScalableActiveButton(int x, int y, int width, int height, ITextComponent text, float scale, boolean activated, RGBA activatedColor, ITooltip tooltip) {
+		this(x, y, width, height, text, scale, activated, activatedColor, EMTPY_PRESSABLE, tooltip);
 	}
 	
-	public ScalableActiveButton(int x, int y, int width, int height, ITextComponent text, float scale, boolean active, RGBA activeColor, IPressable pessable, ITooltip tooltip) {
+	public ScalableActiveButton(int x, int y, int width, int height, ITextComponent text, float scale, boolean activated, RGBA activatedColor, IPressable pessable, ITooltip tooltip) {
 		super(x, y, width, height, text, scale, pessable, tooltip);
-		this.active = active;
-		this.activeColor = activeColor;
+		this.activated = activated;
+		this.activatedColor = activatedColor;
 	}
 	
-	public boolean isActive() {
-		return active;
+	public boolean isActivated() {
+		return activated;
 	}
 	
-	public void setActive(boolean active) {
-		this.active = active;
+	public void setActivated(boolean activated) {
+		this.activated = activated;
 	}
 	
-	public RGBA getActiveColor() {
-		return activeColor;
+	public RGBA getActivatedColor() {
+		return activatedColor;
 	}
 	
-	public void setActiveColor(RGBA activeColor) {
-		this.activeColor = activeColor;
+	public void setActivatedColor(RGBA activatedColor) {
+		this.activatedColor = activatedColor;
 	}
 	
 	@Override
 	protected RGBA getCurrentButtonColor(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
-		return active ? activeColor : buttonColor;
+		return activated ? activatedColor : buttonColor;
 	}
 }
