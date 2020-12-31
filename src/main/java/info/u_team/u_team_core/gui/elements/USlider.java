14ac0d01de01ff857c17e2b750d23c3d3dade669
HyCraftHD <hycrafthd@live.de>
2020-12-31 12:25:48 +0100

    Update USlider

--- src/main/java/info/u_team/u_team_core/gui/elements/USlider.java
+++ src/main/java/info/u_team/u_team_core/gui/elements/USlider.java
@@ -1,14 +1,28 @@
 package info.u_team.u_team_core.gui.elements;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+
+import info.u_team.u_team_core.api.gui.*;
+import info.u_team.u_team_core.util.*;
+import net.minecraft.client.Minecraft;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.fml.client.gui.widget.Slider;
 
-public class USlider extends Slider {
+public class USlider extends Slider implements IPerspectiveRenderable, IBackgroundColorProvider, ITextProvider {
 	
 	protected static ISlider EMTPY_SLIDER = slider -> {
 	};
 	
 	protected static ITooltip EMPTY_TOOLTIP = UButton.EMPTY_TOOLTIP;
+	
+	protected static final RGBA WHITE = UButton.WHITE;
+	protected static final RGBA LIGHT_GRAY = UButton.LIGHT_GRAY;
+	
+	protected RGBA sliderBackgroundColor;
+	protected RGBA sliderColor;
+	
+	protected RGBA textColor;
+	protected RGBA disabledTextColor;
 	
 	public USlider(int x, int y, int width, int height, ITextComponent prefix, ITextComponent suffix, double minValue, double maxValue, double value, boolean decimalPrecision, boolean drawDescription) {
 		this(x, y, width, height, prefix, suffix, minValue, maxValue, value, decimalPrecision, drawDescription, EMTPY_SLIDER);
@@ -25,6 +39,10 @@ public class USlider extends Slider {
 	public USlider(int x, int y, int width, int height, ITextComponent prefix, ITextComponent suffix, double minValue, double maxValue, double value, boolean decimalPrecision, boolean drawDescription, ISlider slider, ITooltip tooltip) {
 		super(x, y, width, height, prefix, suffix, minValue, maxValue, value, decimalPrecision, drawDescription, UButton.EMTPY_PRESSABLE, slider);
 		onTooltip = tooltip;
+		sliderBackgroundColor = WHITE;
+		sliderColor = WHITE;
+		textColor = WHITE;
+		disabledTextColor = LIGHT_GRAY;
 	}
 	
 	public void setSlider(ISlider slider) {
@@ -37,6 +55,86 @@ public class USlider extends Slider {
 	
 	public void setTooltip(ITooltip tooltip) {
 		onTooltip = tooltip;
+	}
+	
+	public RGBA getSliderBackgroundColor() {
+		return sliderBackgroundColor;
+	}
+	
+	public void setSliderBackgroundColor(RGBA sliderBackgroundColor) {
+		this.sliderBackgroundColor = sliderBackgroundColor;
+	}
+	
+	public RGBA getSliderColor() {
+		return sliderColor;
+	}
+	
+	public void setSliderColor(RGBA sliderColor) {
+		this.sliderColor = sliderColor;
+	}
+	
+	public RGBA getTextColor() {
+		return textColor;
+	}
+	
+	public void setTextColor(RGBA textColor) {
+		this.textColor = textColor;
+	}
+	
+	public RGBA getDisabledTextColor() {
+		return disabledTextColor;
+	}
+	
+	public void setDisabledTextColor(RGBA disabledTextColor) {
+		this.disabledTextColor = disabledTextColor;
+	}
+	
+	@Override
+	protected void renderBg(MatrixStack matrixStack, Minecraft minecraft, int mouseX, int mouseY) {
+		if (visible) {
+			if (dragging) {
+				sliderValue = (mouseX - (x + 4)) / (float) (width - 8);
+				updateSlider();
+			}
+			RenderUtil.enableBlend();
+			RenderUtil.defaultBlendFunc();
+			GuiUtil.drawContinuousTexturedBox(matrixStack, WIDGETS_LOCATION, x + (int) (sliderValue * (width - 8)), y, 0, 66 + (isHovered() ? 20 : 0), 8, height, 200, 20, 2, 3, 2, 2, getBlitOffset(), getCurrentSliderColor(matrixStack, mouseX, mouseY, 0));
+			RenderUtil.disableBlend();
+		}
+	}
+	
+	@Override
+	public void renderButton(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+		WidgetUtil.renderButtonLikeWidget(this, matrixStack, mouseX, mouseY, partialTicks);
+	}
+	
+	@Override
+	public void renderBackground(MatrixStack matrixStack, Minecraft minecraft, int mouseX, int mouseY, float partialTicks) {
+		renderBg(matrixStack, minecraft, mouseX, mouseY);
+	}
+	
+	@Override
+	public void renderForeground(MatrixStack matrixStack, Minecraft minecraft, int mouseX, int mouseY, float partialTicks) {
+		WidgetUtil.renderText(this, matrixStack, minecraft, mouseX, mouseY, partialTicks);
+	}
+	
+	@Override
+	public RGBA getCurrentBackgroundColor(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+		return sliderBackgroundColor;
+	}
+	
+	public RGBA getCurrentSliderColor(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+		return sliderColor;
+	}
+	
+	@Override
+	public ITextComponent getCurrentText() {
+		return getMessage();
+	}
+	
+	@Override
+	public RGBA getCurrentTextColor(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+		return active ? textColor : disabledTextColor;
 	}
 	
 }
