ab2e1e3fa03be42c36d035e549c142db0a93a9d0
HyCraftHD <hycrafthd@live.de>
2020-12-31 12:28:57 +0100

    Implement scalable slider

---
+++ src/main/java/info/u_team/u_team_core/gui/elements/ScalableSlider.java
@@ -0,0 +1,48 @@
+package info.u_team.u_team_core.gui.elements;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+
+import info.u_team.u_team_core.api.gui.*;
+import info.u_team.u_team_core.util.WidgetUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.text.ITextComponent;
+
+public class ScalableSlider extends USlider implements IScaleable, IScaleProvider {
+	
+	protected float scale;
+	
+	public ScalableSlider(int x, int y, int width, int height, ITextComponent prefix, ITextComponent suffix, double minValue, double maxValue, double value, boolean decimalPrecision, boolean drawDescription, float scale) {
+		this(x, y, width, height, prefix, suffix, minValue, maxValue, value, decimalPrecision, drawDescription, scale, EMTPY_SLIDER);
+	}
+	
+	public ScalableSlider(int x, int y, int width, int height, ITextComponent prefix, ITextComponent suffix, double minValue, double maxValue, double value, boolean decimalPrecision, boolean drawDescription, float scale, ISlider slider) {
+		this(x, y, width, height, prefix, suffix, minValue, maxValue, value, decimalPrecision, drawDescription, scale, slider, EMPTY_TOOLTIP);
+	}
+	
+	public ScalableSlider(int x, int y, int width, int height, ITextComponent prefix, ITextComponent suffix, double minValue, double maxValue, double value, boolean decimalPrecision, boolean drawDescription, float scale, ITooltip tooltip) {
+		this(x, y, width, height, prefix, suffix, minValue, maxValue, value, decimalPrecision, drawDescription, scale, EMTPY_SLIDER, tooltip);
+	}
+	
+	public ScalableSlider(int x, int y, int width, int height, ITextComponent prefix, ITextComponent suffix, double minValue, double maxValue, double value, boolean decimalPrecision, boolean drawDescription, float scale, ISlider slider, ITooltip tooltip) {
+		super(x, y, width, height, prefix, suffix, minValue, maxValue, value, decimalPrecision, drawDescription, slider, tooltip);
+		this.scale = scale;
+	}
+	
+	public float getScale() {
+		return scale;
+	}
+	
+	public void setScale(float scale) {
+		this.scale = scale;
+	}
+	
+	@Override
+	public void renderForeground(MatrixStack matrixStack, Minecraft minecraft, int mouseX, int mouseY, float partialTicks) {
+		WidgetUtil.renderScaledText(this, matrixStack, minecraft, mouseX, mouseY, partialTicks);
+	}
+	
+	@Override
+	public float getCurrentScale(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+		return scale;
+	}
+}
