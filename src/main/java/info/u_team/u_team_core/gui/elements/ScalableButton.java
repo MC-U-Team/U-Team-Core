c0505bc8afb1298a1570631aa700958867e70ab4
HyCraftHD <hycrafthd@live.de>
2020-12-31 01:00:27 +0100

    Update scalable button

--- src/main/java/info/u_team/u_team_core/gui/elements/ScalableButton.java
+++ src/main/java/info/u_team/u_team_core/gui/elements/ScalableButton.java
@@ -2,13 +2,12 @@ package info.u_team.u_team_core.gui.elements;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 
-import info.u_team.u_team_core.api.gui.IScaleable;
+import info.u_team.u_team_core.api.gui.*;
+import info.u_team.u_team_core.util.WidgetUtil;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.text.*;
+import net.minecraft.util.text.ITextComponent;
 
-public class ScalableButton extends UButton implements IScaleable {
+public class ScalableButton extends UButton implements IScaleable, IScaleProvider {
 	
 	protected float scale;
 	
@@ -38,32 +37,12 @@ public class ScalableButton extends UButton implements IScaleable {
 	}
 	
 	@Override
-	protected void renderForeground(MatrixStack matrixStack, Minecraft minecraft, int mouseX, int mouseY, float partialTicks) {
-		if (scale == 1) {
-			super.renderForeground(matrixStack, minecraft, mouseX, mouseY, partialTicks);
-		} else {
-			final FontRenderer fontRenderer = minecraft.fontRenderer;
-			
-			ITextComponent message = getMessage();
-			
-			if (message != StringTextComponent.EMPTY) {
-				final int messageWidth = MathHelper.ceil(scale * fontRenderer.getStringPropertyWidth(message));
-				final int ellipsisWidth = MathHelper.ceil(scale * fontRenderer.getStringWidth("..."));
-				
-				if (messageWidth > width - 6 && messageWidth > ellipsisWidth) {
-					message = new StringTextComponent(fontRenderer.func_238417_a_(message, width - 6 - ellipsisWidth).getString() + "...");
-				}
-				
-				final float positionFactor = 1 / scale;
-				
-				final float xStart = (x + (width / 2) - messageWidth / 2) * positionFactor;
-				final float yStart = (y + ((int) (height - 8 * scale)) / 2) * positionFactor;
-				
-				matrixStack.push();
-				matrixStack.scale(scale, scale, 0);
-				fontRenderer.func_243246_a(matrixStack, message, xStart, yStart, getCurrentTextColor(matrixStack, mouseX, mouseY, partialTicks).getColorARGB());
-				matrixStack.pop();
-			}
-		}
+	public void renderForeground(MatrixStack matrixStack, Minecraft minecraft, int mouseX, int mouseY, float partialTicks) {
+		WidgetUtil.renderScaledText(this, matrixStack, minecraft, mouseX, mouseY, partialTicks);
+	}
+	
+	@Override
+	public float getCurrentScale(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+		return scale;
 	}
 }
