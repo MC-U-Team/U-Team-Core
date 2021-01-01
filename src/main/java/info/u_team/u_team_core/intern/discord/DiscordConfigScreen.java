413213a3d2b7e459194b67e71b4a4b9d8780665f
HyCraftHD <hycrafthd@live.de>
2021-01-01 14:50:46 +0100

    Widget draw tooltips

--- src/main/java/info/u_team/u_team_core/intern/discord/DiscordConfigScreen.java
+++ src/main/java/info/u_team/u_team_core/intern/discord/DiscordConfigScreen.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 
 import info.u_team.u_team_core.gui.elements.*;
 import info.u_team.u_team_core.intern.config.ClientConfig;
+import info.u_team.u_team_core.util.RGBA;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.util.text.*;
 import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
@@ -24,8 +25,7 @@ public class DiscordConfigScreen extends Screen {
 		final ITextComponent on = new TranslationTextComponent("screen.uteamcore.config.discord.on");
 		final ITextComponent off = new TranslationTextComponent("screen.uteamcore.config.discord.off");
 		
-		final ActiveButton toggleDiscordRichPresenceButton = addButton(new ActiveButton(width / 2 - 100, 50, 200, 20, discordRichPresence.get() ? on : off, 0x006442FF));
-		toggleDiscordRichPresenceButton.setActive(discordRichPresence.get());
+		final ScalableActivatableButton toggleDiscordRichPresenceButton = addButton(new ScalableActivatableButton(width / 2 - 100, 50, 200, 20, discordRichPresence.get() ? on : off, 1, discordRichPresence.get(), new RGBA(0x006442FF)));
 		toggleDiscordRichPresenceButton.setPressable(() -> {
 			discordRichPresence.set(!discordRichPresence.get());
 			
@@ -40,7 +40,7 @@ public class DiscordConfigScreen extends Screen {
 				DiscordRichPresence.stop();
 			}
 			
-			toggleDiscordRichPresenceButton.setActive(discordRichPresence.get());
+			toggleDiscordRichPresenceButton.setActivated(discordRichPresence.get());
 			toggleDiscordRichPresenceButton.setMessage(discordRichPresence.get() ? on : off);
 		});
 		
@@ -52,6 +52,7 @@ public class DiscordConfigScreen extends Screen {
 		renderBackground(matrixStack);
 		drawCenteredString(matrixStack, font, title, width / 2, 15, 0xFFFFFF);
 		super.render(matrixStack, mouseX, mouseY, partialTicks);
+		buttons.forEach(widget -> widget.renderToolTip(matrixStack, mouseX, mouseY));
 	}
 	
 	@Override
