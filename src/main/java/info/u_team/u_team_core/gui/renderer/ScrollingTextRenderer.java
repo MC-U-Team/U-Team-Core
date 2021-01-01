1667b47fb5bc922da89d34c515c9b33cf3c0c141
HyCraftHD <hycrafthd@live.de>
2021-01-01 16:51:33 +0100

    Tests

--- src/main/java/info/u_team/u_team_core/gui/renderer/ScrollingTextRenderer.java
+++ src/main/java/info/u_team/u_team_core/gui/renderer/ScrollingTextRenderer.java
@@ -2,9 +2,8 @@ package info.u_team.u_team_core.gui.renderer;
 
 import java.util.function.Supplier;
 
-import org.lwjgl.opengl.GL11;
-
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
 
 import net.minecraft.client.*;
 import net.minecraft.client.gui.FontRenderer;
@@ -81,17 +80,21 @@ public class ScrollingTextRenderer extends ScalingTextRenderer {
 		final int nativeWidth = MathHelper.ceil(width * scaleFactor);
 		final int nativeHeight = MathHelper.ceil((fontRenderSupplier.get().FONT_HEIGHT + 1) * scale * scaleFactor);
 		
-		GL11.glPushMatrix();
-		GL11.glEnable(GL11.GL_SCISSOR_TEST);
+//		GL11.glPushMatrix();
+//		GL11.glEnable(GL11.GL_SCISSOR_TEST);
 		
-		GL11.glScissor(nativeX, window.getHeight() - (nativeY + nativeHeight), nativeWidth, nativeHeight);
+		RenderSystem.enableScissor(nativeX, window.getHeight() - (nativeY + nativeHeight), nativeWidth, nativeHeight);
+		
+//		GL11.glScissor(nativeX, window.getHeight() - (nativeY + nativeHeight), nativeWidth, nativeHeight);
 		// AbstractGui.fill(matrixStack, 0, 0, window.getScaledWidth(), window.getScaledHeight(), 0xFF00FF00); // test scissor
 		
 		setText(textSupplier.get());
 		renderFont(matrixStack, fontRenderSupplier.get(), getMovingX(x), y + 2);
 		
-		GL11.glDisable(GL11.GL_SCISSOR_TEST);
-		GL11.glPopMatrix();
+		RenderSystem.disableScissor();
+		
+//		GL11.glDisable(GL11.GL_SCISSOR_TEST);
+//		GL11.glPopMatrix();
 	}
 	
 	protected float getMovingX(float x) {
