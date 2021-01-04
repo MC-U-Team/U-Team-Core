7efe114369724668a981a8bcf5f7780c8adcc231
HyCraftHD <hycrafthd@live.de>
2021-01-04 14:05:05 +0100

    Update scalable text button

--- src/main/java/info/u_team/u_team_core/gui/elements/ScalableCheckboxButton.java
+++ src/main/java/info/u_team/u_team_core/gui/elements/ScalableCheckboxButton.java
@@ -5,7 +5,7 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 import info.u_team.u_team_core.api.gui.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.*;
 
 public class ScalableCheckboxButton extends CheckboxButton implements IScaleable, IScaleProvider {
 	
@@ -43,17 +43,34 @@ public class ScalableCheckboxButton extends CheckboxButton implements IScaleable
 		if (drawText) {
 			final FontRenderer fontRenderer = minecraft.fontRenderer;
 			
-			final float currentScale = getCurrentScale(matrixStack, mouseX, mouseY, partialTicks);
-			
-			final float positionFactor = 1 / currentScale;
-			
-			final float xStart = (x + 24) * positionFactor;
-			final float yStart = (y + ((int) (height - 8 * currentScale)) / 2) * positionFactor;
-			
-			matrixStack.push();
-			matrixStack.scale(currentScale, currentScale, 0);
-			fontRenderer.func_243246_a(matrixStack, getCurrentText(), xStart, yStart, getCurrentTextColor(matrixStack, mouseX, mouseY, partialTicks).getColorARGB());
-			matrixStack.pop();
+			final ITextComponent message = getCurrentText();
+			if (message != StringTextComponent.EMPTY) {
+				final float currentScale = getCurrentScale(matrixStack, mouseX, mouseY, partialTicks);
+				
+				final float positionFactor = 1 / currentScale;
+				
+				final float xStart;
+				final float yStart = (y + ((int) (height - 8 * currentScale)) / 2) * positionFactor;
+				
+				if (leftSideText) {
+					xStart = (x - (fontRenderer.getStringPropertyWidth(message) + 4)) * positionFactor;
+				} else {
+					xStart = (x + width + 4) * positionFactor;
+				}
+				
+				final int color = getCurrentTextColor(matrixStack, mouseX, mouseY, partialTicks).getColorARGB();
+				
+				matrixStack.push();
+				matrixStack.scale(currentScale, currentScale, 0);
+				
+				if (dropShadow) {
+					fontRenderer.func_243246_a(matrixStack, getCurrentText(), xStart, yStart, color);
+				} else {
+					fontRenderer.func_243248_b(matrixStack, getCurrentText(), xStart, yStart, color);
+				}
+				
+				matrixStack.pop();
+			}
 		}
 	}
 	
