28393ef7547a598346ceb9697701d12ec4baf413
HyCraftHD <hycrafthd@live.de>
2021-01-05 19:57:37 +0100

    Copy things from previous text field

--- src/main/java/info/u_team/u_team_core/gui/elements/UTextField.java
+++ src/main/java/info/u_team/u_team_core/gui/elements/UTextField.java
@@ -7,13 +7,21 @@ import net.minecraft.util.text.ITextComponent;
 
 public class UTextField extends TextFieldWidget implements IRenderTickable {
 	
-	public UTextField(FontRenderer fontRenderer, int x, int y, int width, int height, TextFieldWidget previousTextField, ITextComponent title) {
-		super(fontRenderer, x, y, width, height, previousTextField, title);
+	public UTextField(FontRenderer fontRenderer, int x, int y, int width, int height, ITextComponent title) {
+		super(fontRenderer, x, y, width, height, title);
 	}
 	
 	@Override
 	public void renderTick() {
 		tick();
+	}
+	
+	public void setPreviousText(UTextField textField) {
+		text = textField.text;
+		maxStringLength = textField.maxStringLength;
+		lineScrollOffset = textField.lineScrollOffset;
+		cursorPosition = textField.cursorPosition;
+		selectionEnd = textField.selectionEnd;
 	}
 	
 }
