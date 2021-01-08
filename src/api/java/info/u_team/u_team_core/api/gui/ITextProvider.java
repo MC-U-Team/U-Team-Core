c2b277da7d2c847b92de5d308eddfa3a46dedef4
HyCraftHD <hycrafthd@live.de>
2021-01-08 19:28:26 +0100

    Refactor ITextProvider in two interfaces

--- src/api/java/info/u_team/u_team_core/api/gui/ITextProvider.java
+++ src/api/java/info/u_team/u_team_core/api/gui/ITextProvider.java
@@ -1,14 +1,9 @@
 package info.u_team.u_team_core.api.gui;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
-
-import info.u_team.u_team_core.util.RGBA;
 import net.minecraft.util.text.ITextComponent;
 
-public interface ITextProvider {
+public interface ITextProvider extends ITextColorProvider {
 	
 	ITextComponent getCurrentText();
-	
-	RGBA getCurrentTextColor(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks);
 	
 }
