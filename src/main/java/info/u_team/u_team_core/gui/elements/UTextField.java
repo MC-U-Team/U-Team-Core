b88280d45245aea6b03ffcb627d61673c4ddac98
HyCraftHD <hycrafthd@live.de>
2021-01-08 20:03:43 +0100

    Finish UTextField. Refactored the vanilla code and added tooltips

--- src/main/java/info/u_team/u_team_core/gui/elements/UTextField.java
+++ src/main/java/info/u_team/u_team_core/gui/elements/UTextField.java
@@ -1,15 +1,55 @@
 package info.u_team.u_team_core.gui.elements;
 
-import info.u_team.u_team_core.api.gui.IRenderTickable;
-import net.minecraft.client.gui.FontRenderer;
+import com.mojang.blaze3d.matrix.MatrixStack;
+
+import info.u_team.u_team_core.api.gui.*;
+import info.u_team.u_team_core.util.RGBA;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.*;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.util.text.ITextComponent;
 
-public class UTextField extends TextFieldWidget implements IRenderTickable {
+public class UTextField extends TextFieldWidget implements IRenderTickable, IPerspectiveRenderable, IBackgroundColorProvider, ITextColorProvider {
+	
+	protected static ITooltip EMPTY_TOOLTIP = (textField, matrixStack, mouseX, mouseY) -> {
+	};
+	
+	protected static final RGBA BLACK = RGBA.BLACK;
+	protected static final RGBA WHITE = RGBA.WHITE;
+	
+	protected static final RGBA LIGHT_GRAY = new RGBA(0xE0E0E0FF);
+	protected static final RGBA LIGHTER_GRAY = new RGBA(0xD0D0D0FF);
+	protected static final RGBA GRAY = new RGBA(0xA0A0A0FF);
+	protected static final RGBA DARKER_GRAY = new RGBA(0x808080FF);
+	protected static final RGBA DARK_GRAY = new RGBA(0x707070FF);
+	
+	protected ITooltip onTooltip;
+	
+	protected RGBA backgroundFrameColor;
+	protected RGBA unfocusedBackgroundFrameColor;
+	protected RGBA backgroundColor;
+	
+	protected RGBA textColor;
+	protected RGBA disabledTextColor;
+	protected RGBA suggestionTextColor;
+	
+	protected RGBA cursorColor;
 	
 	public UTextField(FontRenderer fontRenderer, int x, int y, int width, int height, UTextField previousTextField, ITextComponent title) {
+		this(fontRenderer, x, y, width, height, previousTextField, title, EMPTY_TOOLTIP);
+	}
+	
+	public UTextField(FontRenderer fontRenderer, int x, int y, int width, int height, UTextField previousTextField, ITextComponent title, ITooltip tooltip) {
 		super(fontRenderer, x, y, width, height, title);
 		setPreviousText(previousTextField);
+		onTooltip = tooltip;
+		backgroundFrameColor = WHITE;
+		unfocusedBackgroundFrameColor = GRAY;
+		backgroundColor = BLACK;
+		textColor = LIGHT_GRAY;
+		disabledTextColor = DARK_GRAY;
+		suggestionTextColor = DARKER_GRAY;
+		cursorColor = LIGHTER_GRAY;
 	}
 	
 	@Override
@@ -25,6 +65,113 @@ public class UTextField extends TextFieldWidget implements IRenderTickable {
 			cursorPosition = textField.cursorPosition;
 			selectionEnd = textField.selectionEnd;
 		}
+	}
+	
+	@Override
+	public void renderButton(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+		final Minecraft minecraft = Minecraft.getInstance();
+		renderBackground(matrixStack, minecraft, mouseX, mouseY, partialTicks);
+		renderForeground(matrixStack, minecraft, mouseX, mouseY, partialTicks);
+	}
+	
+	@Override
+	public void renderBackground(MatrixStack matrixStack, Minecraft minecraft, int mouseX, int mouseY, float partialTicks) {
+		if (enableBackgroundDrawing) {
+			fill(matrixStack, x - 1, y - 1, x + width + 1, y + height + 1, getCurrentBackgroundFrameColor(matrixStack, mouseX, mouseY, partialTicks).getColorARGB());
+			fill(matrixStack, x, y, x + width, y + height, getCurrentBackgroundColor(matrixStack, mouseX, mouseY, partialTicks).getColorARGB());
+		}
+	}
+	
+	@Override
+	public void renderForeground(MatrixStack matrixStack, Minecraft minecraft, int mouseX, int mouseY, float partialTicks) {
+		final RGBA currentTextColor = getCurrentTextColor(matrixStack, mouseX, mouseY, partialTicks);
+		
+		final String currentText = fontRenderer.func_238412_a_(text.substring(lineScrollOffset), getAdjustedWidth());
+		
+		final int cursorOffset = cursorPosition - lineScrollOffset;
+		final int selectionOffset = Math.min(selectionEnd - lineScrollOffset, currentText.length());
+		
+		final boolean isCursorInText = cursorOffset >= 0 && cursorOffset <= currentText.length();
+		final boolean shouldCursorBlink = isFocused() && cursorCounter / 6 % 2 == 0 && isCursorInText;
+		final boolean isCursorInTheMiddle = cursorPosition < text.length() || text.length() >= maxStringLength;
+		
+		final int xOffset = enableBackgroundDrawing ? x + 4 : x;
+		final int yOffset = enableBackgroundDrawing ? y + (height - 8) / 2 : y;
+		
+		int leftRenderedTextX = xOffset;
+		
+		if (!currentText.isEmpty()) {
+			final String firstTextPart = isCursorInText ? currentText.substring(0, cursorOffset) : currentText;
+			leftRenderedTextX = fontRenderer.func_238407_a_(matrixStack, textFormatter.apply(firstTextPart, lineScrollOffset), xOffset, yOffset, currentTextColor.getColorARGB());
+		}
+		
+		int rightRenderedTextX = leftRenderedTextX;
+		
+		if (!isCursorInText) {
+			rightRenderedTextX = cursorOffset > 0 ? xOffset + width : xOffset;
+		} else if (isCursorInTheMiddle) {
+			rightRenderedTextX = leftRenderedTextX - 1;
+			--leftRenderedTextX;
+		}
+		
+		if (!currentText.isEmpty() && isCursorInText && cursorOffset < currentText.length()) {
+			fontRenderer.func_238407_a_(matrixStack, textFormatter.apply(currentText.substring(cursorOffset), cursorPosition), leftRenderedTextX, yOffset, currentTextColor.getColorARGB());
+		}
+		
+		if (!isCursorInTheMiddle && suggestion != null) {
+			fontRenderer.drawStringWithShadow(matrixStack, suggestion, rightRenderedTextX - 1, yOffset, getCurrentSuggestionTextColor(matrixStack, mouseX, mouseY, partialTicks).getColorARGB());
+		}
+		
+		if (shouldCursorBlink) {
+			if (isCursorInTheMiddle) {
+				AbstractGui.fill(matrixStack, rightRenderedTextX, yOffset - 1, rightRenderedTextX + 1, yOffset + 1 + 9, getCurrentCursorColor(matrixStack, mouseX, mouseY, partialTicks).getColorARGB());
+			} else {
+				fontRenderer.drawStringWithShadow(matrixStack, "_", rightRenderedTextX, yOffset, currentTextColor.getColorARGB());
+			}
+		}
+		
+		if (selectionOffset != cursorOffset) {
+			final int selectedX = xOffset + fontRenderer.getStringWidth(currentText.substring(0, selectionOffset));
+			drawSelectionBox(rightRenderedTextX, yOffset - 1, selectedX - 1, yOffset + 1 + 9);
+		}
+	}
+	
+	@Override
+	public void renderToolTip(MatrixStack matrixStack, Minecraft minecraft, int mouseX, int mouseY, float partialTicks) {
+		renderToolTip(matrixStack, mouseX, mouseY);
+	}
+	
+	@Override
+	public void renderToolTip(MatrixStack matrixStack, int mouseX, int mouseY) {
+		onTooltip.onTooltip(this, matrixStack, mouseX, mouseY);
+	}
+	
+	@Override
+	public RGBA getCurrentBackgroundColor(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+		return backgroundColor;
+	}
+	
+	public RGBA getCurrentBackgroundFrameColor(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+		return isFocused() ? backgroundFrameColor : unfocusedBackgroundFrameColor;
+	}
+	
+	@Override
+	public RGBA getCurrentTextColor(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+		return isEnabled ? textColor : disabledTextColor;
+	}
+	
+	public RGBA getCurrentSuggestionTextColor(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+		return suggestionTextColor;
+	}
+	
+	public RGBA getCurrentCursorColor(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+		return cursorColor;
+	}
+	
+	@FunctionalInterface
+	public interface ITooltip {
+		
+		void onTooltip(UTextField textField, MatrixStack matrixStack, int mouseX, int mouseY);
 	}
 	
 }
