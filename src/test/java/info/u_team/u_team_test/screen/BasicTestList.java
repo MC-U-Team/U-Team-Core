549363c040b025b1437139d98b2cabeda7ace213
HyCraftHD <hycrafthd@live.de>
2021-01-10 23:48:47 +0100

    Remove unused param

--- src/test/java/info/u_team/u_team_test/screen/BasicTestList.java
+++ src/test/java/info/u_team/u_team_test/screen/BasicTestList.java
@@ -5,7 +5,7 @@ import info.u_team.u_team_core.gui.elements.ScrollableList;
 public class BasicTestList extends ScrollableList<BasicTestListEntry> {
 	
 	public BasicTestList(int x, int y, int width, int height) {
-		super(x, y, width, height, 20, 15, -200);
+		super(x, y, width, height, 20, 15);
 		func_244606_c(false);
 		func_244605_b(false);
 		
