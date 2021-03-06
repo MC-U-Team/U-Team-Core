2bbb1763c95c8973b10137afc3331a422dc314b0
HyCraftHD <hycrafthd@live.de>
2021-03-06 13:15:02 +0100

    Remove client config, as it is not used anymore

--- src/main/java/info/u_team/u_team_core/intern/init/UCoreCommonConstruct.java
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreCommonConstruct.java
@@ -2,19 +2,14 @@ package info.u_team.u_team_core.intern.init;
 
 import info.u_team.u_team_core.UCoreMod;
 import info.u_team.u_team_core.api.construct.*;
-import info.u_team.u_team_core.intern.config.ClientConfig;
 import info.u_team.u_team_core.util.registry.BusRegister;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.config.ModConfig.Type;
 
 @Construct(modid = UCoreMod.MODID)
 public class UCoreCommonConstruct implements IModConstruct {
 	
 	@Override
 	public void construct() {
-		ModLoadingContext.get().registerConfig(Type.CLIENT, ClientConfig.CONFIG);
-		
-		BusRegister.registerMod(UCoreNetwork::registerMod);
+		BusRegister.registerMod(UCoreNetwork::registerMod);	
 		BusRegister.registerMod(UCoreRecipeSerializers::registerMod);
 		BusRegister.registerMod(UCoreLazySpawnEggs::registerMod);
 		BusRegister.registerMod(UCoreLootFunctions::registerMod);
