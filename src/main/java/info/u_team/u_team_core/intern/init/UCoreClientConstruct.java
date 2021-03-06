c477207d4545bd8c6259b2f931f911cfb2077d79
HyCraftHD <hycrafthd@live.de>
2021-03-06 13:14:27 +0100

    Remove discord classes

--- src/main/java/info/u_team/u_team_core/intern/init/UCoreClientConstruct.java
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreClientConstruct.java
@@ -2,21 +2,14 @@ package info.u_team.u_team_core.intern.init;
 
 import info.u_team.u_team_core.UCoreMod;
 import info.u_team.u_team_core.api.construct.*;
-import info.u_team.u_team_core.intern.discord.*;
 import info.u_team.u_team_core.util.registry.BusRegister;
-import net.minecraftforge.fml.*;
 
 @Construct(modid = UCoreMod.MODID, client = true)
 public class UCoreClientConstruct implements IModConstruct {
 	
 	@Override
 	public void construct() {
-		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () -> (minecraft, screen) -> new DiscordConfigScreen(screen));
-		
-		BusRegister.registerMod(UpdateDiscordEventHandler::registerMod);
 		BusRegister.registerMod(UCoreColors::registerMod);
-		
-		BusRegister.registerForge(UpdateDiscordEventHandler::registerForge);
 	}
 	
 }
