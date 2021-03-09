cd07e73f8447d2e767433a1482de3a59cb1bb601
HyCraftHD <hycrafthd@live.de>
2021-03-09 14:36:39 +0100

    Use new EntityAttributeCreationEvent for registering entity attributes

--- src/test/java/info/u_team/u_team_test/init/TestGlobalEntityTypeAttributes.java
+++ src/test/java/info/u_team/u_team_test/init/TestGlobalEntityTypeAttributes.java
@@ -1,20 +1,17 @@
 package info.u_team.u_team_test.init;
 
 import info.u_team.u_team_test.entity.TestLivingEntity;
-import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 public class TestGlobalEntityTypeAttributes {
 	
-	private static void setup(FMLCommonSetupEvent event) {
-		event.enqueueWork(() -> {
-			GlobalEntityTypeAttributes.put(TestEntityTypes.TEST_LIVING.get(), TestLivingEntity.registerAttributes().create());
-		});
+	private static void entityAttributionCreation(EntityAttributeCreationEvent event) {
+		event.put(TestEntityTypes.TEST_LIVING.get(), TestLivingEntity.registerAttributes().create());
 	}
 	
 	public static void registerMod(IEventBus bus) {
-		bus.addListener(TestGlobalEntityTypeAttributes::setup);
+		bus.addListener(TestGlobalEntityTypeAttributes::entityAttributionCreation);
 	}
 	
 }
