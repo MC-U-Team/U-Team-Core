2a43516e34d8cbdfa33b496d3a938cfd9f9429a3
HyCraftHD <hycrafthd@live.de>
2021-07-24 20:34:13 +0200

    Fix some internal classes

--- src/main/java/info/u_team/u_team_core/intern/init/UCoreCommonConstruct.java
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreCommonConstruct.java
@@ -2,13 +2,11 @@ package info.u_team.u_team_core.intern.init;
 
 import java.util.Objects;
 
-import org.apache.commons.lang3.tuple.Pair;
-
 import info.u_team.u_team_core.UCoreMod;
 import info.u_team.u_team_core.api.construct.Construct;
 import info.u_team.u_team_core.api.construct.IModConstruct;
 import info.u_team.u_team_core.util.registry.BusRegister;
-import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.IExtensionPoint.DisplayTest;
 import net.minecraftforge.fml.ModLoadingContext;
 
 @Construct(modid = UCoreMod.MODID)
@@ -17,7 +15,7 @@ public class UCoreCommonConstruct implements IModConstruct {
 	@Override
 	public void construct() {
 		final String version = ModLoadingContext.get().getActiveContainer().getModInfo().getVersion().toString();
-		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> version, (remoteVersion, network) -> {
+		ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> version, (remoteVersion, network) -> {
 			if (remoteVersion == null) {
 				return true;
 			}
--- src/main/java/info/u_team/u_team_core/intern/init/UCoreLazySpawnEggs.java
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreLazySpawnEggs.java
@@ -3,6 +3,7 @@ package info.u_team.u_team_core.intern.init;
 import info.u_team.u_team_core.item.USpawnEggItem;
 import net.minecraft.core.Direction;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.SpawnEggItem;
@@ -35,7 +36,7 @@ public class UCoreLazySpawnEggs {
 	private static void loadComplete(FMLLoadCompleteEvent event) {
 		event.enqueueWork(() -> {
 			USpawnEggItem.LAZY_EGGS.forEach(pair -> {
-				final EntityType<?> type = pair.getKey().get();
+				final EntityType<? extends Mob> type = (EntityType<? extends Mob>) pair.getKey().get();
 				final USpawnEggItem item = pair.getValue();
 				
 				SpawnEggItem.BY_ID.put(type, item);
