bb61cdd45b120ebf619554685cde1c42a99d1665
HyCraftHD <hycrafthd@live.de>
2021-07-24 21:47:58 +0200

    Fix client registry

--- src/main/java/info/u_team/u_team_core/util/registry/ClientRegistry.java
+++ src/main/java/info/u_team/u_team_core/util/registry/ClientRegistry.java
@@ -1,21 +1,20 @@
 package info.u_team.u_team_core.util.registry;
 
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.MenuAccess;
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraftforge.fmlclient.registry.IRenderFactory;
 import net.minecraftforge.fmlclient.registry.RenderingRegistry;
 
 public class ClientRegistry {
@@ -24,20 +23,16 @@ public class ClientRegistry {
 		MenuScreens.register(supplier.get(), factory);
 	}
 	
-	public static <T extends Entity> void registerEntityRenderer(Supplier<? extends EntityType<T>> supplier, IRenderFactory<? super T> rendererFactory) {
-		RenderingRegistry.registerEntityRenderingHandler(supplier.get(), rendererFactory);
+	public static <T extends Entity> void registerEntityRenderer(Supplier<? extends EntityType<T>> supplier, EntityRendererProvider<? super T> provider) {
+		RenderingRegistry.registerEntityRenderingHandler(supplier.get(), provider);
 	}
 	
-	public static <T extends BlockEntity> void registerSpecialTileEntityRenderer(Supplier<? extends BlockEntityType<T>> supplier, BlockEntityRenderer<? super T> renderer) {
-		registerSpecialTileEntityRenderer(supplier, dispatcher -> renderer);
-	}
-	
-	public static <T extends BlockEntity> void registerSpecialTileEntityRenderer(Supplier<? extends BlockEntityType<T>> supplier, Function<? super BlockEntityRenderDispatcher, ? extends BlockEntityRenderer<? super T>> rendererFactory) {
-		net.minecraftforge.fml.client.registry.ClientRegistry.bindTileEntityRenderer(supplier.get(), rendererFactory);
+	public static <T extends BlockEntity> void registerSpecialTileEntityRenderer(Supplier<? extends BlockEntityType<T>> supplier, BlockEntityRendererProvider<T> provider) {
+		BlockEntityRenderers.register(supplier.get(), provider);
 	}
 	
 	public static void registerKeybinding(KeyMapping key) {
-		net.minecraftforge.fml.client.registry.ClientRegistry.registerKeyBinding(key);
+		net.minecraftforge.fmlclient.registry.ClientRegistry.registerKeyBinding(key);
 	}
 	
 }
