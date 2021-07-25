f48ef3b5584cdb24554a201c461e2ab871f0467a
HyCraftHD <hycrafthd@live.de>
2021-07-25 13:08:55 +0200

    Fix test enchantment

--- src/test/java/info/u_team/u_team_test/enchantment/AutoSmeltEnchantment.java
+++ src/test/java/info/u_team/u_team_test/enchantment/AutoSmeltEnchantment.java
@@ -1,12 +1,12 @@
 package info.u_team.u_team_test.enchantment;
 
-import net.minecraft.enchantment.EnchantmentType;
-import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
 public class AutoSmeltEnchantment extends Enchantment {
 	
 	public AutoSmeltEnchantment() {
-		super(Rarity.COMMON, EnchantmentType.DIGGER, new EquipmentSlotType[] { EquipmentSlotType.MAINHAND });
+		super(Rarity.COMMON, EnchantmentCategory.DIGGER, new EquipmentSlot[] { EquipmentSlot.MAINHAND });
 	}
 }
