b249c2bd64b5eda2294ace87bcf23d5e49bddd51
HyCraftHD <hycrafthd@live.de>
2021-07-25 12:39:06 +0200

    Fix potion

--- src/test/java/info/u_team/u_team_test/potion/RadiationPotion.java
+++ src/test/java/info/u_team/u_team_test/potion/RadiationPotion.java
@@ -1,12 +1,12 @@
 package info.u_team.u_team_test.potion;
 
 import info.u_team.u_team_test.init.TestEffects;
-import net.minecraft.client.renderer.EffectInstance;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.alchemy.Potion;
 
 public class RadiationPotion extends Potion {
 	
 	public RadiationPotion(int duration, int amplifier) {
-		super("radiation", new EffectInstance(TestEffects.RADIATION.get(), duration, amplifier));
+		super("radiation", new MobEffectInstance(TestEffects.RADIATION.get(), duration, amplifier));
 	}
 }
