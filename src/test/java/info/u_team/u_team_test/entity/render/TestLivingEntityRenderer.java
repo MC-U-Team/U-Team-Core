cbfa1bd5ebb2cc7a2f54b3c483d1e611b6f69098
HyCraftHD <hycrafthd@live.de>
2021-07-25 13:34:16 +0200

    Fix test living entity renderer

--- src/test/java/info/u_team/u_team_test/entity/render/TestLivingEntityRenderer.java
+++ src/test/java/info/u_team/u_team_test/entity/render/TestLivingEntityRenderer.java
@@ -1,29 +1,29 @@
 package info.u_team.u_team_test.entity.render;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.PoseStack;
 
 import info.u_team.u_team_test.TestMod;
-import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.ZombieRenderer;
-import net.minecraft.entity.monster.ZombieEntity;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.monster.Zombie;
 
 public class TestLivingEntityRenderer extends ZombieRenderer {
 	
 	private static final ResourceLocation TEXTURE = new ResourceLocation(TestMod.MODID, "textures/entity/test_living/test_living.png");
 	
-	public TestLivingEntityRenderer(EntityRendererManager renderManager) {
+	public TestLivingEntityRenderer(EntityRendererProvider.Context renderManager) {
 		super(renderManager);
 	}
 	
 	@Override
-	protected void preRenderCallback(ZombieEntity zombie, MatrixStack matrixStack, float partialTickTime) {
+	protected void scale(Zombie zombie, PoseStack matrixStack, float partialTickTime) {
 		matrixStack.scale(1.8F, 1.8F, 1.8F);
-		super.preRenderCallback(zombie, matrixStack, partialTickTime);
+		super.scale(zombie, matrixStack, partialTickTime);
 	}
 	
 	@Override
-	public ResourceLocation getEntityTexture(ZombieEntity entity) {
+	public ResourceLocation getTextureLocation(Zombie entity) {
 		return TEXTURE;
 	}
 	
