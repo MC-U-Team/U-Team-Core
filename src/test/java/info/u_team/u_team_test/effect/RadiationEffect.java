b7b631e5e08547d9ac241d141156f3347a28bcfc
HyCraftHD <hycrafthd@live.de>
2021-07-25 12:57:53 +0200

    Fix effect

--- src/test/java/info/u_team/u_team_test/effect/RadiationEffect.java
+++ src/test/java/info/u_team/u_team_test/effect/RadiationEffect.java
@@ -3,32 +3,31 @@ package info.u_team.u_team_test.effect;
 import java.util.Random;
 
 import info.u_team.u_team_test.init.TestDamageSources;
-import net.minecraft.client.gui.font.glyphs.BakedGlyph.Effect;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.potion.EffectType;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 
-public class RadiationEffect extends Effect {
+public class RadiationEffect extends MobEffect {
 	
 	private final Random random;
 	
 	public RadiationEffect() {
-		super(EffectType.HARMFUL, 0x0B7A14);
+		super(MobEffectCategory.HARMFUL, 0x0B7A14);
 		random = new Random();
 	}
 	
 	@Override
-	public void performEffect(LivingEntity entityLiving, int amplifier) {
+	public void applyEffectTick(LivingEntity entityLiving, int amplifier) {
 		amplifier += 2;
-		entityLiving.attackEntityFrom(TestDamageSources.RADIATION, random.nextInt(amplifier));
-		if (entityLiving instanceof PlayerEntity) {
-			final PlayerEntity player = (PlayerEntity) entityLiving;
-			player.addExhaustion(0.005F * amplifier);
+		entityLiving.hurt(TestDamageSources.RADIATION, random.nextInt(amplifier));
+		if (entityLiving instanceof Player player) {
+			player.causeFoodExhaustion(0.005F * amplifier);
 		}
 	}
 	
 	@Override
-	public boolean isReady(int duration, int amplifier) {
+	public boolean isDurationEffectTick(int duration, int amplifier) {
 		return random.nextInt(Math.max(20 - (amplifier * 2), 2)) == 0;
 	}
 }
