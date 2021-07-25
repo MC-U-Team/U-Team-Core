df77529adf9b72726f84de92238dc8aecbad9828
HyCraftHD <hycrafthd@live.de>
2021-07-25 13:08:19 +0200

    Update test blocks

--- src/test/java/info/u_team/u_team_test/block/BasicFluidInventoryBlock.java
+++ src/test/java/info/u_team/u_team_test/block/BasicFluidInventoryBlock.java
@@ -4,21 +4,22 @@ import info.u_team.u_team_core.block.UTileEntityBlock;
 import info.u_team.u_team_test.init.TestItemGroups;
 import info.u_team.u_team_test.init.TestTileEntityTypes;
 import net.minecraft.core.BlockPos;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.world.World;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.BlockHitResult;
 
 public class BasicFluidInventoryBlock extends UTileEntityBlock {
 	
 	public BasicFluidInventoryBlock() {
-		super(TestItemGroups.GROUP, Properties.create(Material.IRON).hardnessAndResistance(1), TestTileEntityTypes.BASIC_FLUID_INVENTORY);
+		super(TestItemGroups.GROUP, Properties.of(Material.METAL).strength(1), TestTileEntityTypes.BASIC_FLUID_INVENTORY);
 	}
 	
 	@Override
-	public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult hit) {
+	public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
 		return openContainer(world, pos, player, true);
 	}
 	
--- src/test/java/info/u_team/u_team_test/block/BasicTileEntityBlock.java
+++ src/test/java/info/u_team/u_team_test/block/BasicTileEntityBlock.java
@@ -4,37 +4,42 @@ import info.u_team.u_team_core.block.UTileEntityBlock;
 import info.u_team.u_team_test.init.TestItemGroups;
 import info.u_team.u_team_test.init.TestTileEntityTypes;
 import net.minecraft.core.BlockPos;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.BlockItemUseContext;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.world.World;
+import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition.Builder;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.BlockHitResult;
 
 public class BasicTileEntityBlock extends UTileEntityBlock {
 	
 	public static final DirectionProperty FACING = BlockStateProperties.FACING;
 	
 	public BasicTileEntityBlock() {
-		super(TestItemGroups.GROUP, Properties.create(Material.ROCK).hardnessAndResistance(2F).sound(SoundType.GROUND).slipperiness(0.8F).setLightLevel(state -> 1), TestTileEntityTypes.BASIC);
-		setDefaultState(getDefaultState().with(FACING, Direction.NORTH));
+		super(TestItemGroups.GROUP, Properties.of(Material.STONE).strength(2F).sound(SoundType.GRAVEL).friction(0.8F).lightLevel(state -> 1), TestTileEntityTypes.BASIC);
+		registerDefaultState(defaultBlockState().setValue(FACING, Direction.NORTH));
 	}
 	
 	@Override
-	public BlockState getStateForPlacement(BlockItemUseContext context) {
-		return getDefaultState().with(FACING, context.getNearestLookingDirection().getOpposite());
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		return defaultBlockState().setValue(FACING, context.getNearestLookingDirection().getOpposite());
 	}
 	
 	@Override
-	public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult hit) {
+	public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
 		return openContainer(world, pos, player, true);
 	}
 	
 	@Override
-	protected void fillStateContainer(Builder<Block, BlockState> builder) {
+	protected void createBlockStateDefinition(Builder<Block, BlockState> builder) {
 		builder.add(FACING);
 	}
 	
