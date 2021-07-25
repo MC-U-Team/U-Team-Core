bb034b17d1be5b51ba3bc322185a13fa23310bcc
HyCraftHD <hycrafthd@live.de>
2021-07-25 12:54:33 +0200

    Fix uteamtest2

--- src/test/java/info/u_team/u_team_test2/init/Test2CommonBusRegister.java
+++ src/test/java/info/u_team/u_team_test2/init/Test2CommonBusRegister.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import info.u_team.u_team_core.api.construct.Construct;
 import info.u_team.u_team_core.api.construct.IModConstruct;
 import info.u_team.u_team_test2.TestMod2;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.event.RegistryEvent.Register;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
