50a5cbf230ba2c566bf99ef6d1023820a639b282
HyCraftHD <hycrafthd@live.de>
2021-08-07 23:09:02 +0200

    Update entity renderer to use the new events

--- src/test/java/info/u_team/u_team_test/init/TestRenderers.java
+++ src/test/java/info/u_team/u_team_test/init/TestRenderers.java
@@ -1,19 +1,18 @@
 package info.u_team.u_team_test.init;
 
-import info.u_team.u_team_core.util.registry.ClientRegistry;
 import info.u_team.u_team_test.entity.render.TestLivingEntityRenderer;
 import net.minecraft.client.renderer.entity.ThrownItemRenderer;
+import net.minecraftforge.client.event.EntityRenderersEvent.RegisterRenderers;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class TestRenderers {
-
-	private static void setup(FMLClientSetupEvent event) {
-		ClientRegistry.registerEntityRenderer(TestEntityTypes.BETTER_ENDERPEARL, ThrownItemRenderer::new);
-		ClientRegistry.registerEntityRenderer(TestEntityTypes.TEST_LIVING, TestLivingEntityRenderer::new);
+	
+	private static void registerRenderers(RegisterRenderers event) {
+		event.registerEntityRenderer(TestEntityTypes.BETTER_ENDERPEARL.get(), ThrownItemRenderer::new);
+		event.registerEntityRenderer(TestEntityTypes.TEST_LIVING.get(), TestLivingEntityRenderer::new);
 	}
-
+	
 	public static void registerMod(IEventBus bus) {
-		bus.addListener(TestRenderers::setup);
+		bus.addListener(TestRenderers::registerRenderers);
 	}
 }
