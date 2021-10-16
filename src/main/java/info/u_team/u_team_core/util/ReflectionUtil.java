890763bd0bcf8546f84220f47fce948b9b235100
HyCraftHD <hycrafthd@live.de>
2021-10-17 00:45:44 +0200

    Add docs for reflection util

--- src/main/java/info/u_team/u_team_core/util/ReflectionUtil.java
+++ src/main/java/info/u_team/u_team_core/util/ReflectionUtil.java
@@ -7,14 +7,34 @@ import org.apache.logging.log4j.Logger;
 
 import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 
+/**
+ * Utility class for reflection stuff
+ * 
+ * @author HyCraftHD
+ */
 public class ReflectionUtil {
 	
 	private static final Logger LOGGER = LogManager.getLogger();
 	
+	/**
+	 * Method to find a field in a give class. The name will be mapped to the right mappings. Will return the field with
+	 * widened access.
+	 * 
+	 * @param clazz Class of the field
+	 * @param field The name of the field
+	 * @return The field
+	 */
 	public static Field findField(Class<?> clazz, String field) {
 		return ObfuscationReflectionHelper.findField(clazz, field);
 	}
 	
+	/**
+	 * Sets the value of a field. Catches exceptions and throws a runtime exception.
+	 * 
+	 * @param field The field
+	 * @param instance The object instance. Null for static fields
+	 * @param value The value
+	 */
 	public static <I, V> void setValue(Field field, I instance, V value) {
 		try {
 			field.set(instance, value);
@@ -25,6 +45,13 @@ public class ReflectionUtil {
 		}
 	}
 	
+	/**
+	 * Gets the value of a field. Catches exceptions and throws a runtime exception.
+	 * 
+	 * @param field The field
+	 * @param instance The object instance. Null for static fields
+	 * @return The value
+	 */
 	public static <I, V> V getValue(Field field, I instance) {
 		try {
 			return CastUtil.uncheckedCast(field.get(instance));
@@ -35,10 +62,23 @@ public class ReflectionUtil {
 		}
 	}
 	
+	/**
+	 * Utility method to copy a field value from an old oject instance to a new instance. Catches exceptions and throws a
+	 * runtime exception.
+	 * 
+	 * @param field The field
+	 * @param oldInstance Old object
+	 * @param newInstance New object
+	 */
 	public static <O, N> void copyValue(Field field, O oldInstance, N newInstance) {
 		setValue(field, newInstance, getValue(field, oldInstance));
 	}
 	
+	/**
+	 * Runtime exeception for field access exceptions.
+	 * 
+	 * @author HyCraftHD
+	 */
 	private static class AccessFieldException extends RuntimeException {
 		
 		private static final long serialVersionUID = 1L;
