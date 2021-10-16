0b9700623348046ceb5f2e7878a2e5998dba7bfa
HyCraftHD <hycrafthd@live.de>
2021-10-17 01:03:37 +0200

    Document recipe util

--- src/main/java/info/u_team/u_team_core/util/RecipeBuilderUtil.java
+++ src/main/java/info/u_team/u_team_core/util/RecipeBuilderUtil.java
@@ -6,8 +6,20 @@ import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 
+/**
+ * Utility methods for recipes
+ * 
+ * @author HyCraftHD
+ */
 public class RecipeBuilderUtil {
 	
+	/**
+	 * Returns a {@link FinishedRecipe} with a custom serializer.
+	 * 
+	 * @param recipe Finished recipe
+	 * @param serializer Serializer
+	 * @return Finished recipe with serializer
+	 */
 	public static FinishedRecipe getRecipeWithSerializer(FinishedRecipe recipe, RecipeSerializer<?> serializer) {
 		return new FinishedRecipe() {
 			
