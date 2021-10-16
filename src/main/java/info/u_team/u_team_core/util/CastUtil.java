91b512badcb9d2842381f135abbbe0fbde49a87c
HyCraftHD <hycrafthd@live.de>
2021-10-17 00:48:42 +0200

    Document cast util

--- src/main/java/info/u_team/u_team_core/util/CastUtil.java
+++ src/main/java/info/u_team/u_team_core/util/CastUtil.java
@@ -1,9 +1,22 @@
 package info.u_team.u_team_core.util;
 
+/**
+ * Utility class for casts
+ * 
+ * @author HyCraftHD
+ */
 public class CastUtil {
 	
+	/**
+	 * Convenient helper method for unchecked class without suppressing warnings. CAREFULL, CAST IS STILL UNCHECKED!
+	 * 
+	 * @param <T> The type that the object should be
+	 * @param object The object
+	 * @return Casted object to the desired type
+	 * @throws ClassCastException when unchecked cast fails
+	 */
 	@SuppressWarnings("unchecked")
-	public static <T> T uncheckedCast(Object object) {
+	public static <T> T uncheckedCast(Object object) throws ClassCastException {
 		return (T) object;
 	}
 	
