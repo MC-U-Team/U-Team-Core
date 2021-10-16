c94ee2e32782b2fd7a37d8ef51e4b98ce4dd00c0
HyCraftHD <hycrafthd@live.de>
2021-10-17 00:53:46 +0200

    Add gson util docs

--- src/main/java/info/u_team/u_team_core/util/GsonUtil.java
+++ src/main/java/info/u_team/u_team_core/util/GsonUtil.java
@@ -7,8 +7,21 @@ import com.google.gson.Gson;
 import com.google.gson.internal.Streams;
 import com.google.gson.stream.JsonWriter;
 
+/**
+ * Utility class for gson
+ * 
+ * @author HyCraftHD
+ */
 public class GsonUtil {
 	
+	/**
+	 * Creates a {@link JsonWriter} with the indent set to one tab
+	 * 
+	 * @param gson Gson instance
+	 * @param writer Write
+	 * @return Configured writer with one tab
+	 * @throws IOException
+	 */
 	public static JsonWriter createTabWriter(Gson gson, Writer writer) throws IOException {
 		final var jsonWriter = gson.newJsonWriter(Streams.writerForAppendable(writer));
 		jsonWriter.setIndent("	"); // Use tab instead of two spaces
