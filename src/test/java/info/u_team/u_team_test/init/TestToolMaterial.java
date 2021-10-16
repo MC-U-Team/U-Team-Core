f0426a3bd4f90796448c44af07ae54afca7e096d
HyCraftHD <hycrafthd@live.de>
2021-10-16 23:49:06 +0200

    Fix test tool material

--- src/test/java/info/u_team/u_team_test/init/TestToolMaterial.java
+++ src/test/java/info/u_team/u_team_test/init/TestToolMaterial.java
@@ -1,10 +1,11 @@
 package info.u_team.u_team_test.init;
 
-import info.u_team.u_team_core.api.IToolMaterial;
-import info.u_team.u_team_core.item.tool.UToolMaterial;
+import info.u_team.u_team_core.api.item.ExtendedTier;
+import info.u_team.u_team_core.item.tier.UExtendedTier;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.crafting.Ingredient;
 
 public class TestToolMaterial {
 	
-	public static final IToolMaterial BASIC = new UToolMaterial(new float[] { 8, 0, 4, 2, 6 }, new float[] { -3.1F, -1F, -2F, -2F, 0F }, 2, 500, 5F, 8, 30, () -> Ingredient.of(TestItems.BASIC.get()));
+	public static final ExtendedTier BASIC = new UExtendedTier(new float[] { 8, 0, 4, 2, 6 }, new float[] { -3.1F, -1F, -2F, -2F, 0F }, BlockTags.NEEDS_DIAMOND_TOOL, 500, 5F, 8, 30, () -> Ingredient.of(TestItems.BASIC.get()));
 }
