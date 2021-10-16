549b38bb8cdf27176e4ff6410deedadd2a27f36d
HyCraftHD <hycrafthd@live.de>
2021-10-17 00:04:09 +0200

    Rename to creative mode tab

--- src/main/java/info/u_team/u_team_core/creativetab/UCreativeModeTab.java
+++ src/main/java/info/u_team/u_team_core/creativetab/UCreativeModeTab.java
@@ -1,4 +1,4 @@
-package info.u_team.u_team_core.itemgroup;
+package info.u_team.u_team_core.creativetab;
 
 import java.util.function.Supplier;
 
@@ -6,23 +6,20 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class UItemGroup extends CreativeModeTab {
+public class UCreativeModeTab extends CreativeModeTab {
 	
 	private final Supplier<? extends ItemLike> provider;
 	
-	public UItemGroup(ResourceLocation location, Supplier<? extends ItemLike> provider) {
+	public UCreativeModeTab(ResourceLocation location, Supplier<? extends ItemLike> provider) {
 		this(location.getNamespace(), location.getPath(), provider);
 	}
 	
-	public UItemGroup(String modid, String name, Supplier<? extends ItemLike> provider) {
+	public UCreativeModeTab(String modid, String name, Supplier<? extends ItemLike> provider) {
 		super(modid + "." + name);
 		this.provider = provider;
 	}
 	
-	@OnlyIn(Dist.CLIENT)
 	@Override
 	public ItemStack makeIcon() {
 		return new ItemStack(provider.get());
--- src/test/java/info/u_team/u_team_test/init/TestItemGroups.java
+++ src/test/java/info/u_team/u_team_test/init/TestItemGroups.java
@@ -1,11 +1,11 @@
 package info.u_team.u_team_test.init;
 
-import info.u_team.u_team_core.itemgroup.UItemGroup;
+import info.u_team.u_team_core.creativetab.UCreativeModeTab;
 import info.u_team.u_team_test.TestMod;
 import net.minecraft.world.item.CreativeModeTab;
 
 public class TestItemGroups {
 	
-	public static final CreativeModeTab GROUP = new UItemGroup(TestMod.MODID, "group", TestBlocks.BASIC);
+	public static final CreativeModeTab GROUP = new UCreativeModeTab(TestMod.MODID, "group", TestBlocks.BASIC);
 	
 }
