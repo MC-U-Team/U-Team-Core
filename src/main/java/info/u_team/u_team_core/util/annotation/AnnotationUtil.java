e20e794ba9f614da98cdf3443365bde87823ac0d
HyCraftHD <hycrafthd@live.de>
2021-10-17 15:46:43 +0200

    Document annotation util

--- src/main/java/info/u_team/u_team_core/util/annotation/AnnotationUtil.java
+++ src/main/java/info/u_team/u_team_core/util/annotation/AnnotationUtil.java
@@ -8,8 +8,20 @@ import org.objectweb.asm.Type;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.forgespi.language.ModFileScanData.AnnotationData;
 
+/**
+ * Annotation utility methods
+ * 
+ * @author HyCraftHD
+ */
 public class AnnotationUtil {
 	
+	/**
+	 * Returns a list of found {@link AnnotationData} for the give annotation {@link Type} and modid.
+	 * 
+	 * @param modid The mod files to search for annotations
+	 * @param type Annotation type
+	 * @return List of found annotation data
+	 */
 	public static List<AnnotationData> getAnnotations(String modid, Type type) {
 		return ModList.get() //
 				.getModFileById(modid) //
