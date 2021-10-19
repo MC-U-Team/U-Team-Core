4e7f7c4e8d93da6d3e93db1f783c64aacd82bb56
HyCraftHD <hycrafthd@live.de>
2021-10-19 22:54:20 +0200

    Rename some registry names

--- src/test/java/info/u_team/u_team_test/init/TestItems.java
+++ src/test/java/info/u_team/u_team_test/init/TestItems.java
@@ -20,14 +20,12 @@ public class TestItems {
 	
 	public static final CommonDeferredRegister<Item> ITEMS = CommonDeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MODID);
 	
-	public static final RegistryObject<BasicItem> BASIC = ITEMS.register("basicitem", BasicItem::new);
-	
+	public static final RegistryObject<BasicItem> BASIC = ITEMS.register("basic_item", BasicItem::new);
+	public static final RegistryObject<BasicFoodItem> BASIC_FOOD = ITEMS.register("basic_food", BasicFoodItem::new);
 	public static final RegistryObject<BetterEnderPearlItem> BETTER_ENDERPEARL = ITEMS.register("better_enderpearl", BetterEnderPearlItem::new);
 	
-	public static final RegistryObject<BasicFoodItem> BASIC_FOOD = ITEMS.register("basicfood", BasicFoodItem::new);
-	
-	public static final TierSet BASIC_TOOL = TierSetCreator.create(ITEMS, "basictool", TestCreativeTabs.TAB, new Properties(), TestToolMaterial.BASIC);
-	public static final ArmorSet BASIC_ARMOR = ArmorSetCreator.create(ITEMS, "basicarmor", TestCreativeTabs.TAB, new Properties(), TestArmorMaterial.BASIC);
+	public static final TierSet BASIC_TOOL = TierSetCreator.create(ITEMS, "basic_tool", TestCreativeTabs.TAB, new Properties(), TestToolMaterial.BASIC);
+	public static final ArmorSet BASIC_ARMOR = ArmorSetCreator.create(ITEMS, "basic_armor", TestCreativeTabs.TAB, new Properties(), TestArmorMaterial.BASIC);
 	
 	public static final RegistryObject<USpawnEggItem> TEST_LIVING_SPAWN_EGG = ITEMS.register("test_living_spawn_egg", () -> new USpawnEggItem(TestCreativeTabs.TAB, new Properties(), TestEntityTypes.TEST_LIVING, 0xFF0000, 0x00FF00));
 	
