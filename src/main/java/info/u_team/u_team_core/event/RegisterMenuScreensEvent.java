7da0ca9f8a748814faf7452c2c3a074b01c9897c
HyCraftHD <hycrafthd@live.de>
2021-10-19 22:07:15 +0200

    Use event

--- src/main/java/info/u_team/u_team_core/event/RegisterMenuScreensEvent.java
+++ src/main/java/info/u_team/u_team_core/event/RegisterMenuScreensEvent.java
@@ -1,5 +1,7 @@
 package info.u_team.u_team_core.event;
 
+import java.util.function.Supplier;
+
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.gui.screens.MenuScreens.ScreenConstructor;
 import net.minecraft.client.gui.screens.Screen;
@@ -10,6 +12,16 @@ import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.fml.event.IModBusEvent;
 
 public class RegisterMenuScreensEvent extends Event implements IModBusEvent {
+	
+	/**
+	 * Registers a menu screen.
+	 * 
+	 * @param menuTypeSupplier The menu type as a supplier to register a screen for
+	 * @param screenConstructor The constructor of the screen
+	 */
+	public <M extends AbstractContainerMenu, U extends Screen & MenuAccess<M>> void registerScreen(Supplier<? extends MenuType<? extends M>> menuTypeSupplier, ScreenConstructor<M, U> screenConstructor) {
+		registerScreen(menuTypeSupplier.get(), screenConstructor);
+	}
 	
 	/**
 	 * Registers a menu screen.
--- src/test/java/info/u_team/u_team_test/init/TestScreens.java
+++ src/test/java/info/u_team/u_team_test/init/TestScreens.java
@@ -1,23 +1,20 @@
 package info.u_team.u_team_test.init;
 
-import info.u_team.u_team_core.util.registry.ClientRegistry;
+import info.u_team.u_team_core.event.RegisterMenuScreensEvent;
 import info.u_team.u_team_test.screen.BasicEnergyCreatorScreen;
 import info.u_team.u_team_test.screen.BasicFluidInventoryScreen;
 import info.u_team.u_team_test.screen.BasicTileEntityScreen;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class TestScreens {
 	
-	private static void setup(FMLClientSetupEvent event) {
-		event.enqueueWork(() -> {
-			ClientRegistry.registerScreen(TestContainers.BASIC, BasicTileEntityScreen::new);
-			ClientRegistry.registerScreen(TestContainers.BASIC_ENERGY_CREATOR, BasicEnergyCreatorScreen::new);
-			ClientRegistry.registerScreen(TestContainers.BASIC_FLUID_INVENTORY, BasicFluidInventoryScreen::new);
-		});
+	private static void register(RegisterMenuScreensEvent event) {
+		event.registerScreen(TestContainers.BASIC, BasicTileEntityScreen::new);
+		event.registerScreen(TestContainers.BASIC_ENERGY_CREATOR, BasicEnergyCreatorScreen::new);
+		event.registerScreen(TestContainers.BASIC_FLUID_INVENTORY, BasicFluidInventoryScreen::new);
 	}
 	
 	public static void registerMod(IEventBus bus) {
-		bus.addListener(TestScreens::setup);
+		bus.addListener(TestScreens::register);
 	}
 }
