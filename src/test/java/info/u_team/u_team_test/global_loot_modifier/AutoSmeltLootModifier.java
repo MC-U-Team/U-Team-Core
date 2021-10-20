632b4db49a0b3066d462b344f7ee4ec2db3affa3
HyCraftHD <hycrafthd@live.de>
2021-10-20 19:29:31 +0200

    Update autosmelt to the right methods

--- src/test/java/info/u_team/u_team_test/global_loot_modifier/AutoSmeltLootModifier.java
+++ src/test/java/info/u_team/u_team_test/global_loot_modifier/AutoSmeltLootModifier.java
@@ -9,12 +9,10 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeType;
-import net.minecraft.world.item.crafting.SmeltingRecipe;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.common.loot.LootModifier;
-import net.minecraftforge.items.ItemHandlerHelper;
 
 public class AutoSmeltLootModifier extends LootModifier {
 	
@@ -28,11 +26,17 @@ public class AutoSmeltLootModifier extends LootModifier {
 	}
 	
 	private static ItemStack smeltItem(ItemStack stack, LootContext context) {
+		final var container = new SimpleContainer(stack);
+		
 		return context.getLevel() //
 				.getRecipeManager() //
-				.getRecipeFor(RecipeType.SMELTING, new SimpleContainer(stack), context.getLevel()) //
-				.map(SmeltingRecipe::getResultItem).filter(itemStack -> !itemStack.isEmpty()) //
-				.map(itemStack -> ItemHandlerHelper.copyStackWithSize(itemStack, stack.getCount() * itemStack.getCount())) //
+				.getRecipeFor(RecipeType.SMELTING, container, context.getLevel()) //
+				.map(recipe -> recipe.assemble(container)) //
+				.filter(itemStack -> !itemStack.isEmpty()) //
+				.map(itemStack -> {
+					itemStack.setCount(stack.getCount() * itemStack.getCount());
+					return itemStack;
+				}) //
 				.orElse(stack);
 	}
 	
