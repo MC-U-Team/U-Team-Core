fb783f0d883bbf93f9b5af29bfbecdb60d62cea2
HyCraftHD <hycrafthd@live.de>
2021-10-26 01:39:20 +0200

    Remove empty line

--- src/main/java/info/u_team/u_team_core/intern/init/UCoreRecipeSerializers.java
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreRecipeSerializers.java
@@ -19,7 +19,6 @@ public class UCoreRecipeSerializers {
 	public static final CommonDeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS = CommonDeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, UCoreMod.MODID);
 	
 	public static final RegistryObject<SimpleRecipeSerializer<DyeableItemDyeRecipe>> CRAFTING_SPECIAL_ITEMDYE = RECIPE_SERIALIZERS.register("crafting_special_itemdye", () -> new SimpleRecipeSerializer<>(DyeableItemDyeRecipe::new));
-	
 	public static final RegistryObject<NoMirrorShapedRecipe.Serializer> NO_MIRROR_SHAPED = RECIPE_SERIALIZERS.register("crafting_shaped_no_mirror", NoMirrorShapedRecipe.Serializer::new);
 	
 	private static void registerIngredient(Register<RecipeSerializer<?>> event) {
