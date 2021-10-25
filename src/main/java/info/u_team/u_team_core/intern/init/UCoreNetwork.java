b1946814aaf37b1ed3f307d94a558fe1848340e0
HyCraftHD <hycrafthd@live.de>
2021-10-26 01:03:19 +0200

    Specify packet direction

--- src/main/java/info/u_team/u_team_core/intern/init/UCoreNetwork.java
+++ src/main/java/info/u_team/u_team_core/intern/init/UCoreNetwork.java
@@ -1,13 +1,16 @@
 package info.u_team.u_team_core.intern.init;
 
+import java.util.Optional;
+
 import info.u_team.u_team_core.UCoreMod;
-import info.u_team.u_team_core.intern.network.DataHolderMenuMessage;
-import info.u_team.u_team_core.intern.network.FluidClickContainerMessage;
 import info.u_team.u_team_core.intern.network.ContainerSetFluidContentMessage;
 import info.u_team.u_team_core.intern.network.ContainerSetFluidSlotMessage;
+import info.u_team.u_team_core.intern.network.DataHolderMenuMessage;
+import info.u_team.u_team_core.intern.network.FluidClickContainerMessage;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fmllegacy.network.NetworkDirection;
 import net.minecraftforge.fmllegacy.network.NetworkRegistry;
 import net.minecraftforge.fmllegacy.network.simple.SimpleChannel;
 
@@ -24,10 +27,10 @@ public class UCoreNetwork {
 	}, PROTOCOL::equals);
 	
 	public static void setup(FMLCommonSetupEvent event) {
-		NETWORK.registerMessage(0, DataHolderMenuMessage.class, DataHolderMenuMessage::encode, DataHolderMenuMessage::decode, DataHolderMenuMessage.Handler::handle);
-		NETWORK.registerMessage(1, ContainerSetFluidContentMessage.class, ContainerSetFluidContentMessage::encode, ContainerSetFluidContentMessage::decode, ContainerSetFluidContentMessage.Handler::handle);
-		NETWORK.registerMessage(2, ContainerSetFluidSlotMessage.class, ContainerSetFluidSlotMessage::encode, ContainerSetFluidSlotMessage::decode, ContainerSetFluidSlotMessage.Handler::handle);
-		NETWORK.registerMessage(3, FluidClickContainerMessage.class, FluidClickContainerMessage::encode, FluidClickContainerMessage::decode, FluidClickContainerMessage.Handler::handle);
+		NETWORK.registerMessage(0x0, DataHolderMenuMessage.class, DataHolderMenuMessage::encode, DataHolderMenuMessage::decode, DataHolderMenuMessage.Handler::handle);
+		NETWORK.registerMessage(0x1, ContainerSetFluidContentMessage.class, ContainerSetFluidContentMessage::encode, ContainerSetFluidContentMessage::decode, ContainerSetFluidContentMessage.Handler::handle, Optional.of(NetworkDirection.PLAY_TO_CLIENT));
+		NETWORK.registerMessage(0x2, ContainerSetFluidSlotMessage.class, ContainerSetFluidSlotMessage::encode, ContainerSetFluidSlotMessage::decode, ContainerSetFluidSlotMessage.Handler::handle, Optional.of(NetworkDirection.PLAY_TO_CLIENT));
+		NETWORK.registerMessage(0x3, FluidClickContainerMessage.class, FluidClickContainerMessage::encode, FluidClickContainerMessage::decode, FluidClickContainerMessage.Handler::handle, Optional.of(NetworkDirection.PLAY_TO_SERVER));
 	}
 	
 	public static void registerMod(IEventBus bus) {
