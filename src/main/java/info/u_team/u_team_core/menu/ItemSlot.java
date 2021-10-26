f0770455c3ac0f4d62d5b9d248a90bab0fab936d
HyCraftHD <hycrafthd@live.de>
2021-10-26 02:04:43 +0200

    Change back to normal item handler, cuz dumb :/

--- src/main/java/info/u_team/u_team_core/menu/ItemSlot.java
+++ src/main/java/info/u_team/u_team_core/menu/ItemSlot.java
@@ -1,15 +1,15 @@
 package info.u_team.u_team_core.menu;
 
 import info.u_team.u_team_core.inventory.UItemStackHandler;
-import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.SlotItemHandler;
 
 public class ItemSlot extends SlotItemHandler {
 	
-	private final IItemHandlerModifiable itemHandler;
+	private final IItemHandler itemHandler;
 	private final int slot;
 	
-	public ItemSlot(IItemHandlerModifiable itemHandler, int slot, int x, int y) {
+	public ItemSlot(IItemHandler itemHandler, int slot, int x, int y) {
 		super(itemHandler, slot, x, y);
 		this.itemHandler = itemHandler;
 		this.slot = slot;
