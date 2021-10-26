84500d21bfa77d902edaa9d0e84f2af4e93be474
HyCraftHD <hycrafthd@live.de>
2021-10-26 02:16:19 +0200

    Set fluid slots in test mod

--- src/test/java/info/u_team/u_team_test/menu/BasicFluidInventoryMenu.java
+++ src/test/java/info/u_team/u_team_test/menu/BasicFluidInventoryMenu.java
@@ -5,6 +5,7 @@ import info.u_team.u_team_test.blockentity.BasicFluidInventoryBlockEntity;
 import info.u_team.u_team_test.init.TestMenuTypes;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraftforge.fml.LogicalSide;
 
 public class BasicFluidInventoryMenu extends UBlockEntityContainerMenu<BasicFluidInventoryBlockEntity> {
@@ -24,6 +25,9 @@ public class BasicFluidInventoryMenu extends UBlockEntityContainerMenu<BasicFlui
 		addFluidSlots(blockEntity.getFluidTanks(), 1, 4, 8, 18);
 		addSlots(blockEntity.getItemSlots(), 1, 4, 8, 50);
 		addPlayerInventory(playerInventory, 8, 82);
+		
+		fluidSlots.get(0).setBackground(InventoryMenu.BLOCK_ATLAS, InventoryMenu.EMPTY_ARMOR_SLOT_SHIELD);
+		slots.get(0).setBackground(InventoryMenu.BLOCK_ATLAS, InventoryMenu.EMPTY_ARMOR_SLOT_SHIELD);
 	}
 	
 }
