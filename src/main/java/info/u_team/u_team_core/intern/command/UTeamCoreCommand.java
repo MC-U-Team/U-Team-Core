3abec154af61a5b439bd250f12c6e18b2e6f8ef2
HyCraftHD <hycrafthd@live.de>
2022-03-12 15:50:01 +0100

    Remove locate commands as vanilla commands now use the registry name! :)

--- src/main/java/info/u_team/u_team_core/intern/command/UTeamCoreCommand.java
+++ src/main/java/info/u_team/u_team_core/intern/command/UTeamCoreCommand.java
@@ -4,8 +4,6 @@ import com.mojang.brigadier.CommandDispatcher;
 
 import info.u_team.u_team_core.intern.command.uteamcore.DimensionTeleportSubCommand;
 import info.u_team.u_team_core.intern.command.uteamcore.ItemStackInfoSubCommand;
-import info.u_team.u_team_core.intern.command.uteamcore.LocateBiomeSubCommand;
-import info.u_team.u_team_core.intern.command.uteamcore.LocateStructureSubCommand;
 import info.u_team.u_team_core.intern.command.uteamcore.PingSubCommand;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
@@ -16,10 +14,7 @@ public class UTeamCoreCommand {
 		dispatcher.register(Commands.literal("uteamcore") //
 				.then(ItemStackInfoSubCommand.register()) //
 				.then(DimensionTeleportSubCommand.register()) //
-				.then(dispatcher.register(PingSubCommand.register())) //
-				);
-//				.then(LocateBiomeSubCommand.register()) //
-//				.then(LocateStructureSubCommand.register()));
+				.then(dispatcher.register(PingSubCommand.register())));
 	}
 	
 }
