ef355e4101f50d98f94c252df19248a803ec36c0
HyCraftHD <hycrafthd@live.de>
2022-06-08 00:18:01 +0200

    Fix UArmor

--- src/main/java/info/u_team/u_team_core/item/armor/UArmorItem.java
+++ src/main/java/info/u_team/u_team_core/item/armor/UArmorItem.java
@@ -6,6 +6,7 @@ import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class UArmorItem extends ArmorItem {
 	
@@ -25,7 +26,7 @@ public class UArmorItem extends ArmorItem {
 		if (!material.getName().equals("invalid")) {
 			return null;
 		}
-		return String.format("%s:textures/models/armor/%s_layer_%d%s.png", getRegistryName().getNamespace(), textureName, (slot == EquipmentSlot.LEGS ? 2 : 1), type == null ? "" : String.format("_%s", type));
+		return String.format("%s:textures/models/armor/%s_layer_%d%s.png", ForgeRegistries.ITEMS.getKey(this).getNamespace(), textureName, (slot == EquipmentSlot.LEGS ? 2 : 1), type == null ? "" : String.format("_%s", type));
 	}
 	
 	protected String getTypeString(EquipmentSlot slot) {
