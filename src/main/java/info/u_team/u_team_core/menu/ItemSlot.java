8c9a92b267b1427b7e642a5ecfbc5432745088b3
HyCraftHD <hycrafthd@live.de>
2022-06-08 01:50:55 +0200

    Fix ArrayIndexOutOfBoundsException

--- src/main/java/info/u_team/u_team_core/menu/ItemSlot.java
+++ src/main/java/info/u_team/u_team_core/menu/ItemSlot.java
@@ -1,6 +1,7 @@
 package info.u_team.u_team_core.menu;
 
 import info.u_team.u_team_core.inventory.UItemStackHandler;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.SlotItemHandler;
 
@@ -20,6 +21,11 @@ public class ItemSlot extends SlotItemHandler {
 		if (itemHandler instanceof final UItemStackHandler uItemHandler) {
 			uItemHandler.onContentsChanged(slot);
 		}
+	}
+	
+	@Override
+	public void initialize(ItemStack stack) {
+		set(stack); // TODO is this right to prevent java.lang.ArrayIndexOutOfBoundsException: Index 0 out of bounds for length 0
 	}
 	
 }
