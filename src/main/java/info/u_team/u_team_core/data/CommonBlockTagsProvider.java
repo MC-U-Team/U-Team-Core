69c45a26023a05aa7df974b7b399077a313c8d44
HyCraftHD <hycrafthd@live.de>
2022-07-02 01:04:07 +0200

    Add fluid tags

--- src/main/java/info/u_team/u_team_core/data/CommonBlockTagsProvider.java
+++ src/main/java/info/u_team/u_team_core/data/CommonBlockTagsProvider.java
@@ -1,8 +1,6 @@
 package info.u_team.u_team_core.data;
 
 import net.minecraft.core.Registry;
-import net.minecraft.tags.TagBuilder;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 
 public abstract class CommonBlockTagsProvider extends CommonTagsProvider<Block> {
@@ -10,11 +8,6 @@ public abstract class CommonBlockTagsProvider extends CommonTagsProvider<Block> 
 	@SuppressWarnings("deprecation")
 	public CommonBlockTagsProvider(GenerationData generationData) {
 		super(generationData, Registry.BLOCK);
-	}
-	
-	@Override
-	protected TagBuilder getOrCreateRawBuilder(TagKey<Block> tag) {
-		return super.getOrCreateRawBuilder(tag);
 	}
 	
 }
---
+++ src/main/java/info/u_team/u_team_core/data/CommonFluidTagsProvider.java
@@ -0,0 +1,13 @@
+package info.u_team.u_team_core.data;
+
+import net.minecraft.core.Registry;
+import net.minecraft.world.level.material.Fluid;
+
+public abstract class CommonFluidTagsProvider extends CommonTagsProvider<Fluid> {
+	
+	@SuppressWarnings("deprecation")
+	public CommonFluidTagsProvider(GenerationData generationData) {
+		super(generationData, Registry.FLUID);
+	}
+	
+}
