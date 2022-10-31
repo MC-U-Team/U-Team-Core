22c2aa1a0e12fb46d9f7d98c78ae0d4f233605c4
HyCraftHD <hycrafthd@live.de>
2022-10-31 21:56:51 +0100

    Use event in test mod for spawn placement registry

--- src/test_mod/java/info/u_team/u_team_test/init/TestSpawnPlacementRegistries.java
+++ src/test_mod/java/info/u_team/u_team_test/init/TestSpawnPlacementRegistries.java
@@ -3,19 +3,18 @@ package info.u_team.u_team_test.init;
 import info.u_team.u_team_test.entity.TestLiving;
 import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent.Operation;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 public class TestSpawnPlacementRegistries {
 	
-	private static void setup(FMLCommonSetupEvent event) {
-		event.enqueueWork(() -> {
-			SpawnPlacements.register(TestEntityTypes.TEST_LIVING.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, TestLiving::checkTestLivingSpawnRules);
-		});
+	private static void register(SpawnPlacementRegisterEvent event) {
+		event.register(TestEntityTypes.TEST_LIVING.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, TestLiving::checkTestLivingSpawnRules, Operation.OR);
 	}
 	
 	public static void registerMod(IEventBus bus) {
-		bus.addListener(TestSpawnPlacementRegistries::setup);
+		bus.addListener(TestSpawnPlacementRegistries::register);
 	}
 	
 }
