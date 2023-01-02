9d22711295777847d224a8cf87d8ddb30ec7c29a
HyCraftHD <hycrafthd@live.de>
2023-01-02 19:29:41 +0100

    Add param to no shaped recipe

--- src/main/java/info/u_team/u_team_core/data/builder/NoMirrorShapedRecipeBuilder.java
+++ src/main/java/info/u_team/u_team_core/data/builder/NoMirrorShapedRecipeBuilder.java
@@ -5,22 +5,23 @@ import java.util.function.Consumer;
 import info.u_team.u_team_core.intern.init.UCoreRecipeSerializers;
 import info.u_team.u_team_core.util.RecipeBuilderUtil;
 import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.ItemLike;
 
 public class NoMirrorShapedRecipeBuilder extends ShapedRecipeBuilder {
 	
-	public NoMirrorShapedRecipeBuilder(ItemLike item, int count) {
-		super(item, count);
+	public NoMirrorShapedRecipeBuilder(RecipeCategory cateogry, ItemLike item, int count) {
+		super(cateogry, item, count);
 	}
 	
-	public static NoMirrorShapedRecipeBuilder noMirrorShapedRecipe(ItemLike item) {
-		return noMirrorShapedRecipe(item, 1);
+	public static NoMirrorShapedRecipeBuilder noMirrorShapedRecipe(RecipeCategory cateogry, ItemLike item) {
+		return noMirrorShapedRecipe(cateogry, item, 1);
 	}
 	
-	public static NoMirrorShapedRecipeBuilder noMirrorShapedRecipe(ItemLike item, int count) {
-		return new NoMirrorShapedRecipeBuilder(item, count);
+	public static NoMirrorShapedRecipeBuilder noMirrorShapedRecipe(RecipeCategory cateogry, ItemLike item, int count) {
+		return new NoMirrorShapedRecipeBuilder(cateogry, item, count);
 	}
 	
 	@Override
