6a99b64e900098db04553cb755d1ceea495532ca
HyCraftHD <hycrafthd@live.de>
2023-01-03 14:51:30 +0100

    Fix common json codecs provider for now

--- src/main/java/info/u_team/u_team_core/data/CommonJsonCodecsProvider.java
+++ src/main/java/info/u_team/u_team_core/data/CommonJsonCodecsProvider.java
@@ -1,16 +1,11 @@
 package info.u_team.u_team_core.data;
 
-import java.io.IOException;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 
-import com.google.gson.JsonElement;
-
-import net.minecraft.core.Registry;
 import net.minecraft.data.CachedOutput;
-import net.minecraft.resources.RegistryOps;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.data.JsonCodecProvider;
 
 public abstract class CommonJsonCodecsProvider implements CommonDataProvider<Consumer<JsonCodecProvider<?>>> {
@@ -27,14 +22,12 @@ public abstract class CommonJsonCodecsProvider implements CommonDataProvider<Con
 	}
 	
 	@Override
-	public void run(CachedOutput cachedOutput) throws IOException {
+	public CompletableFuture<?> run(CachedOutput cachedOutput) {
+		final List<CompletableFuture<?>> futures = new ArrayList<>();
 		register(provider -> {
-			try {
-				provider.run(cachedOutput);
-			} catch (final IOException ex) {
-				throw new RuntimeException(ex);
-			}
+			futures.add(provider.run(cachedOutput));
 		});
+		return CompletableFuture.allOf(futures.toArray(CompletableFuture[]::new));
 	}
 	
 	@Override
@@ -42,7 +35,10 @@ public abstract class CommonJsonCodecsProvider implements CommonDataProvider<Con
 		return "Json-Codecs-Provider";
 	}
 	
-	protected <T> JsonCodecProvider<T> dataPackRegistry(RegistryOps<JsonElement> registryOps, ResourceKey<Registry<T>> registryKey, Map<ResourceLocation, T> entries) {
-		return JsonCodecProvider.forDatapackRegistry(generationData.generator(), generationData.existingFileHelper(), modid(), registryOps, registryKey, entries);
-	}
+	// TODO use datapack provider??
+	// protected <T> JsonCodecProvider<T> dataPackRegistry(RegistryOps<JsonElement> registryOps, ResourceKey<Registry<T>>
+	// registryKey, Map<ResourceLocation, T> entries) {
+	// return JsonCodecProvider.forDatapackRegistry(generationData.generator(), generationData.existingFileHelper(),
+	// modid(), registryOps, registryKey, entries);
+	// }
 }
