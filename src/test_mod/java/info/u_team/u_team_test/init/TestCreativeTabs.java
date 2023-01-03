6ca0dd02b0dd1bad21c3d2ea18a740c8a018e471
HyCraftHD <hycrafthd@live.de>
2023-01-03 19:40:10 +0100

    Fix creative tabs

--- src/test_mod/java/info/u_team/u_team_test/init/TestCreativeTabs.java
+++ src/test_mod/java/info/u_team/u_team_test/init/TestCreativeTabs.java
@@ -4,7 +4,9 @@ import info.u_team.u_team_test.TestMod;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 
@@ -17,8 +19,14 @@ public class TestCreativeTabs {
 			builder.icon(() -> new ItemStack(TestBlocks.BASIC.get()));
 			builder.title(Component.translatable("creativetabs.uteamtest.tab"));
 			builder.displayItems((enabledFeatures, output, displayOperatorCreativeTab) -> {
-				TestBlocks.BLOCKS.getItemRegister().forEach(item -> {
-					output.accept(item.get());
+				TestBlocks.BLOCKS.getBlockRegister().forEach(block -> {
+					final Item item = block.get().asItem();
+					if (item != null && item != Items.AIR) {
+						final ItemStack stack = new ItemStack(item);
+						if (!stack.isEmpty()) {
+							output.accept(stack);
+						}
+					}
 				});
 				TestItems.ITEMS.forEach(item -> {
 					output.accept(item.get());
