9e5791f96268e4b72b42b97d5f3e6d83d19a76a5
HyCraftHD <hycrafthd@live.de>
2023-02-23 18:51:57 +0100

    JarSignVerifier now uses secure jars way of verifing the jars. Should be faster and should be more portable

--- src/main/java/info/u_team/u_team_core/util/verify/JarSignVerifier.java
+++ src/main/java/info/u_team/u_team_core/util/verify/JarSignVerifier.java
@@ -1,27 +1,28 @@
 package info.u_team.u_team_core.util.verify;
 
-import java.io.InputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.security.CodeSigner;
+import java.security.cert.Certificate;
+import java.util.Locale;
 import java.util.Optional;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 
 import com.google.common.base.Stopwatch;
-import com.google.common.io.ByteStreams;
 import com.mojang.logging.LogUtils;
 
+import cpw.mods.jarhandling.SecureJar;
+import cpw.mods.jarhandling.SecureJar.ModuleDataProvider;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fml.util.CertificateHelper;
 import net.minecraftforge.forgespi.language.IModFileInfo;
 
-// TODO evaluate if this is still useful. Forge has now a system for checking the fingerprint too, but makes this
-// information not accessible
 public class JarSignVerifier {
 	
 	private static final Logger LOGGER = LogUtils.getLogger();
@@ -46,76 +47,62 @@ public class JarSignVerifier {
 	public static VerifyStatus verify(String modid) {
 		final IModFileInfo info = ModList.get().getModFileById(modid);
 		
-		// TODO replace with this as this is forge native
-		// if (info instanceof ModFileInfo concreteInfo) {
-		// LOGGER.info("The code signing fingerprint for {} reported by forge is {}", modid,
-		// concreteInfo.getCodeSigningFingerprint().orElse("NULL"));
-		//
-		// var signers = info.getFile().getSecureJar().getManifestSigners();
-		// if (signers != null) {
-		// Arrays.stream(signers).forEach(csa -> {
-		// System.out.println(CertificateHelper.getFingerprints(csa.getSignerCertPath().getCertificates().toArray(Certificate[]::new)));
-		// });
-		// }
-		//
-		// // if (FMLEnvironment.secureJarsEnabled) {
-		// // LOGGER.info("The trust data reported by forge is {}", concreteInfo.getTrustData().orElse("NULL"));
-		// // }
-		// }
-		
 		// We don't need to check sign in dev environment
 		if (!FMLEnvironment.production) {
 			return VerifyStatus.DEV;
 		}
 		
-		final Path path = info.getFile().getFilePath();
+		final SecureJar jar = info.getFile().getSecureJar();
+		final ModuleDataProvider moduleData = jar.moduleDataProvider();
 		
-		if (Files.isDirectory(path)) {
-			return VerifyStatus.DEV;
+		final Optional<String> fingerPrintOptional = getFingerPrint(Optional.ofNullable(moduleData.getManifest()));
+		if (fingerPrintOptional.isEmpty()) {
+			return VerifyStatus.UNSIGNED;
 		}
+		final String fingerprint = fingerPrintOptional.get();
 		
-		try (final JarFile jarFile = new JarFile(path.toFile())) {
-			
-			// Get fingerprint from manifest
-			final Optional<String> fingerPrintOptional = getFingerPrint(Optional.ofNullable(jarFile.getManifest()));
-			
-			if (!fingerPrintOptional.isPresent()) {
+		try (final Stream<Path> paths = Files.find(jar.getRootPath(), Integer.MAX_VALUE, JarSignVerifier::validPath)) {
+			if ((jar.getManifestSigners() == null) || paths.parallel().map(path -> pathSigned(moduleData, path, fingerprint)).anyMatch(signed -> !signed)) {
 				return VerifyStatus.UNSIGNED;
 			}
 			
-			final String fingerprint = fingerPrintOptional.get();
-			
-			try (final Stream<JarEntry> entryStream = jarFile.stream()) {
-				// Check sign on every resource excluding directories and the certificate files
-				if (entryStream.filter(JarSignVerifier::checkEntryForSign).allMatch(entry -> {
-					// Read everything so the certificate gets loaded but trash the input
-					try (final InputStream stream = jarFile.getInputStream(entry)) {
-						ByteStreams.copy(stream, ByteStreams.nullOutputStream());
-					} catch (final Exception ex) {
-						return false;
-					}
-					// Check if finger print is valid
-					return CertificateHelper.getFingerprints(entry.getCertificates()).stream().filter(fingerprint::equals).findAny().isPresent();
-				})) {
-					return VerifyStatus.SIGNED;
-				}
-			}
-			return VerifyStatus.UNSIGNED;
+			return VerifyStatus.SIGNED;
 		} catch (final Exception ex) {
 			return VerifyStatus.UNSIGNED;
 		}
 	}
 	
-	private static boolean checkEntryForSign(JarEntry entry) {
-		if (entry.isDirectory()) {
+	private static boolean validPath(Path path, BasicFileAttributes attributes) {
+		if (path.getNameCount() < 0 || !attributes.isRegularFile()) {
 			return false;
 		}
-		final String name = entry.getName().toUpperCase();
+		final String name = path.getFileName().toString().toUpperCase(Locale.ROOT);
 		return !name.endsWith(".SF") && !name.endsWith(".DSA") && !name.endsWith(".EC") && !name.endsWith(".RSA");
 	}
 	
 	private static Optional<String> getFingerPrint(Optional<Manifest> manifest) {
-		return manifest.map(Manifest::getMainAttributes).map(attributes -> attributes.getValue("Fingerprint")).map(fingerPrint -> fingerPrint.replace(":", "").toLowerCase());
+		return manifest.map(Manifest::getMainAttributes).map(attributes -> attributes.getValue("Fingerprint")).map(fingerPrint -> fingerPrint.replace(":", "").toLowerCase(Locale.ROOT));
+	}
+	
+	private static boolean pathSigned(ModuleDataProvider moduleData, Path path, String fingerprint) {
+		LOGGER.trace("Check {} for valid signature.", path);
+		
+		final byte[] bytes;
+		try {
+			bytes = Files.readAllBytes(path);
+		} catch (final IOException ex) {
+			return false;
+		}
+		
+		final CodeSigner[] codeSigners = moduleData.verifyAndGetSigners(path.toString(), bytes);
+		
+		return CertificateHelper.getFingerprints(Stream.of(codeSigners) //
+				.flatMap(signers -> signers.getSignerCertPath().getCertificates().stream()) //
+				.toArray(Certificate[]::new)) //
+				.stream() //
+				.filter(fingerprint::equals) //
+				.findAny() //
+				.isPresent();
 	}
 	
 	public static enum VerifyStatus {
