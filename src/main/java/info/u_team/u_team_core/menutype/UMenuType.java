d4b3fe4887d7345387d4c1313f9ae0968f933168
HyCraftHD <hycrafthd@live.de>
2023-03-15 18:34:52 +0100

    Port umenu type

--- src/main/java/info/u_team/u_team_core/menutype/UMenuType.java
+++ src/main/java/info/u_team/u_team_core/menutype/UMenuType.java
@@ -1,5 +1,6 @@
 package info.u_team.u_team_core.menutype;
 
+import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraftforge.network.IContainerFactory;
@@ -7,11 +8,11 @@ import net.minecraftforge.network.IContainerFactory;
 public class UMenuType<T extends AbstractContainerMenu> extends MenuType<T> {
 	
 	public UMenuType(ForgeMenuSupplier<T> forgeFactory) {
-		super(forgeFactory);
+		super(forgeFactory, FeatureFlags.DEFAULT_FLAGS);
 	}
 	
 	public UMenuType(MenuSupplier<T> factory) {
-		super(factory);
+		super(factory, FeatureFlags.DEFAULT_FLAGS);
 	}
 	
 	@FunctionalInterface
