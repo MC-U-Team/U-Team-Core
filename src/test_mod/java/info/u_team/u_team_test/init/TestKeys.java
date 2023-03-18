ff51d430ed69701da2ef5eab88c5f9963e255184
HyCraftHD <hycrafthd@live.de>
2023-03-18 23:12:54 +0100

    Add test screen for some vanilla gui components as reference

--- src/test_mod/java/info/u_team/u_team_test/init/TestKeys.java
+++ src/test_mod/java/info/u_team/u_team_test/init/TestKeys.java
@@ -5,6 +5,7 @@ import org.lwjgl.glfw.GLFW;
 import com.mojang.blaze3d.platform.InputConstants;
 
 import info.u_team.u_team_test.screen.ButtonTestScreen;
+import info.u_team.u_team_test.screen.ButtonTestScreenVanilla;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
@@ -14,14 +15,21 @@ import net.minecraftforge.eventbus.api.IEventBus;
 public class TestKeys {
 	
 	public static final KeyMapping BASIC = new KeyMapping("Basic key", GLFW.GLFW_KEY_F8, "Basic category");
+	public static final KeyMapping BASIC_VANILLA = new KeyMapping("Basic key for Vanilla test", GLFW.GLFW_KEY_F9, "Basic category");
 	
 	private static void register(RegisterKeyMappingsEvent event) {
 		event.register(BASIC);
+		event.register(BASIC_VANILLA);
 	}
 	
 	private static void onKeyboardPressed(KeyPressed.Post event) {
 		if (BASIC.isActiveAndMatches(InputConstants.getKey(event.getKeyCode(), event.getScanCode()))) {
 			Minecraft.getInstance().setScreen(new ButtonTestScreen());
+			event.setCanceled(true);
+		}
+		
+		if (BASIC_VANILLA.isActiveAndMatches(InputConstants.getKey(event.getKeyCode(), event.getScanCode()))) {
+			Minecraft.getInstance().setScreen(new ButtonTestScreenVanilla());
 			event.setCanceled(true);
 		}
 	}
---
+++ src/test_mod/java/info/u_team/u_team_test/screen/ButtonTestScreenVanilla.java
@@ -0,0 +1,55 @@
+package info.u_team.u_team_test.screen;
+
+import org.slf4j.Logger;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.components.ImageButton;
+import net.minecraft.client.gui.components.MultiLineEditBox;
+import net.minecraft.client.gui.components.TextAndImageButton;
+import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+public class ButtonTestScreenVanilla extends Screen {
+	
+	private static final Logger LOGGER = LogUtils.getLogger();
+	
+	public ButtonTestScreenVanilla() {
+		super(Component.literal("test"));
+	}
+	
+	@Override
+	protected void init() {
+		addRenderableWidget(new Button.Builder(Component.literal("Test Button"), button -> {
+			LOGGER.info("Clicked Testbutton");
+		}).bounds(50, 50, 200, 20).tooltip(Tooltip.create(Component.literal("Tooltip for Test Button"))).build());
+		
+		addRenderableWidget(new ImageButton(50, 80, 50, 20, 0, 106, 20, Button.WIDGETS_LOCATION, 256, 256, button -> {
+			LOGGER.info("Clicked Test ImageButton");
+		}, Component.literal("Test ImageButton")));
+		
+		final AbstractWidget textImageButton = addRenderableWidget(TextAndImageButton.builder(Component.literal("Test Button with Image"), Button.WIDGETS_LOCATION, button -> {
+			LOGGER.info("Clicked Testbutton with Image");
+		}).texStart(3, 109).offset(65, 3).yDiffTex(20).usedTextureSize(14, 14).textureSize(256, 256).build());
+		textImageButton.setPosition(50, 110);
+		textImageButton.setTooltip(Tooltip.create(Component.literal("Tooltip for Test Button with Image")));
+		
+		final AbstractWidget editBox = addRenderableWidget(new EditBox(font, 50, 140, 200, 20, Component.literal("Test box")));
+		editBox.setTooltip(Tooltip.create(Component.literal("Test text box")));
+		
+		final AbstractWidget multiEditBox = addRenderableWidget(new MultiLineEditBox(font, 50, 170, 200, 20, Component.empty(), Component.literal("Test box")));
+		multiEditBox.setTooltip(Tooltip.create(Component.literal("Test multi line text box")));
+	}
+	
+	@Override
+	public void render(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
+		renderBackground(poseStack);
+		super.render(poseStack, mouseX, mouseY, partialTick);
+	}
+	
+}
