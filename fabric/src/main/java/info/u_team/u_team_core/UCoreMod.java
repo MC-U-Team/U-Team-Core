7b0d8515e6fe7f8565752244af2a4dba44b0b763
HyCraftHD <hycrafthd@live.de>
2023-05-14 01:16:58 +0200

    Make usage of mod construct

--- fabric/src/main/java/info/u_team/u_team_core/UCoreMod.java
+++ fabric/src/main/java/info/u_team/u_team_core/UCoreMod.java
@@ -2,7 +2,7 @@ package info.u_team.u_team_core;
 
 import org.slf4j.Logger;
 
-import info.u_team.u_team_core.intern.update.UpdateResolver;
+import info.u_team.u_team_core.util.annotation.AnnotationManager;
 import net.fabricmc.api.ModInitializer;
 
 public class UCoreMod implements ModInitializer {
@@ -12,7 +12,7 @@ public class UCoreMod implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
-		UpdateResolver.load();
+		AnnotationManager.callAnnotations(MODID);
 	}
 	
 }
---
+++ fabric/src/main/java/info/u_team/u_team_core/intern/init/UCoreCommonConstruct.java
@@ -0,0 +1,16 @@
+package info.u_team.u_team_core.intern.init;
+
+import info.u_team.u_team_core.UCoreMod;
+import info.u_team.u_team_core.api.construct.Construct;
+import info.u_team.u_team_core.api.construct.ModConstruct;
+import info.u_team.u_team_core.intern.update.UpdateResolver;
+
+@Construct(modid = UCoreMod.MODID)
+public class UCoreCommonConstruct implements ModConstruct {
+	
+	@Override
+	public void construct() {
+		UpdateResolver.load();
+	}
+	
+}
