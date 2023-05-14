48384d1a83ac2a91a08aab226c5aadbde9aebeae
HyCraftHD <hycrafthd@live.de>
2023-05-14 20:35:01 +0200

    Add debug output to service

--- common/src/common/java/info/u_team/u_team_core/util/ServiceUtil.java
+++ common/src/common/java/info/u_team/u_team_core/util/ServiceUtil.java
@@ -4,14 +4,18 @@ import java.util.List;
 import java.util.ServiceLoader;
 import java.util.function.Supplier;
 
+import info.u_team.u_team_core.UCoreReference;
+
 public class ServiceUtil {
 	
-	public static <I> I load(Class<I> service) {
-		final List<I> services = ServiceLoader.load(service).stream().map(Supplier::get).toList();
+	public static <I> I load(Class<I> serviceClass) {
+		final List<I> services = ServiceLoader.load(serviceClass).stream().map(Supplier::get).toList();
 		if (services.size() != 1) {
-			throw new IllegalStateException("Only one service for class '" + service + "' is allowed. Currently there are the following services registered: " + services);
+			throw new IllegalStateException("Only one service for class '" + serviceClass + "' is allowed. Currently there are the following services registered: " + services);
 		}
-		return services.get(0);
+		final I service = services.get(0);
+		UCoreReference.LOGGER.debug("Loaded service {} for class {}", service, serviceClass);
+		return service;
 	}
 	
 }
