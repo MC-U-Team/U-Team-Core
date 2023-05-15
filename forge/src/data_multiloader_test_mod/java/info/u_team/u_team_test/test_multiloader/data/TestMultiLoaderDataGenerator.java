52792659e0313b0188d23a08471143d952d3d836
HyCraftHD <hycrafthd@live.de>
2023-05-15 17:21:52 +0200

    Generate block state for multiloader test mod

--- forge/src/data_multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/data/TestMultiLoaderDataGenerator.java
+++ forge/src/data_multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/data/TestMultiLoaderDataGenerator.java
@@ -2,6 +2,7 @@ package info.u_team.u_team_test.test_multiloader.data;
 
 import info.u_team.u_team_core.data.GenerationData;
 import info.u_team.u_team_test.test_multiloader.TestMultiLoaderMod;
+import info.u_team.u_team_test.test_multiloader.data.provider.TestMultiLoaderBlockStateProvider;
 import info.u_team.u_team_test.test_multiloader.data.provider.TestMultiLoaderItemModelProvider;
 import info.u_team.u_team_test.test_multiloader.data.provider.TestMultiLoaderLanguagesProvider;
 import net.minecraftforge.data.event.GatherDataEvent;
@@ -16,6 +17,7 @@ public class TestMultiLoaderDataGenerator {
 	public static void data(GatherDataEvent event) {
 		final GenerationData data = new GenerationData(TestMultiLoaderMod.MODID, event);
 		
+		data.addProvider(event.includeClient(), TestMultiLoaderBlockStateProvider::new);
 		data.addProvider(event.includeClient(), TestMultiLoaderItemModelProvider::new);
 		data.addProvider(event.includeClient(), TestMultiLoaderLanguagesProvider::new);
 	}
