70c6406fbc0c087599bf8355d1aab61be868afa7
HyCraftHD <hycrafthd@live.de>
2023-05-15 20:31:19 +0200

    Use UBlock for registry

--- common/src/common-multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/init/TestMultiLoaderBlocks.java
+++ common/src/common-multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/init/TestMultiLoaderBlocks.java
@@ -2,10 +2,9 @@ package info.u_team.u_team_test.test_multiloader.init;
 
 import info.u_team.u_team_core.api.registry.BlockRegister;
 import info.u_team.u_team_core.api.registry.BlockRegistryEntry;
+import info.u_team.u_team_core.block.UBlock;
 import info.u_team.u_team_test.test_multiloader.TestMultiLoaderReference;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
 import net.minecraft.world.level.material.Material;
 
@@ -13,7 +12,7 @@ public class TestMultiLoaderBlocks {
 	
 	public static final BlockRegister BLOCKS = BlockRegister.create(TestMultiLoaderReference.MODID);
 	
-	public static final BlockRegistryEntry<Block, BlockItem> TEST = BLOCKS.register("test_block", () -> new Block(Properties.of(Material.STONE)), block -> new BlockItem(block, new Item.Properties()));
+	public static final BlockRegistryEntry<UBlock, BlockItem> TEST = BLOCKS.register("test_block", () -> new UBlock(Properties.of(Material.STONE)));
 	
 	public static void register() {
 		BLOCKS.register();
