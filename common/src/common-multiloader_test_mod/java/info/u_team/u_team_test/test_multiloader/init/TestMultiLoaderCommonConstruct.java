e69af89f054bc2e15b6acee247bea9c676b5bf8f
HyCraftHD <hycrafthd@live.de>
2023-05-15 15:52:21 +0200

    Create forge mod for multi loader test mod & move construct to common module

--- common/src/common-multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/init/TestMultiLoaderCommonConstruct.java
+++ common/src/common-multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/init/TestMultiLoaderCommonConstruct.java
@@ -2,9 +2,9 @@ package info.u_team.u_team_test.test_multiloader.init;
 
 import info.u_team.u_team_core.api.construct.Construct;
 import info.u_team.u_team_core.api.construct.ModConstruct;
-import info.u_team.u_team_test.test_multiloader.TestMultiLoaderMod;
+import info.u_team.u_team_test.test_multiloader.TestMultiLoaderReference;
 
-@Construct(modid = TestMultiLoaderMod.MODID)
+@Construct(modid = TestMultiLoaderReference.MODID)
 public class TestMultiLoaderCommonConstruct implements ModConstruct {
 	
 	@Override
