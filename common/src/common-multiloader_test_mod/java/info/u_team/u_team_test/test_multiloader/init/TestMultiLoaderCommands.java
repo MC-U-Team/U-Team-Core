9375eeac7356558b738aa96e01c7f35c85de4b1f
HyCraftHD <hycrafthd@live.de>
2023-05-20 03:09:33 +0200

    Update testmod to changes

--- common/src/common-multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/init/TestMultiLoaderCommands.java
+++ common/src/common-multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/init/TestMultiLoaderCommands.java
@@ -2,13 +2,15 @@ package info.u_team.u_team_test.test_multiloader.init;
 
 import info.u_team.u_team_core.api.registry.CommandRegister;
 import info.u_team.u_team_test.test_multiloader.command.TestNetworkCommand;
+import net.minecraft.Util;
 
 public class TestMultiLoaderCommands {
 	
-	public static void register() {
-		CommandRegister.register(handler -> {
-			TestNetworkCommand.register(handler.dispatcher());
-		});
-	}
+	public static final CommandRegister COMMANDS = Util.make(CommandRegister.create(), commands -> {
+		commands.register(handler -> TestNetworkCommand.register(handler.dispatcher()));
+	});
 	
+	public static void register() {
+		COMMANDS.register();
+	}
 }
