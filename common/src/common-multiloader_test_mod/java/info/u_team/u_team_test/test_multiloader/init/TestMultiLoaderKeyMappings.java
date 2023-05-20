e7f3873c74f746f03dcf7c47b3f94854a09ff9dd
HyCraftHD <hycrafthd@live.de>
2023-05-20 03:17:14 +0200

    Add test key mapping with test key for explicit c2s test key

---
+++ common/src/common-multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/init/TestMultiLoaderKeyMappings.java
@@ -0,0 +1,19 @@
+package info.u_team.u_team_test.test_multiloader.init;
+
+import org.lwjgl.glfw.GLFW;
+
+import info.u_team.u_team_core.api.registry.LazyEntry;
+import info.u_team.u_team_core.api.registry.client.KeyMappingRegister;
+import net.minecraft.client.KeyMapping;
+
+public class TestMultiLoaderKeyMappings {
+	
+	public static final KeyMappingRegister KEY_MAPPINGS = KeyMappingRegister.create();
+	
+	public static final LazyEntry<KeyMapping> TEST_NETWORK_EXPLICIT_C2S = KEY_MAPPINGS.register(() -> new KeyMapping("Explicit C2S", GLFW.GLFW_KEY_KP_1, "Test Network"));
+	
+	public static void register() {
+		KEY_MAPPINGS.register();
+	}
+	
+}
