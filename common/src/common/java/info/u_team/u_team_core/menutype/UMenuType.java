8ee92a77772b237226ccde3e7d73f29a0b84ed87
HyCraftHD <hycrafthd@live.de>
2023-05-22 15:27:52 +0200

    Move umenu type to common module

---
+++ common/src/common/java/info/u_team/u_team_core/menutype/UMenuType.java
@@ -0,0 +1,29 @@
+package info.u_team.u_team_core.menutype;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+
+public class UMenuType<T extends AbstractContainerMenu> extends MenuType<T> {
+	
+	public UMenuType(DataMenuSupplier<T> menuSupplier) {
+		this((MenuSupplier<T>) menuSupplier);
+	}
+	
+	public UMenuType(MenuSupplier<T> menuSupplier) {
+		super(menuSupplier, FeatureFlags.DEFAULT_FLAGS);
+	}
+	
+	@FunctionalInterface
+	public static interface DataMenuSupplier<T extends AbstractContainerMenu> extends MenuSupplier<T> {
+		
+		T create(int containerId, Inventory playerInventory, FriendlyByteBuf byteBuf);
+		
+		@Override
+		default T create(int containerId, Inventory playerInventory) {
+			return create(containerId, playerInventory, null);
+		}
+	}
+}
