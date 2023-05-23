d742e26202d928be933597c3ef1dfdb435d02e4f
HyCraftHD <hycrafthd@live.de>
2023-05-24 01:53:14 +0200

    Update forge common construct

--- forge/src/main/java/info/u_team/u_team_core/intern/init/UCoreForgeCommonConstruct.java
+++ forge/src/main/java/info/u_team/u_team_core/intern/init/UCoreForgeCommonConstruct.java
@@ -6,11 +6,16 @@ import info.u_team.u_team_core.UCoreMod;
 import info.u_team.u_team_core.api.construct.Construct;
 import info.u_team.u_team_core.api.construct.ModConstruct;
 import info.u_team.u_team_core.impl.ForgeNetworkHandler;
+import info.u_team.u_team_core.ingredient.ItemIngredient;
 import info.u_team.u_team_core.util.CastUtil;
 import info.u_team.u_team_core.util.registry.BusRegister;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.crafting.CraftingHelper;
 import net.minecraftforge.fml.IExtensionPoint.DisplayTest;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.registries.RegisterEvent;
 
 @Construct(modid = UCoreMod.MODID, priority = 2000)
 public class UCoreForgeCommonConstruct implements ModConstruct {
@@ -26,11 +31,19 @@ public class UCoreForgeCommonConstruct implements ModConstruct {
 		}));
 		
 		BusRegister.registerMod(UCoreNetworkForge::registerMod);
-		BusRegister.registerMod(UCoreRecipeSerializers::registerMod);
 		
 		CastUtil.assertCast(UCoreNetwork.NETWORK, ForgeNetworkHandler.class).setProtocolVersion(() -> version, protocolVersion -> {
 			// Allow clients to join / view servers without errors when uteamcore is not present there
 			return NetworkRegistry.acceptMissingOr(version).test(protocolVersion);
 		}, version::equals);
+		
+		BusRegister.registerMod(bus -> bus.addListener(UCoreForgeCommonConstruct::registerIngredient));
+	}
+	
+	// TODO move somewhere else
+	private static void registerIngredient(RegisterEvent event) {
+		if (event.getRegistryKey().equals(Registries.RECIPE_SERIALIZER)) {
+			CraftingHelper.register(new ResourceLocation(UCoreMod.MODID, "item"), ItemIngredient.Serializer.INSTANCE);
+		}
 	}
 }
