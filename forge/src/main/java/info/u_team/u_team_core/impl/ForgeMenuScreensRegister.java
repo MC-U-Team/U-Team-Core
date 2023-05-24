e961c79a496e85426d8c6b4166ee361908ccdfae
HyCraftHD <hycrafthd@live.de>
2023-05-24 02:29:07 +0200

    Implement forge menu screen register

---
+++ forge/src/main/java/info/u_team/u_team_core/impl/ForgeMenuScreensRegister.java
@@ -0,0 +1,34 @@
+package info.u_team.u_team_core.impl;
+
+import info.u_team.u_team_core.api.registry.MenuScreensRegister;
+import info.u_team.u_team_core.impl.common.CommonMenuScreensRegister;
+import info.u_team.u_team_core.util.CastUtil;
+import info.u_team.u_team_core.util.registry.BusRegister;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+public class ForgeMenuScreensRegister extends CommonMenuScreensRegister {
+	
+	ForgeMenuScreensRegister() {
+	}
+	
+	@Override
+	public void register() {
+		BusRegister.registerMod(bus -> bus.addListener(this::setup));
+	}
+	
+	private void setup(FMLClientSetupEvent event) {
+		event.enqueueWork(() -> screens.forEach((menuTypeSupplier, screenConstructor) -> {
+			MenuScreens.register(CastUtil.uncheckedCast(menuTypeSupplier.get()), screenConstructor);
+		}));
+	}
+	
+	public static class Factory implements MenuScreensRegister.Factory {
+		
+		@Override
+		public MenuScreensRegister create() {
+			return new ForgeMenuScreensRegister();
+		}
+	}
+	
+}
