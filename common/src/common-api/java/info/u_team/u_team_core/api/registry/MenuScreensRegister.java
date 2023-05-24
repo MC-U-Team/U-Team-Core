82644175b29648f0264d26fc0e93b792aa6a2dc6
HyCraftHD <hycrafthd@live.de>
2023-05-24 02:28:29 +0200

    Define menu screen register

---
+++ common/src/common-api/java/info/u_team/u_team_core/api/registry/MenuScreensRegister.java
@@ -0,0 +1,29 @@
+package info.u_team.u_team_core.api.registry;
+
+import java.util.function.Supplier;
+
+import info.u_team.u_team_core.util.ServiceUtil;
+import net.minecraft.client.gui.screens.MenuScreens.ScreenConstructor;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.MenuAccess;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+
+public interface MenuScreensRegister {
+	
+	static MenuScreensRegister create() {
+		return Factory.INSTANCE.create();
+	}
+	
+	<M extends AbstractContainerMenu, U extends Screen & MenuAccess<M>> void register(Supplier<? extends MenuType<? extends M>> menuTypeSupplier, ScreenConstructor<M, U> screenConstructor);
+	
+	void register();
+	
+	interface Factory {
+		
+		Factory INSTANCE = ServiceUtil.loadOne(Factory.class);
+		
+		MenuScreensRegister create();
+	}
+	
+}
