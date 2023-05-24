af74d3d64477022928a511140f56aa84053fb97d
HyCraftHD <hycrafthd@live.de>
2023-05-24 02:29:11 +0200

    Implement fabric menu screen register

---
+++ fabric/src/main/java/info/u_team/u_team_core/impl/FabricMenuScreensRegister.java
@@ -0,0 +1,28 @@
+package info.u_team.u_team_core.impl;
+
+import info.u_team.u_team_core.api.registry.MenuScreensRegister;
+import info.u_team.u_team_core.impl.common.CommonMenuScreensRegister;
+import info.u_team.u_team_core.util.CastUtil;
+import net.minecraft.client.gui.screens.MenuScreens;
+
+public class FabricMenuScreensRegister extends CommonMenuScreensRegister {
+	
+	FabricMenuScreensRegister() {
+	}
+	
+	@Override
+	public void register() {
+		screens.forEach((menuTypeSupplier, screenConstructor) -> {
+			MenuScreens.register(CastUtil.uncheckedCast(menuTypeSupplier.get()), screenConstructor);
+		});
+	}
+	
+	public static class Factory implements MenuScreensRegister.Factory {
+		
+		@Override
+		public MenuScreensRegister create() {
+			return new FabricMenuScreensRegister();
+		}
+	}
+	
+}
