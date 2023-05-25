26cda9e3be920be87384fd9ca48fa568e66c746e
HyCraftHD <hycrafthd@live.de>
2023-05-25 23:55:17 +0200

    Implement fabric platform

---
+++ fabric/src/main/java/info/u_team/u_team_core/impl/FabricPlatform.java
@@ -0,0 +1,23 @@
+package info.u_team.u_team_core.impl;
+
+import java.util.function.Supplier;
+
+import com.google.common.base.Suppliers;
+
+import info.u_team.u_team_core.api.Platform;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatform implements Platform {
+	
+	private final Supplier<Environment> environment = Suppliers.memoize(() -> {
+		return switch (FabricLoader.getInstance().getEnvironmentType()) {
+		case CLIENT -> Environment.CLIENT;
+		case SERVER -> Environment.SERVER;
+		};
+	});
+	
+	@Override
+	public Environment getEnvironment() {
+		return environment.get();
+	}
+}
