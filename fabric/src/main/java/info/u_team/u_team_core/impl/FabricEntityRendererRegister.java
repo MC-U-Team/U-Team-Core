9abcb8c0c89ab483efc0da4da1b3f383f3f3827a
HyCraftHD <hycrafthd@live.de>
2023-05-26 16:17:12 +0200

    Implement fabric entity renderer registry

---
+++ fabric/src/main/java/info/u_team/u_team_core/impl/FabricEntityRendererRegister.java
@@ -0,0 +1,29 @@
+package info.u_team.u_team_core.impl;
+
+import info.u_team.u_team_core.api.registry.client.EntityRendererRegister;
+import info.u_team.u_team_core.impl.common.CommonEntityRendererRegister;
+import info.u_team.u_team_core.util.CastUtil;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
+import net.minecraft.client.renderer.entity.EntityRenderers;
+
+public class FabricEntityRendererRegister extends CommonEntityRendererRegister {
+	
+	@Override
+	public void register() {
+		entityProviders.forEach((supplier, provider) -> {
+			EntityRenderers.register(CastUtil.uncheckedCast(supplier.get()), provider);
+		});
+		blockEntityProviders.forEach((supplier, provider) -> {
+			BlockEntityRenderers.register(CastUtil.uncheckedCast(supplier.get()), provider);
+		});
+	}
+	
+	public static class Factory implements EntityRendererRegister.Factory {
+		
+		@Override
+		public EntityRendererRegister create() {
+			return new FabricEntityRendererRegister();
+		}
+	}
+	
+}
