76ae7adb4d5499946e961ce1e178f6607c7e4a9e
HyCraftHD <hycrafthd@live.de>
2023-06-01 19:41:57 +0200

    Add our potions to creative tab

--- common/src/common-multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/init/TestMultiLoaderCreativeTabs.java
+++ common/src/common-multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/init/TestMultiLoaderCreativeTabs.java
@@ -1,10 +1,15 @@
 package info.u_team.u_team_test.test_multiloader.init;
 
+import java.util.stream.Stream;
+
 import info.u_team.u_team_core.api.registry.CreativeModeTabRegister;
 import info.u_team.u_team_core.api.registry.ResourceEntry;
 import info.u_team.u_team_test.test_multiloader.TestMultiLoaderReference;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTab.TabVisibility;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionUtils;
 
 public class TestMultiLoaderCreativeTabs {
 	
@@ -18,6 +23,11 @@ public class TestMultiLoaderCreativeTabs {
 			});
 			TestMultiLoaderItems.ITEMS.entryIterable().forEach(item -> {
 				output.accept(item);
+			});
+			Stream.of(Items.POTION, Items.SPLASH_POTION, Items.LINGERING_POTION, Items.TIPPED_ARROW).forEach(item -> {
+				TestMultiLoaderPotions.POTIONS.entryIterable().forEach(potion -> {
+					output.accept(PotionUtils.setPotion(new ItemStack(item), potion), TabVisibility.PARENT_TAB_ONLY);
+				});
 			});
 		});
 	});
