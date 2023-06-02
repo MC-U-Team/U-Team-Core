17e84e67a37462f907a7f20c8323a7e06ca5d2f9
HyCraftHD <hycrafthd@live.de>
2023-06-03 01:02:22 +0200

    Implement forge entity attribute register

---
+++ forge/src/main/java/info/u_team/u_team_core/impl/ForgeEntityAttributeRegister.java
@@ -0,0 +1,32 @@
+package info.u_team.u_team_core.impl;
+
+import info.u_team.u_team_core.api.registry.EntityAttributeRegister;
+import info.u_team.u_team_core.impl.common.CommonEntityAttributeRegister;
+import info.u_team.u_team_core.util.registry.BusRegister;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+
+public class ForgeEntityAttributeRegister extends CommonEntityAttributeRegister {
+	
+	ForgeEntityAttributeRegister() {
+	}
+	
+	@Override
+	public void register() {
+		BusRegister.registerMod(bus -> bus.addListener(this::entityAttributionCreation));
+	}
+	
+	private void entityAttributionCreation(EntityAttributeCreationEvent event) {
+		attributes.forEach((supplier, map) -> {
+			event.put(supplier.get(), map);
+		});
+	}
+	
+	public static class Factory implements EntityAttributeRegister.Factory {
+		
+		@Override
+		public EntityAttributeRegister create() {
+			return new ForgeEntityAttributeRegister();
+		}
+	}
+	
+}
