4d77548820bd4d422bc04149e0c7cd50aa03922b
HyCraftHD <hycrafthd@live.de>
2023-06-03 01:22:03 +0200

    Define spawn placement register

---
+++ common/src/common-api/java/info/u_team/u_team_core/api/registry/SpawnPlacementRegister.java
@@ -0,0 +1,28 @@
+package info.u_team.u_team_core.api.registry;
+
+import java.util.function.Supplier;
+
+import info.u_team.u_team_core.util.ServiceUtil;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.level.levelgen.Heightmap;
+
+public interface SpawnPlacementRegister {
+	
+	static SpawnPlacementRegister create() {
+		return Factory.INSTANCE.create();
+	}
+	
+	<T extends Mob> void register(Supplier<? extends EntityType<T>> supplier, SpawnPlacements.Type placementType, Heightmap.Types heightmap, SpawnPlacements.SpawnPredicate<T> predicate);
+	
+	void register();
+	
+	interface Factory {
+		
+		Factory INSTANCE = ServiceUtil.loadOne(Factory.class);
+		
+		SpawnPlacementRegister create();
+	}
+	
+}
