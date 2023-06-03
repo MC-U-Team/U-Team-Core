31567c2dfd5c3757c0f7adb78025d275f33858a2
HyCraftHD <hycrafthd@live.de>
2023-06-03 13:43:38 +0200

    Add spawning with fabric as well. Maybe better way to integrate forge data gen and fabric code approach?

---
+++ fabric/src/multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/init/fabric/TestMultiLoaderFabricBiomeModification.java
@@ -0,0 +1,18 @@
+package info.u_team.u_team_test.test_multiloader.init.fabric;
+
+import info.u_team.u_team_core.event.SetupEvents;
+import info.u_team.u_team_test.test_multiloader.init.TestMultiLoaderEntityTypes;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.minecraft.tags.BiomeTags;
+
+public class TestMultiLoaderFabricBiomeModification {
+	
+	private static void setup() {
+		BiomeModifications.addSpawn(context -> context.hasTag(BiomeTags.IS_OVERWORLD), TestMultiLoaderEntityTypes.TEST_LIVING.get().getCategory(), TestMultiLoaderEntityTypes.TEST_LIVING.get(), 80, 4, 4);
+	}
+	
+	static void register() {
+		SetupEvents.COMMON_SETUP.register(TestMultiLoaderFabricBiomeModification::setup);
+	}
+	
+}
---
+++ fabric/src/multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/init/fabric/TestMultiLoaderFabricCommonConstruct.java
@@ -0,0 +1,15 @@
+package info.u_team.u_team_test.test_multiloader.init.fabric;
+
+import info.u_team.u_team_core.api.construct.Construct;
+import info.u_team.u_team_core.api.construct.ModConstruct;
+import info.u_team.u_team_test.test_multiloader.TestMultiLoaderReference;
+
+@Construct(modid = TestMultiLoaderReference.MODID)
+public class TestMultiLoaderFabricCommonConstruct implements ModConstruct {
+	
+	@Override
+	public void construct() {
+		TestMultiLoaderFabricBiomeModification.register();
+	}
+	
+}
