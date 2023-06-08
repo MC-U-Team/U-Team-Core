02f3d1a7fc1486a905f043a7aa06d72460ca6b7f
HyCraftHD <hycrafthd@live.de>
2023-06-08 13:43:56 +0200

    Fix wrong modid

--- forge/src/test_mod/java/info/u_team/u_team_test/init/TestCreativeTabs.java
+++ forge/src/test_mod/java/info/u_team/u_team_test/init/TestCreativeTabs.java
@@ -2,14 +2,14 @@ package info.u_team.u_team_test.init;
 
 import info.u_team.u_team_core.api.registry.CreativeModeTabRegister;
 import info.u_team.u_team_core.api.registry.ResourceEntry;
-import info.u_team.u_team_test.test_multiloader.TestMultiLoaderReference;
+import info.u_team.u_team_test.TestMod;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class TestCreativeTabs {
 	
-	public static final CreativeModeTabRegister CREATIVE_TABS = CreativeModeTabRegister.create(TestMultiLoaderReference.MODID);
+	public static final CreativeModeTabRegister CREATIVE_TABS = CreativeModeTabRegister.create(TestMod.MODID);
 	
 	public static final ResourceEntry<CreativeModeTab> TAB = CREATIVE_TABS.register("tab", builder -> {
 		builder.icon(() -> new ItemStack(TestBlocks.BASIC.get()));
