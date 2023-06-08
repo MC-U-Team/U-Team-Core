ccdffb29c9e944915e17f2a5ff5eadc03e7c0f85
HyCraftHD <hycrafthd@live.de>
2023-06-08 15:54:23 +0200

    Sorting is broken

--- forge/src/main/java/info/u_team/u_team_core/impl/ForgeCreativeModeTabRegisterBuilder.java
+++ forge/src/main/java/info/u_team/u_team_core/impl/ForgeCreativeModeTabRegisterBuilder.java
@@ -7,6 +7,8 @@ public class ForgeCreativeModeTabRegisterBuilder implements CreativeModeTabRegis
 	
 	@Override
 	public CreativeModeTab.Builder create() {
+		// .withTabsBefore(BUILDING_BLOCKS, COLORED_BLOCKS, NATURAL_BLOCKS, FUNCTIONAL_BLOCKS, REDSTONE_BLOCKS, HOTBAR, SEARCH,
+		// TOOLS_AND_UTILITIES, COMBAT, FOOD_AND_DRINKS, INGREDIENTS, SPAWN_EGGS, OP_BLOCKS, INVENTORY); // TODO
 		return CreativeModeTab.builder();
 	}
 	
