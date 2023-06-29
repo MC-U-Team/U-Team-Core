26f3822f4acce56682c92e35dd221ae913abcb4e
HyCraftHD <hycrafthd@live.de>
2023-06-29 19:08:37 +0200

    Implement fabric enchantment util

---
+++ fabric/src/main/java/info/u_team/u_team_core/impl/FabricEnchantmentUtil.java
@@ -0,0 +1,15 @@
+package info.u_team.u_team_core.impl;
+
+import info.u_team.u_team_core.util.EnchantmentUtil;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+
+public class FabricEnchantmentUtil implements EnchantmentUtil.Handler {
+	
+	@Override
+	public int getEnchantmentLevel(Enchantment enchantment, ItemStack stack) {
+		return EnchantmentHelper.getItemEnchantmentLevel(enchantment, stack);
+	}
+	
+}
