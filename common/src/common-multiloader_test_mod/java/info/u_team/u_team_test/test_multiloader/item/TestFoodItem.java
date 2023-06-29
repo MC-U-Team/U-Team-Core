ee5a78f22be567118f918a4d107fc1ebcdb6ba6b
HyCraftHD <hycrafthd@live.de>
2023-06-29 20:05:38 +0200

    Add test for enchantment util

--- common/src/common-multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/item/TestFoodItem.java
+++ common/src/common-multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/item/TestFoodItem.java
@@ -2,11 +2,20 @@ package info.u_team.u_team_test.test_multiloader.item;
 
 import info.u_team.u_team_core.item.UItem;
 import info.u_team.u_team_core.item.food.UFoodPropertiesBuilder;
+import info.u_team.u_team_core.util.EnchantmentUtil;
+import info.u_team.u_team_core.util.RegistryUtil;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Level;
 
 public class TestFoodItem extends UItem {
 	
@@ -19,5 +28,20 @@ public class TestFoodItem extends UItem {
 	@Override
 	public boolean isFoil(ItemStack stack) {
 		return true;
+	}
+	
+	@Override
+	public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity livingEntity) {
+		if (livingEntity instanceof ServerPlayer player) {
+			final int enchantment = EnchantmentUtil.getEnchantmentLevel(Enchantments.UNBREAKING, stack);
+			if (enchantment > 0) {
+				final ItemEntity itemEntity = player.drop(new ItemStack(RegistryUtil.getBuiltInRegistry(Registries.ITEM).getRandom(player.getRandom()).map(reference -> reference.value()).orElse(Items.STONE), enchantment * 5), true);
+				if (itemEntity != null) {
+					itemEntity.setDefaultPickUpDelay();
+					itemEntity.setExtendedLifetime();
+				}
+			}
+		}
+		return super.finishUsingItem(stack, level, livingEntity);
 	}
 }
