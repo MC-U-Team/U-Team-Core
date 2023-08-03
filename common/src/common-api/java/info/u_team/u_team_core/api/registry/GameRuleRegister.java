6bf227c95d8ba55a79283c2dcbc59a2fed4493ae
HyCraftHD <hycrafthd@live.de>
2023-08-03 23:52:22 +0200

    Define interface for game rule registration

---
+++ common/src/common-api/java/info/u_team/u_team_core/api/registry/GameRuleRegister.java
@@ -0,0 +1,41 @@
+package info.u_team.u_team_core.api.registry;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.function.Supplier;
+
+import com.google.common.collect.Streams;
+
+import info.u_team.u_team_core.util.CastUtil;
+import info.u_team.u_team_core.util.ServiceUtil;
+import net.minecraft.world.level.GameRules;
+
+public interface GameRuleRegister extends Iterable<LazyEntry<GameRules.Key<? extends GameRules.Value<?>>>> {
+	
+	static GameRuleRegister create() {
+		return Factory.INSTANCE.create();
+	}
+	
+	<T extends GameRules.Value<T>> LazyEntry<GameRules.Key<T>> register(String name, GameRules.Category category, Supplier<? extends GameRules.Type<T>> type);
+	
+	void register();
+	
+	Collection<LazyEntry<GameRules.Key<? extends GameRules.Value<?>>>> getEntries();
+	
+	@Override
+	default Iterator<LazyEntry<GameRules.Key<? extends GameRules.Value<?>>>> iterator() {
+		return getEntries().iterator();
+	}
+	
+	default Iterable<GameRules.Key<? extends GameRules.Value<?>>> entryIterable() {
+		return () -> CastUtil.uncheckedCast(Streams.stream(this).map(LazyEntry::get).iterator());
+	}
+	
+	interface Factory {
+		
+		Factory INSTANCE = ServiceUtil.loadOne(Factory.class);
+		
+		GameRuleRegister create();
+	}
+	
+}
