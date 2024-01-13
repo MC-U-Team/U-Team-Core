14a90fd6e39d31c9c9e0891d955b2e5ac510a4fe
HyCraftHD <hycrafthd@live.de>
2024-01-13 05:27:06 +0100

    Add missing resources to neoforge such as mixin config / coremods and textures

---
+++ neoforge/src/main/resources/asm/dataprovider-set-indent.js
@@ -0,0 +1,30 @@
+function initializeCoreMod() {
+	ASMAPI = Java.type("net.minecraftforge.coremod.api.ASMAPI")
+
+	Opcodes = Java.type("org.objectweb.asm.Opcodes")
+
+	return {
+		"DataProvider": {
+			"target": {
+				"type": "CLASS",
+				"name": "net.minecraft.data.DataProvider"
+			},
+			"transformer": function(classNode) {
+				if (ASMAPI.getSystemPropertyFlag("uteamcore.dataprovider-set-indent")) {
+					replaceIndent(classNode);
+				}
+				return classNode;
+			}
+		}
+	}
+}
+
+function replaceIndent(classNode) {
+	classNode.methods.forEach(function(methodNode) {
+		var ldc = ASMAPI.findFirstInstruction(methodNode, Opcodes.LDC)
+		if (ldc != null && ldc.cst.equals("\u0020\u0020")) {
+			ldc.cst = "\u0009";
+			ASMAPI.log("INFO", "Replaced json indent in {}#{}", "DataProvider", methodNode.name)
+		}
+	});
+}
