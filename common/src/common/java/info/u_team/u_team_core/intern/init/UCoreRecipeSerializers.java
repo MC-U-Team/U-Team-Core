782592c48911263201bec017cd89debddb8c74cf
HyCraftHD <hycrafthd@live.de>
2024-05-09 16:53:35 +0200

    Remove custom dyable item implementation. Should be now usable from vanilla with item components

--- common/src/common/java/info/u_team/u_team_core/intern/init/UCoreRecipeSerializers.java
+++ common/src/common/java/info/u_team/u_team_core/intern/init/UCoreRecipeSerializers.java
@@ -3,17 +3,14 @@ package info.u_team.u_team_core.intern.init;
 import info.u_team.u_team_core.UCoreReference;
 import info.u_team.u_team_core.api.registry.CommonRegister;
 import info.u_team.u_team_core.api.registry.RegistryEntry;
-import info.u_team.u_team_core.intern.recipe.DyeableItemDyeRecipe;
 import info.u_team.u_team_core.intern.recipe.NoMirrorShapedRecipe;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.crafting.RecipeSerializer;
-import net.minecraft.world.item.crafting.SimpleCraftingRecipeSerializer;
 
 public class UCoreRecipeSerializers {
 	
 	public static final CommonRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS = CommonRegister.create(Registries.RECIPE_SERIALIZER, UCoreReference.MODID);
 	
-	public static final RegistryEntry<SimpleCraftingRecipeSerializer<DyeableItemDyeRecipe>> CRAFTING_SPECIAL_ITEMDYE = RECIPE_SERIALIZERS.register("crafting_special_itemdye", () -> new SimpleCraftingRecipeSerializer<>(DyeableItemDyeRecipe::new));
 	public static final RegistryEntry<NoMirrorShapedRecipe.Serializer> NO_MIRROR_SHAPED = RECIPE_SERIALIZERS.register("crafting_shaped_no_mirror", NoMirrorShapedRecipe.Serializer::new);
 	
 	static void register() {
