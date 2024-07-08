ad2793cf4a543dc0c6cdeafba9987b7ecaf4cc03
HyCraftHD <hycrafthd@live.de>
2024-07-08 18:06:18 +0200

    Fix registry lookup "the" easy way. HolderLookup is getting annoying ...

--- forge/src/forge_data_multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/forge/data/provider/ForgeTestMultiLoaderGlobalLootModifierProvider.java
+++ forge/src/forge_data_multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/forge/data/provider/ForgeTestMultiLoaderGlobalLootModifierProvider.java
@@ -1,14 +1,33 @@
 package info.u_team.u_team_test.test_multiloader.forge.data.provider;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
+
 import info.u_team.u_team_core.data.CommonGlobalLootModifierProvider;
 import info.u_team.u_team_core.data.GenerationData;
 import info.u_team.u_team_test.test_multiloader.forge.global_loot_modifier.TestLootModifier;
+import info.u_team.u_team_test.test_multiloader.init.TestMultiLoaderEnchantments;
 import info.u_team.u_team_test.test_multiloader.loot_item_condition.TestEnchantmentLootItemCondition;
+import net.minecraft.core.HolderLookup.Provider;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.RegistrySetBuilder.PatchedRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.registries.RegistryPatchGenerator;
 
 public class ForgeTestMultiLoaderGlobalLootModifierProvider extends CommonGlobalLootModifierProvider {
 	
 	public ForgeTestMultiLoaderGlobalLootModifierProvider(GenerationData data) {
 		super(data);
+	}
+	
+	@Override
+	public <U> CompletableFuture<U> withRegistries(Function<Provider, ? extends CompletionStage<U>> function) {
+		// Dummy holder lookup with enchantment as it will else not find / load our registries. Not sure how there is an elegant
+		// fix other than what vanilla does with its bootstrap methods ...
+		return RegistryPatchGenerator.createLookup(getGenerationData().registriesFuture(), new RegistrySetBuilder().add(Registries.ENCHANTMENT, context -> {
+			context.register(TestMultiLoaderEnchantments.TEST, null);
+		})).thenApply(PatchedRegistries::full).thenCompose(function);
 	}
 	
 	@Override
