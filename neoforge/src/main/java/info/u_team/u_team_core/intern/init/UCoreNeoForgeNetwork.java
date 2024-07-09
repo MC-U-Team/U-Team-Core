65d3dcddacb24606da97f471722be2453fab69ef
HyCraftHD <hycrafthd@live.de>
2024-07-09 20:00:10 +0200

    Fix wrong network direction crash

--- neoforge/src/main/java/info/u_team/u_team_core/intern/init/UCoreNeoForgeNetwork.java
+++ neoforge/src/main/java/info/u_team/u_team_core/intern/init/UCoreNeoForgeNetwork.java
@@ -21,7 +21,7 @@ public class UCoreNeoForgeNetwork {
 				.executesOn(HandlerThread.NETWORK) //
 				.playToClient(ContainerSetFluidContentMessage.TYPE, StreamCodec.of(ContainerSetFluidContentMessage::write, ContainerSetFluidContentMessage::read), ContainerSetFluidContentMessage.Handler::handle) //
 				.playToClient(ContainerSetFluidSlotMessage.TYPE, StreamCodec.of(ContainerSetFluidSlotMessage::write, ContainerSetFluidSlotMessage::read), ContainerSetFluidSlotMessage.Handler::handle) //
-				.playToClient(FluidClickContainerMessage.TYPE, StreamCodec.of(FluidClickContainerMessage::write, FluidClickContainerMessage::read), FluidClickContainerMessage.Handler::handle);
+				.playToServer(FluidClickContainerMessage.TYPE, StreamCodec.of(FluidClickContainerMessage::write, FluidClickContainerMessage::read), FluidClickContainerMessage.Handler::handle);
 	}
 	
 	public static void registerMod(IEventBus bus) {
