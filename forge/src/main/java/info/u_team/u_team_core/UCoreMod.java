b36899213d711497d6771333d658dfc4658d6d4f
HyCraftHD <hycrafthd@live.de>
2024-07-20 22:58:55 +0200

    Remove forge jar sign checker (useless)

--- forge/src/main/java/info/u_team/u_team_core/UCoreMod.java
+++ forge/src/main/java/info/u_team/u_team_core/UCoreMod.java
@@ -3,7 +3,6 @@ package info.u_team.u_team_core;
 import org.slf4j.Logger;
 
 import info.u_team.u_team_core.util.annotation.AnnotationManager;
-import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(UCoreMod.MODID)
@@ -13,8 +12,6 @@ public class UCoreMod {
 	public static final Logger LOGGER = UCoreReference.LOGGER;
 	
 	public UCoreMod() {
-		JarSignVerifier.checkSigned(MODID);
-		
 		AnnotationManager.callAnnotations(MODID);
 	}
 }
