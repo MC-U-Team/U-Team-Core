af652f6740443bc545f5e99f8da9132c40521e5c
HyCraftHD <hycrafthd@live.de>
2025-01-25 00:45:33 +0100

    Add docs to UItemExtension

--- common/src/common_api/java/info/u_team/u_team_core/api/item/UItemExtension.java
+++ common/src/common_api/java/info/u_team/u_team_core/api/item/UItemExtension.java
@@ -5,10 +5,22 @@ import net.minecraft.world.item.ItemStack;
 
 public interface UItemExtension {
 	
+	/**
+	 * Should play update animations when e.g. switching item slots or updating stacks
+	 * @param oldStack Old stack
+	 * @param newStack New stack
+	 * @return Return true to update
+	 */
 	default boolean shouldPlayUpdateAnimation(ItemStack oldStack, ItemStack newStack) {
 		return !oldStack.equals(newStack);
 	}
 	
+	/**
+	 * Should the player be able to drop this item?
+	 * @param stack Stack
+	 * @param player Player
+	 * @return Return true to allow drops
+	 */
 	default boolean canBeDropped(ItemStack stack, Player player) {
 		return true;
 	}
