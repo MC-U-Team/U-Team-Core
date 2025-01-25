245888d9369546e05f854b6b15a9e8c19b9915d7
HyCraftHD <hycrafthd@live.de>
2025-01-25 01:07:29 +0100

    Implement test for update item entity method in test use item

--- common/src/common_multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/item/TestUseItem.java
+++ common/src/common_multiloader_test_mod/java/info/u_team/u_team_test/test_multiloader/item/TestUseItem.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import info.u_team.u_team_core.item.UItem;
 import info.u_team.u_team_core.util.LevelUtil;
+import info.u_team.u_team_core.util.MathUtil;
 import info.u_team.u_team_test.test_multiloader.init.TestMultiLoaderDataComponentTypes;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
@@ -69,5 +72,19 @@ public class TestUseItem extends UItem {
 	@Override
 	public boolean canBeDropped(ItemStack stack, Player player) {
 		return !player.isSteppingCarefully();
+	}
+	
+	@Override
+	public boolean updateItemEntity(ItemStack stack, ItemEntity entity) {
+		final Level level = entity.level();
+		if (level.isClientSide()) {
+			final Vec3 position = entity.position();
+			if (level.getRandom().nextInt(10) == 0) {
+				for (int i = 0; i < 5; i++) {
+					level.addParticle(ParticleTypes.TOTEM_OF_UNDYING, true, position.x(), position.y() + 0.5, position.z(), MathUtil.randomNumberInRange(level.getRandom(), -0.2, 0.2), MathUtil.randomNumberInRange(level.getRandom(), 0.1, 1.5), MathUtil.randomNumberInRange(level.getRandom(), -0.2, 0.2));
+				}
+			}
+		}
+		return false;
 	}
 }
