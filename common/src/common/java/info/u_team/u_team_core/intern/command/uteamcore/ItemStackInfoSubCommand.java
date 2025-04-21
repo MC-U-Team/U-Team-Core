5668132b9539eb5af87f1ab0bb51f37145d02190
HyCraftHD <hycrafthd@live.de>
2025-04-21 14:53:01 +0200

    Port command
    Signed-off-by: HyCraftHD <hycrafthd@live.de>

--- common/src/common/java/info/u_team/u_team_core/intern/command/uteamcore/ItemStackInfoSubCommand.java
+++ common/src/common/java/info/u_team/u_team_core/intern/command/uteamcore/ItemStackInfoSubCommand.java
@@ -11,9 +11,9 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.ClickEvent.SuggestCommand;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.HoverEvent;
+import net.minecraft.network.chat.HoverEvent.ShowText;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
@@ -59,8 +59,8 @@ public class ItemStackInfoSubCommand {
 		final MutableComponent component = Component.literal(vanillaRegistry.getKey(entry).toString());
 		final String className = getClassString(entry);
 		component.withStyle(style -> style.withColor(ChatFormatting.AQUA) //
-				.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal(className).withStyle(ChatFormatting.YELLOW))) //
-				.withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, className)));
+				.withHoverEvent(new ShowText(Component.literal(className).withStyle(ChatFormatting.YELLOW))) //
+				.withClickEvent(new SuggestCommand(className)));
 		return component;
 	}
 	
